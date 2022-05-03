50b8d087c63e0fed509fa663385d361d5479521c
ikeyu0806 <ikeyu0806@gmail.com>
2022-05-03 10:10:46 +0900

    fix navbar

--- frontend/components/templates/HomeTemplate.tsx
+++ frontend/components/templates/HomeTemplate.tsx
@@ -1,19 +1,31 @@
 import React from 'react'
-import { Navbar, Nav } from 'react-bootstrap'
+import { Container, Navbar, Nav, NavDropdown, Button } from 'react-bootstrap'
 
 const HomeTemplate = (): JSX.Element => {
   return (
-    <>
-      <Navbar>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">機能</Nav.Link>
-          <Nav.Link href="#features">料金</Nav.Link>
-          <Nav.Link href="#pricing">お問い合わせ</Nav.Link>
-          <Nav.Link href="#pricing">ログイン</Nav.Link>
-          <Nav.Link href="#pricing">無料で試してみる</Nav.Link>
-        </Nav>
-      </Navbar >
-    </>
+    <Container>
+<Navbar collapseOnSelect expand='lg'>
+  <Container>
+  <Navbar.Brand href='#home'>SmartLesson</Navbar.Brand>
+  <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+  <Navbar.Collapse id='responsive-navbar-nav'>
+    <Nav className='me-auto'>
+      <Nav.Link href='#features'>概要</Nav.Link>
+      <Nav.Link href='#features'>機能</Nav.Link>
+      <Nav.Link href='#pricing'>料金</Nav.Link>
+    </Nav>
+    <Nav>
+      <Nav.Link href='#deets'>
+        <Button>ログイン</Button>
+      </Nav.Link>
+      <Nav.Link href='#memes'>
+        <Button>無料でお試し</Button>
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+    </Container>
   )
 }  
 export default HomeTemplate
