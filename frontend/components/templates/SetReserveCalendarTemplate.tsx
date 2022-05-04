870dee70e44f174483626b36108638a28b0499dc
ikeyu0806 <ikeyu0806@gmail.com>
2022-05-04 16:10:11 +0900

    カレンダーモーダル追加

--- frontend/components/templates/SetReserveCalendarTemplate.tsx
+++ frontend/components/templates/SetReserveCalendarTemplate.tsx
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
-import { Container } from 'react-bootstrap'
+import { Container, Modal, Button, Form } from 'react-bootstrap'
 
 const SetReserveCalendarTemplate = (): JSX.Element => {
+  const [showModal, setShowModal] = useState(false)
+
   return(
     <>
       <Container>
-        <div>
-          <FullCalendar plugins={[dayGridPlugin, interactionPlugin]}
-                        initialView='dayGridMonth'
-                        selectable={true}
-                        select={() => alert('hhhh')}
-                        locale='ja' />
-        </div>
+        <FullCalendar plugins={[dayGridPlugin, interactionPlugin]}
+                      initialView='dayGridMonth'
+                      selectable={true}
+                      select={() => setShowModal(true)}
+                      locale='ja' />
+        <Modal show={showModal} size='lg'>
+          <Modal.Header>
+            <Modal.Title>新規予約枠登録</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3" controlId="menuName">
+                <Form.Label>メニュー名</Form.Label>
+                <Form.Control placeholder="メニュー名" />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="menuText">
+                <Form.Label>メニュー説明文</Form.Label>
+                <Form.Control placeholder="メニュー説明文" />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button variant='secondary' onClick={() => setShowModal(false)}>閉じる</Button>
+            <Button variant='primary'>登録する</Button>
+          </Modal.Footer>
+        </Modal>
       </Container>
     </>
   )
