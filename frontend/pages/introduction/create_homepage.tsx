cc32dca889468b64d41006deeb5a0cb138da3639
ikeyu0806 <ikeyu0806@gmail.com>
2022-05-08 16:47:16 +0900

    ホームページ作成画面にカルーセルサンプル追加

--- frontend/pages/introduction/create_homepage.tsx
+++ frontend/pages/introduction/create_homepage.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import IntroductionNavbar from '../../components/atoms/IntroductionNavbar'
-import { Button, Container, Card, Row, Col, Form, Navbar } from 'react-bootstrap'
+import { Button, Container, Card, Row, Col, Carousel, Navbar } from 'react-bootstrap'
 import { useRouter } from 'next/router'
 import PencilAquareIcon from '../../components/atoms/PencilAquareIcon'
 import PlusCircleIcon from '../../components/atoms/PlusCircleIcon'
@@ -19,16 +19,56 @@ const CreateHomepage: NextPage = () => {
           <Col>
           </Col>
           <Col sm={9}>
+          <div className='mb20'>
+            トップページ サイト内パス: /
+          </div>
           <Card>
             <Card.Body>
             <Navbar>
               サイトタイトル
               <PencilAquareIcon width={20} height={20} fill={'#0000FF'} />
             </Navbar>
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className='d-block w-100'
+                  src='/images/snow_mountains.jpg'
+                  alt='First slide'
+                />
+                <Carousel.Caption>
+                  <h3>First slide label</h3>
+                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className='d-block w-100'
+                  src='/images/snow_mountains.jpg'
+                  alt='Second slide'
+                />
+
+                <Carousel.Caption>
+                  <h3>Second slide label</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className='d-block w-100'
+                  src='/images/snow_mountains.jpg'
+                  alt='Third slide'
+                />
+
+                <Carousel.Caption>
+                  <h3>Third slide label</h3>
+                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
             </Card.Body>
           </Card>
           <div className='text-center mt30 mb30'>
-          <PlusCircleIcon width={40} height={40} fill={'#0000FF'} />
+            <PlusCircleIcon width={40} height={40} fill={'#0000FF'} />
           </div>
         </Col>
         <Col>
@@ -38,17 +78,21 @@ const CreateHomepage: NextPage = () => {
       <br />
       <Row>
         <Col></Col>
-        <Col>
+        <Col></Col>
+        <Col sm={2}>
           <Button variant='outline-primary' size='lg' onClick={() => router.back()}>戻る</Button>
         </Col>
+        <Col></Col>
         <Col></Col>
         <Col sm={2}>
           <Button variant='primary' size='lg' href='/introduction/set_reserve_calendar'>保存して次へ</Button>
         </Col>
         <Col></Col>
-        <Col>
+        <Col></Col>
+        <Col sm={2}>
           <Button variant='outline-primary' size='lg' href='/introduction/set_reserve_calendar'>スキップ</Button>
         </Col>
+        <Col></Col>
         <Col></Col>
       </Row>
     </>
