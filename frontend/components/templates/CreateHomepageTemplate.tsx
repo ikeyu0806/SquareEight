0a81e451455c135413a6764728a50145d8b84e57
ikeyu0806 <ikeyu0806@gmail.com>
2022-05-19 21:59:14 +0900

    テキスト+画像ブロック追加モーダル実装

--- frontend/components/templates/CreateHomepageTemplate.tsx
+++ frontend/components/templates/CreateHomepageTemplate.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Button, Container, Card, Row, Col, Carousel, Navbar, Modal } from 'react-bootstrap'
+import { Button, Container, Card, Row, Col, Carousel, Navbar, Modal, Form } from 'react-bootstrap'
 import PencilAquareIcon from '../../components/atoms/PencilAquareIcon'
 import PlusCircleIcon from '../../components/atoms/PlusCircleIcon'
 import { RootState } from '../../redux/store'
@@ -7,9 +7,21 @@ import { useSelector, useDispatch } from 'react-redux'
 import { blockTypeChanged } from '../../redux/homepageSlice'
 
 const CreateHomepageTemplate = (): JSX.Element => {
+  const dispatch = useDispatch()
+  const BLOCKTYPE = {
+    Small: 'small'
+  }
+
   const [showBlockModal, setShowBlockModal] = useState(false)
+  const [selectedBlockedType, setSelectedBlockedType] = useState('')
+  const [image, setImage] = useState('')
 
   const blockType = useSelector((state: RootState) => state.homepage.blockType)
+
+  const handleChangeFile = (e: any) => {
+    const { files } = e.target
+    setImage(window.URL.createObjectURL(files[0]))
+  }
 
   return(
     <>
@@ -69,7 +81,7 @@ const CreateHomepageTemplate = (): JSX.Element => {
               <span className='mr10'>ブロックを追加</span>
               <a onClick={() => setShowBlockModal(true)}><PlusCircleIcon width={40} height={40} fill={'#0000FF'} /></a>
             </div>
-            <Card.Footer className="text-muted text-center">Copyright SmartLesson Inc. 2022</Card.Footer>
+            <Card.Footer className='text-muted text-center'>Copyright SmartLesson Inc. 2022</Card.Footer>
           </Card>
           <div className='text-center mt30 mb30'>
             <span className='mr10'>ページを追加</span>
@@ -82,14 +94,20 @@ const CreateHomepageTemplate = (): JSX.Element => {
       </Container>
       <Modal show={showBlockModal} size='lg'>
         <Modal.Header> 
-          <Modal.Title>追加したいブロックを選択してください</Modal.Title>
+          <Modal.Title>
+          {blockType === '' &&
+            <span>追加したいブロックを選択してください</span>}
+          {blockType === 'textImage' &&
+            <span>テキストと画像を編集してください</span>}
+          </Modal.Title>
         </Modal.Header>
         <Modal.Body>
+          {blockType === '' &&
           <Row>
             <Col>
               <Card>
                 <Card.Body>
-                  <input className='form-check-input mr10' type='checkbox' />
+                  <input className='form-check-input mr10' type='checkbox' onChange={() => setSelectedBlockedType('textImage')} />
                   <span>テキスト+画像</span>
                 </Card.Body>
               </Card>
@@ -130,11 +148,37 @@ const CreateHomepageTemplate = (): JSX.Element => {
               </Card>
               <br />
             </Col>
-          </Row>
+          </Row>}
+          {blockType === 'textImage' &&
+            <>
+              <Row>
+                <Col>
+                  <h2>見出し<PencilAquareIcon width={20} height={20} fill={'#0000FF'} /></h2>
+                  <div>
+                    本文を入力<PencilAquareIcon width={20} height={20} fill={'#0000FF'} />
+                  </div>
+                </Col>
+                <Col>
+                <img
+                  className='d-block w-100'
+                  src={image}
+                  alt='image'
+                />
+                  <Form.Group controlId='formFile' className='mb5 mt10'>
+                    <Form.Label>画像をアップロード</Form.Label>
+                    <Form.Control type='file' onChange={handleChangeFile} />
+                  </Form.Group>
+                </Col>
+              </Row>
+            </>
+          }
         </Modal.Body>
         <Modal.Footer>
-          <Button variant='secondary' onClick={() => setShowBlockModal(false)}>閉じる</Button>
-          <Button variant='primary'>次へ</Button>
+          <Button variant='secondary' onClick={() => { setShowBlockModal(false); dispatch(blockTypeChanged(''))}}>閉じる</Button>
+          {blockType === '' &&
+           <Button variant='primary' onClick={() => dispatch(blockTypeChanged(selectedBlockedType))}>次へ</Button>}
+          {blockType === 'textImage' &&
+          <Button variant='primary' onClick={() => dispatch(blockTypeChanged(selectedBlockedType))}>編集を終えてブロックを追加</Button>}
         </Modal.Footer>
       </Modal>
     </>
@@ -142,5 +186,3 @@ const CreateHomepageTemplate = (): JSX.Element => {
 }
 
 export default CreateHomepageTemplate
-
-
