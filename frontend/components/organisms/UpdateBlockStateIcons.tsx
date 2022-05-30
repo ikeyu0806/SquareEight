7ea3f368ca2b3c4d3cf69636a6312592e1de25ae
ikeyu0806 <ikeyu0806@gmail.com>
2022-05-30 09:43:56 +0900

    ブロックを上に移動させるメソッド実装

--- frontend/components/organisms/UpdateBlockStateIcons.tsx
+++ frontend/components/organisms/UpdateBlockStateIcons.tsx
@@ -12,6 +12,37 @@ const UpdateBlockStateIcons = ({ blockID, sortOrder }: UpdateBlockStateIconsProp
   const dispatch = useDispatch()
   const pageContent = useSelector((state: RootState) => state.homepage.pageContent)
 
+  const moveUpBlock = () => {
+    let updatePageContentState: PageContentState[]
+    const decrementSortOrder = sortOrder - 1
+    let moveDownPageContent = pageContent.find(content => content.sortOrder === sortOrder)
+    let moveUpPageContent = pageContent.find(content => content.sortOrder === decrementSortOrder)
+    updatePageContentState = pageContent.filter(content => content.sortOrder !== sortOrder)
+    updatePageContentState = updatePageContentState.filter(content => content.sortOrder !== decrementSortOrder)
+    dispatch(pageContentChanged(updatePageContentState))
+    if (moveDownPageContent !== undefined) {
+      let updateMoveDownPageContent = { blockID: moveDownPageContent.blockID,
+                                        blockType: moveDownPageContent.blockType,
+                                        blockState: moveDownPageContent.blockState,
+                                        sortOrder: decrementSortOrder
+                                      }
+      updatePageContentState = [...updatePageContentState, updateMoveDownPageContent]
+    }
+    if (moveUpPageContent !== undefined) {
+      let updateMoveUpPageContent = { blockID: moveUpPageContent.blockID,
+                                      blockType: moveUpPageContent.blockType,
+                                      blockState: moveUpPageContent.blockState,
+                                      sortOrder: sortOrder
+                                    }
+      updatePageContentState = [...updatePageContentState, updateMoveUpPageContent]
+    }
+    dispatch(pageContentChanged(updatePageContentState.sort(function(a, b) { return a.sortOrder < b.sortOrder ? -1 : 1 })))
+  }
+
+  const moveDownBlock = () => {
+
+  }
+
   const deleteBlock = () => {
     let updatePageContentState: PageContentState[]
     updatePageContentState = pageContent.filter(content => content.blockID !== blockID)
@@ -20,8 +51,8 @@ const UpdateBlockStateIcons = ({ blockID, sortOrder }: UpdateBlockStateIconsProp
 
   return (
     <>
-      <a className='mr10 color-black none-under-decoration'><CaretUpSquare width={20} height={20} fill={'#5AFF19'}></CaretUpSquare>上に移動</a>
-      <a className='mr10 color-black none-under-decoration'><CaretDownSquare width={20} height={20} fill={'#5AFF19'}></CaretDownSquare>下に移動</a>
+      <a className='mr10 color-black none-under-decoration' onClick={moveUpBlock}><CaretUpSquare width={20} height={20} fill={'#5AFF19'}></CaretUpSquare>上に移動</a>
+      <a className='mr10 color-black none-under-decoration' onClick={moveDownBlock}><CaretDownSquare width={20} height={20} fill={'#5AFF19'}></CaretDownSquare>下に移動</a>
       <a className='color-black none-under-decoration' onClick={deleteBlock}><TrashIcon width={20} height={20} fill={'#ff0000'}></TrashIcon>ブロックを削除</a>
     </>
   )
