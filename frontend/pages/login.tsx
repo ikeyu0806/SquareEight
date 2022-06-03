2504d6e0f5348d97729c6402ba4f99178bb10d5c
ikeyu0806 <ikeyu0806@gmail.com>
2022-06-03 11:47:16 +0900

    ログイン、サインアップ画面のフォーム変更

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -9,33 +9,32 @@ const Login: NextPage = () => {
       <Container>
         <Row>
           <Col></Col>
-          <Col>
-            <Card>
-              <Card.Header>ログイン</Card.Header>
-              <Card.Body>
-                <Form>
-                  <Form.Group className='mb-3' controlId='formEmail'>
-                    <Form.Label>メールアドレス</Form.Label>
-                    <Form.Control type='email' placeholder='メールアドレス' />
-                    <Form.Text className='text-muted'></Form.Text>
-                  </Form.Group>
-
-                  <Form.Group className='mb-3' controlId='formPassword'>
-                    <Form.Label>パスワード</Form.Label>
-                    <Form.Control type='password' placeholder='パスワード' />
-                  </Form.Group>
-                  <Form.Group className='mb-3' controlId='formBasicCheckbox'>
-                    <Form.Check type='checkbox' label='プライバシーポリシーに同意しました' />
-                  </Form.Group>
-                  <div className='text-center'>
-                    <Button variant='primary' type='submit' href='/admin/dashboard'>
-                      送信
-                    </Button>
-                  </div>
-                </Form>
-              </Card.Body>
-            </Card>
-          </Col>
+            <Col>
+              <Card>
+                <Card.Header>ログイン</Card.Header>
+                <Card.Body>
+                  <Form>
+                    <Form.Group className='mb-3' controlId='formEmail'>
+                      <Form.Label>メールアドレス</Form.Label>
+                      <Form.Control type='email' placeholder='メールアドレス' />
+                      <Form.Text className='text-muted'></Form.Text>
+                    </Form.Group>
+                    <Form.Group className='mb-3' controlId='formPassword'>
+                      <Form.Label>パスワード</Form.Label>
+                      <Form.Control type='password' placeholder='パスワード' />
+                    </Form.Group>
+                    <Form.Group className='mb-3' controlId='formBasicCheckbox'>
+                      <Form.Check type='checkbox' label='プライバシーポリシーに同意しました' />
+                    </Form.Group>
+                    <div className='text-center'>
+                      <Button variant='primary' type='submit' href='/admin/dashboard'>
+                        送信
+                      </Button>
+                    </div>
+                  </Form>
+                </Card.Body>
+              </Card>
+            </Col>
           <Col></Col>
         </Row>
       </Container>
--- frontend/pages/signup.tsx
+++ frontend/pages/signup.tsx
@@ -15,18 +15,22 @@ const Signup: NextPage = () => {
               <Card.Body>
                 <Form>
                   <Form.Group className='mb-3' controlId='formEmail'>
+                      <Form.Label>ビジネス名称</Form.Label>
+                      <Form.Control type='text' placeholder='企業名、店舗名、教室名など' />
+                      <Form.Text className='text-muted'></Form.Text>
+                    </Form.Group>
+                  <Form.Group className='mb-3' controlId='formEmail'>
                     <Form.Label>メールアドレス</Form.Label>
-                    <Form.Control type='email' placeholder='メールアドレス' />
+                    <Form.Control type='email' placeholder='必須' />
                     <Form.Text className='text-muted'></Form.Text>
                   </Form.Group>
-
                   <Form.Group className='mb-3' controlId='formPassword'>
                     <Form.Label>パスワード</Form.Label>
-                    <Form.Control type='password' placeholder='パスワード' />
+                    <Form.Control type='password' placeholder='必須' />
                   </Form.Group>
                   <Form.Group className='mb-3' controlId='formPassword'>
                     <Form.Label>パスワード(確認)</Form.Label>
-                    <Form.Control type='password' placeholder='パスワード' />
+                    <Form.Control type='password' />
                   </Form.Group>
                   <Form.Group className='mb-3' controlId='formBasicCheckbox'>
                     <Form.Check type='checkbox' label='プライバシーポリシーに同意しました' />
