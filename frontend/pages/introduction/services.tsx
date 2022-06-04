98739ce89f05fe6ec8363c0af7b009b07b288b4d
ikeyu0806 <ikeyu0806@gmail.com>
2022-06-04 14:00:52 +0900

    サービス選択ページにアラート表示追加

--- frontend/pages/introduction/services.tsx
+++ frontend/pages/introduction/services.tsx
@@ -1,10 +1,23 @@
+import React, { useState, useEffect } from 'react'
 import type { NextPage } from 'next'
 import ServiceTemplate from '../../components/templates/ServiceTemplate'
 import IntroductionNavbar from '../../components/atoms/IntroductionNavbar'
+import { Alert } from 'react-bootstrap'
+import { useRouter } from 'next/router'
 
 const Service: NextPage = () => {
+  const [alertMessage, setAlertMessage] = useState('')
+  const router = useRouter()
+
+  useEffect(() => {
+    if (router.query.status === 'verify_code') {
+      setAlertMessage('ユーザ登録が完了しました。')
+    }
+  }, [router.query.status])
+
   return (
     <>
+      {alertMessage != '' && <Alert variant='success' onClose={() => setAlertMessage('')} dismissible>{alertMessage}</Alert>}
       <IntroductionNavbar />
       <ServiceTemplate/>
     </>
