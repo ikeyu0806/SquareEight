e6eb6553b333eebaa08985b625f7562f5c1c655f
ikeyu0806 <ikeyu0806@gmail.com>
2022-06-11 17:55:34 +0900

    一旦無理くりredisにsession書き込んでみる

--- rails_api/config/environments/development.rb
+++ rails_api/config/environments/development.rb
@@ -19,18 +19,16 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join("tmp/caching-dev.txt").exist?
+  # if Rails.root.join("tmp/caching-dev.txt").exist?
     # config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
-    }
-  else
+  #   config.public_file_server.headers = {
+  #     "Cache-Control" => "public, max-age=#{2.days.to_i}"
+  #   }
+  # else
     config.action_controller.perform_caching = false
 
     # config.cache_store = :null_store
-  end
-
-  config.cache_store = :redis_cache_store, { expires_in: 1.day, host: ENV["REDIS_HOST"], port: 6379 }
+  # end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
@@ -58,7 +56,8 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-
+  config.cache_store = :redis_cache_store, { expires_in: 1.day, host: ENV["REDIS_HOST"], port: 6379 }
+  config.session_store :redis_store, key: "smartlesson_session", expire_after: 1.day
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
