919f547c7aa53d34fd4fc742021544faf7eda8ee
ikeyu0806 <ikeyu0806@gmail.com>
2022-06-11 16:16:27 +0900

    railsのcache storeをredisに書き込めるところまで確認

--- rails_api/config/environments/development.rb
+++ rails_api/config/environments/development.rb
@@ -20,15 +20,17 @@ Rails.application.configure do
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
-    config.cache_store = :memory_store
+    # config.cache_store = :memory_store
     config.public_file_server.headers = {
       "Cache-Control" => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
 
-    config.cache_store = :null_store
+    # config.cache_store = :null_store
   end
+
+  config.cache_store = :redis_cache_store, { expires_in: 1.day, host: ENV["REDIS_HOST"], port: 6379 }
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
