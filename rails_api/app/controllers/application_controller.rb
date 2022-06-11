c92603834231a4567147f1716452a7f04391e738
ikeyu0806 <ikeyu0806@gmail.com>
2022-06-11 10:21:03 +0900

    current_userメソッド追加

--- rails_api/app/controllers/application_controller.rb
+++ rails_api/app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+
+  def current_user
+    session_hash = Rails.cache.fetch('_session_id:2::' + Digest::SHA256.hexdigest(request.headers["Session-Id"]))
+    raise "session not exists" if session_hash.blank?
+    user = User.find(session_hash["user_id"])
+    raise "current user not exists" if user.blank?
+    user
+  rescue => error
+    Rails.logger.error error
+  end
 end
