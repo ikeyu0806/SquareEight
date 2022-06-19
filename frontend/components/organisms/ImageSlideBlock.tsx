88348c0e595a06b7ca87187f5319dd6598813686
ikeyu0806 <ikeyu0806@gmail.com>
2022-06-19 15:29:38 +0900

    画像スライドブロックのコンポーネント化

---
+++ frontend/components/organisms/ImageSlideBlock.tsx
@@ -0,0 +1,30 @@
+import { ImageSlideState } from '../../interfaces/ImageSlideState'
+import { Carousel } from 'react-bootstrap'
+
+type propType = {
+  blockState: ImageSlideState
+}
+
+const ImageSlideBlock = (props: propType): JSX.Element => {
+  return (
+    <Carousel>
+      {props.blockState.imageSlide != undefined && props.blockState.imageSlide.map((slide, i) => {
+        return (
+          <Carousel.Item key={i}>
+            <img
+              className='d-block w-100'
+              src={slide.image}
+              alt={slide + String(i)}
+            />
+            <Carousel.Caption>
+              <h3>{slide.title}</h3>
+              <p>{slide.text}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        )
+      })}
+    </Carousel>
+  )
+}
+
+export default ImageSlideBlock
