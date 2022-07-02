07192cc9f9e05179d987f79ea50a61426e9e2c75
ikeyu0806 <ikeyu0806@gmail.com>
2022-07-02 17:18:50 +0900

    import redux state in create resource component

--- frontend/components/organisms/EditBusinessHours.tsx
+++ frontend/components/organisms/EditBusinessHours.tsx
@@ -1,7 +1,45 @@
 import React from 'react'
-import { Container, Table, Row, Col, Form } from 'react-bootstrap'
+import { Table, Col, Form } from 'react-bootstrap'
+import { useSelector, useDispatch } from 'react-redux'
+import { RootState } from 'redux/store'
+import { nameChanged, priceChanged, receptionTimeSettingChanged } from 'redux/resourceSlice'
+import { monStartChanged,
+         monEndChanged,
+         tueStartChanged,
+         tueEndChanged,
+         wedStartChanged,
+         wedEndChanged,
+         thuStartChanged,
+         thuEndChanged,
+         friStartChanged,
+         friEndChanged,
+         satStartChanged,
+         satEndChanged,
+         sunStartChanged,
+         sunEndChanged,
+         holidayStartChanged,
+         holidayEndChanged, } from 'redux/businessHourSlice'
 
 const EditBusinessHours = (): JSX.Element => {
+  const name = useSelector((state: RootState) => state.resource.name)
+  const price = useSelector((state: RootState) => state.resource.price)
+  const receptionTimeSetting = useSelector((state: RootState) => state.resource.receptionTimeSetting)
+  const monStart = useSelector((state: RootState) => state.businessHour.monStart)
+  const monEnd = useSelector((state: RootState) => state.businessHour.monEnd)
+  const tueStart = useSelector((state: RootState) => state.businessHour.tueStart)
+  const tueEnd = useSelector((state: RootState) => state.businessHour.tueEnd)
+  const wedStart = useSelector((state: RootState) => state.businessHour.wedStart)
+  const wedEnd = useSelector((state: RootState) => state.businessHour.wedEnd)
+  const thuStart = useSelector((state: RootState) => state.businessHour.thuStart)
+  const thuEnd = useSelector((state: RootState) => state.businessHour.thuEnd)
+  const friStart = useSelector((state: RootState) => state.businessHour.friStart)
+  const friEnd = useSelector((state: RootState) => state.businessHour.friEnd)
+  const satStart = useSelector((state: RootState) => state.businessHour.satStart)
+  const satEnd = useSelector((state: RootState) => state.businessHour.satEnd)
+  const sunStart = useSelector((state: RootState) => state.businessHour.sunStart)
+  const sunEnd = useSelector((state: RootState) => state.businessHour.sunEnd)
+  const holidayStart = useSelector((state: RootState) => state.businessHour.holidayStart)
+  const holidayEnd = useSelector((state: RootState) => state.businessHour.holidayEnd)
 
   return( 
     <>
@@ -18,79 +56,79 @@ const EditBusinessHours = (): JSX.Element => {
           <tr>
             <td rowSpan={2} className='text-center'>日曜日</td>
             <td className='text-center'>営業時間</td>
-            <td><Form.Control type='time' defaultValue={'10:00'} /></td>
-            <td><Form.Control type='time' /></td>
+            <td><Form.Control type='time' defaultValue={monStart} /></td>
+            <td><Form.Control type='time'defaultValue={monEnd} /></td>
           </tr>
           <tr>
             <td className='text-center'>休憩時間</td>
-            <td><Form.Control type='time' /></td>
-            <td><Form.Control type='time' /></td>
+            <td><Form.Control type='time' defaultValue={monStart} /></td>
+            <td><Form.Control type='time'defaultValue={monEnd} /></td>
           </tr>
           <tr>
             <td rowSpan={2} className='text-center'>月曜日</td>
             <td className='text-center'>営業時間</td>
-            <td><Form.Control type='time' /></td>
-            <td><Form.Control type='time' /></td>
+            <td><Form.Control type='time' defaultValue={tueStart} /></td>
+            <td><Form.Control type='time'defaultValue={tueEnd} /></td>
           </tr>
           <tr>
             <td className='text-center'>休憩時間</td>
-            <td><Form.Control type='time' /></td>
-            <td><Form.Control type='time' /></td>
+            <td><Form.Control type='time' defaultValue={monStart} /></td>
+            <td><Form.Control type='time'defaultValue={monEnd} /></td>
           </tr>
           <tr>
             <td rowSpan={2} className='text-center'>火曜日</td>
             <td className='text-center'>営業時間</td>
-            <td><Form.Control type='time' /></td>
-            <td><Form.Control type='time' /></td>
+            <td><Form.Control type='time' defaultValue={monStart} /></td>
+            <td><Form.Control type='time'defaultValue={monEnd} /></td>
           </tr>
           <tr>
             <td className='text-center'>休憩時間</td>
-            <td><Form.Control type='time' /></td>
-            <td><Form.Control type='time' /></td>
+            <td><Form.Control type='time' defaultValue={monStart} /></td>
+            <td><Form.Control type='time'defaultValue={monEnd} /></td>
           </tr>
           <tr>
             <td rowSpan={2} className='text-center'>水曜日</td>
             <td className='text-center'>営業時間</td>
-            <td><Form.Control type='time' /></td>
-            <td><Form.Control type='time' /></td>
+            <td><Form.Control type='time' defaultValue={monStart} /></td>
+            <td><Form.Control type='time'defaultValue={monEnd} /></td>
           </tr>
           <tr>
             <td className='text-center'>休憩時間</td>
-            <td><Form.Control type='time' /></td>
-            <td><Form.Control type='time' /></td>
+            <td><Form.Control type='time' defaultValue={monStart} /></td>
+            <td><Form.Control type='time'defaultValue={monEnd} /></td>
           </tr>
           <tr>
             <td rowSpan={2} className='text-center'>木曜日</td>
             <td className='text-center'>営業時間</td>
-            <td><Form.Control type='time' /></td>
-            <td><Form.Control type='time' /></td>
+            <td><Form.Control type='time' defaultValue={monStart} /></td>
+            <td><Form.Control type='time'defaultValue={monEnd} /></td>
           </tr>
           <tr>
             <td className='text-center'>休憩時間</td>
-            <td><Form.Control type='time' /></td>
-            <td><Form.Control type='time' /></td>
+            <td><Form.Control type='time' defaultValue={monStart} /></td>
+            <td><Form.Control type='time'defaultValue={monEnd} /></td>
           </tr>
           <tr>
             <td rowSpan={2} className='text-center'>金曜日</td>
             <td className='text-center'>営業時間</td>
-            <td><Form.Control type='time' /></td>
-            <td><Form.Control type='time' /></td>
+            <td><Form.Control type='time' defaultValue={monStart} /></td>
+            <td><Form.Control type='time'defaultValue={monEnd} /></td>
           </tr>
           <tr>
             <td className='text-center'>休憩時間</td>
-            <td><Form.Control type='time' /></td>
-            <td><Form.Control type='time' /></td>
+            <td><Form.Control type='time' defaultValue={monStart} /></td>
+            <td><Form.Control type='time'defaultValue={monEnd} /></td>
           </tr>
           <tr>
             <td rowSpan={2} className='text-center'>土曜日</td>
             <td className='text-center'>営業時間</td>
-            <td><Form.Control type='time' /></td>
-            <td><Form.Control type='time' /></td>
+            <td><Form.Control type='time' defaultValue={monStart} /></td>
+            <td><Form.Control type='time'defaultValue={monEnd} /></td>
           </tr>
           <tr>
             <td className='text-center'>休憩時間</td>
-            <td><Form.Control type='time' /></td>
-            <td><Form.Control type='time' /></td>
+            <td><Form.Control type='time' defaultValue={monStart} /></td>
+            <td><Form.Control type='time'defaultValue={monEnd} /></td>
           </tr>
         </tbody>
       </Table>
