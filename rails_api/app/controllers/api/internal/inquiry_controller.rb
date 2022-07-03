3852e55c5c8e0afa475577a4ea869f3975d1c5fa
ikeyu0806 <ikeyu0806@gmail.com>
2022-07-03 17:31:53 +0900

    fix not send inquiry bug

--- rails_api/app/controllers/api/internal/inquiry_controller.rb
+++ rails_api/app/controllers/api/internal/inquiry_controller.rb
@@ -1,6 +1,6 @@
 class Api::Internal::InquiryController < ApplicationController
   def send_mail_to_admin
-    InquiryMailer.send_mail_to_admin(inquiry_params[:email], inquiry_params[:content])
+    InquiryMailer.send_mail_to_admin(inquiry_params[:email], inquiry_params[:content]).deliver_later
     render json: { status: 'success' }, states: 200
   rescue => error
     render json: { statue: 'fail', error: error }, status: 500
