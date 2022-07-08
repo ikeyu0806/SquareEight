3a0b655c890184ac88faa30ba0a6c8dd3cf1a3a9
ikeyu0806 <ikeyu0806@gmail.com>
2022-07-08 09:32:21 +0900

    add ticket consume number

--- rails_api/db/migrate/20220708001223_create_reserve_frame_ticket_masters.rb
+++ rails_api/db/migrate/20220708001223_create_reserve_frame_ticket_masters.rb
@@ -3,6 +3,7 @@ class CreateReserveFrameTicketMasters < ActiveRecord::Migration[7.0]
     create_table :reserve_frame_ticket_masters do |t|
       t.integer :reserve_frame_id
       t.integer :ticket_master_id
+      t.integer :consume_number
       t.timestamps
     end
   end
