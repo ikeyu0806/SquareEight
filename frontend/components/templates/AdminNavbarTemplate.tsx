0b6978adbb3247bf99dfc876e14307f24fdff5b3
ikeyu0806 <ikeyu0806@gmail.com>
2022-07-09 19:24:15 +0900

    add depends option to docker-compose

--- frontend/components/templates/AdminNavbarTemplate.tsx
+++ frontend/components/templates/AdminNavbarTemplate.tsx
@@ -27,7 +27,7 @@ const AdminNavbarTemplate = (): JSX.Element => {
     <>
       <Navbar bg='dark'  variant='dark' expand='lg'>
         <Container>
-          <Navbar.Brand href='#home'>GYBuilder Admin</Navbar.Brand>
+          <Navbar.Brand href='#home'>GYBuilder</Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
             <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='me-auto'>
