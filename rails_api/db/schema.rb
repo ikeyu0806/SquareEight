983f009031a186b0f1534fcd9339660953877bf0
ikeyu0806 <ikeyu0806@gmail.com>
2022-07-17 18:41:37 +0900

    add desctiption and image columns

--- rails_api/db/schema.rb
+++ rails_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_17_081939) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_17_093852) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -145,6 +145,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_17_081939) do
     t.datetime "updated_at", null: false
     t.integer "account_id"
     t.text "description"
+    t.string "s3_object_public_url"
   end
 
   create_table "reserve_frame_monthly_payment_plans", force: :cascade do |t|
@@ -197,6 +198,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_17_081939) do
     t.boolean "is_monthly_plan_payment_enable"
     t.integer "creadit_card_payment_price"
     t.boolean "is_creadit_card_payment_enable"
+    t.string "s3_object_public_url"
   end
 
   create_table "resources", force: :cascade do |t|
@@ -247,6 +249,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_17_081939) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "s3_object_public_url"
+    t.text "description"
   end
 
   create_table "unreservable_frames", force: :cascade do |t|
