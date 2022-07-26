6cf9d6addb6db83d66ac6ef07990b054bc3bbbf7
ikeyu0806 <ikeyu0806@gmail.com>
2022-07-26 09:46:14 +0900

    add name_kana to end_user

--- frontend/interfaces/EndUserParam.tsx
+++ frontend/interfaces/EndUserParam.tsx
@@ -1,6 +1,8 @@
 export interface EndUserParam {
   first_name: string
   last_name: string
+  first_name_kana: string
+  last_name_kana: string
   email: string
   google_auth_id: string
   google_auth_email: string
