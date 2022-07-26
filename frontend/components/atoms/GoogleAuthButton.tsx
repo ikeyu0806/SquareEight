0674167ae67af06820da80764cfd7769fee485ca
ikeyu0806 <ikeyu0806@gmail.com>
2022-07-26 11:55:53 +0900

    add icon to google login button

--- frontend/components/atoms/GoogleAuthButton.tsx
+++ frontend/components/atoms/GoogleAuthButton.tsx
@@ -1,3 +1,4 @@
+import GoogleIcon from 'components/atoms/GoogleIcon'
 interface Props {
   buttonText: string
 }
@@ -5,9 +6,9 @@ interface Props {
 const GoogleAuthButton = ({buttonText}: Props): JSX.Element => {
   return (
     <div className='mt20 text-center'>
-      <a className='btn btn-primary'
+      <a className='btn btn-outline-primary'
           href={`https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=${process.env.GOOGLE_AUTH_END_USER_CLIENT_ID}&redirect_uri=${process.env.GOOGLE_AUTH_END_USER_REDIRECT_URL}&scope=email&access_type=offline&approval_prompt=force`}>
-        {buttonText}
+        <GoogleIcon width={20} height={20} className={'mr10'}></GoogleIcon>{buttonText}
       </a>
     </div>
   )
