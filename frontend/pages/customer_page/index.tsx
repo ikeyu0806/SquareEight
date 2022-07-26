a43dcf5d8107c2cb0ce56f72b9ccdd71212956e6
ikeyu0806 <ikeyu0806@gmail.com>
2022-07-26 09:14:42 +0900

    add EnvelopIcon to EndUser mypage

--- frontend/pages/customer_page/index.tsx
+++ frontend/pages/customer_page/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import { Container, Card, Row, Col } from 'react-bootstrap'
 import EndUserLoginLayout from 'components/templates/EndUserLoginLayout'
 import GoogleIcon from 'components/atoms/GoogleIcon'
+import EnvelopIcon from 'components/atoms/EnvelopIcon'
 import ConnectedTextWithIcon from 'components/molecules/ConnectedTextWithIcon'
 
 const Index: NextPage = () => {
@@ -10,16 +11,20 @@ const Index: NextPage = () => {
       <EndUserLoginLayout>
         <Container className='mt50 mb50'>
           <Row>
-            <Col lg={4} md={3}></Col>
-              <Col lg={4}>
+            <Col lg={3} md={3}></Col>
+              <Col lg={6}>
                 <Card>
                   <Card.Header>ご登録情報</Card.Header>
                   <Card.Body>
                     <table className='table'>
                       <tbody>
                         <tr>
-                          <td scope='row'>メールアドレス</td>
+                          <td scope='row'>メールアドレス（パスワード認証）</td>
                           <td className='text-center'>sample.com</td>
+                        </tr>
+                        <tr>
+                          <td scope='row'><EnvelopIcon width={20} height={20} className={'mr10'}></EnvelopIcon>メールアドレス認証</td>
+                          <td className='text-center'>{true ? <ConnectedTextWithIcon></ConnectedTextWithIcon> : '連携なし'}</td>
                         </tr>
                         <tr>
                           <td scope='row'><GoogleIcon width={20} height={20} className={'mr10'}></GoogleIcon>Google</td>
