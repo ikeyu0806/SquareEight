ce87dc8998cf006cbef22f9019f7bdaef75fc2a0
ikeyu0806 <ikeyu0806@gmail.com>
2022-08-05 21:59:20 +0900

    implement business notification index

--- rails_api/config/routes.rb
+++ rails_api/config/routes.rb
@@ -77,9 +77,9 @@ Rails.application.routes.draw do
       post 'reserve_frames', to: 'reserve_frames#create'
       get 'orders/:order_id/order_items', to: 'orders#order_items'
       get 'calendar/:reserve_frame_id/monthly_reserve_frames', to: 'calendar#monthly_reserve_frames'
-      get 'account_notifications/monthly_reserve_frames', to: 'account_notifications#index'
+      get 'account_notifications', to: 'account_notifications#index'
       post 'account_notifications', to: 'account_notifications#create'
-      get 'end_user_notifications/monthly_reserve_frames', to: 'end_user_notifications#index'
+      get 'end_user_notifications', to: 'end_user_notifications#index'
       post 'end_user_notifications', to: 'end_user_notifications#create'
       namespace :merchant do
         get 'sessions', to: 'sessions#login_status'
