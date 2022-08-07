d45c69d03bea121fff3c45bba8faeb95f1687105
ikeyu0806 <ikeyu0806@gmail.com>
2022-08-07 15:10:45 +0900

    add contents to product purchase page

--- frontend/pages/product/[id]/purchase.tsx
+++ frontend/pages/product/[id]/purchase.tsx
@@ -20,15 +20,22 @@ import { nameChanged,
          s3ObjectPublicUrlChanged } from 'redux/productSlice'
 
 const Purchase: NextPage = () => {
+  const dispatch = useDispatch()
+  const router = useRouter()
   const [cookies] = useCookies(['_gybuilder_end_user_session'])
+  const name = useSelector((state: RootState) => state.product.name)
+  const price = useSelector((state: RootState) => state.product.price)
+  const taxRate = useSelector((state: RootState) => state.product.taxRate)
+  const inventory = useSelector((state: RootState) => state.product.inventory)
+  const description = useSelector((state: RootState) => state.product.description)
+  const s3ObjectPublicUrl = useSelector((state: RootState) => state.product.s3ObjectPublicUrl)
+  const productTypes = useSelector((state: RootState) => state.product.productTypes)
   const currentEndUserLogintStatus = useSelector((state: RootState) => state.currentEndUser.loginStatus)
   const defaultPaymentMethodId = useSelector((state: RootState) => state.currentEndUser.defaultPaymentMethodId)
   const paymentMethods = useSelector((state: RootState) => state.currentEndUser.paymentMethods)
-  const dispatch = useDispatch()
-  const router = useRouter()
 
   useEffect(() => {
-    const fetchTicketMaster = () => {
+    const fetchProduct = () => {
       axios.get(
         `${process.env.BACKEND_URL}/api/internal/products/${router.query.id}/purchase_info`, {
           headers: {
@@ -53,12 +60,116 @@ const Purchase: NextPage = () => {
         console.log(error)
       })
     }
-    fetchTicketMaster()
+    fetchProduct()
   }, [cookies._gybuilder_end_user_session, router.query.id, router.query.ticket_master_id, dispatch])
+
+  const execPurchase = () => {
+    axios.post(`${process.env.BACKEND_URL}/api/internal/products/purchase`,
+    {
+      ticket_master: {
+        id: router.query.id
+      },
+    },
+    {
+      headers: { 
+        'Session-Id': cookies._gybuilder_end_user_session
+      }
+    }).then(response => {
+      router.push(`/purchase/${response.data.order_id}/payment_complete`)
+    }).catch(error => {
+      dispatch(alertChanged({message: error, show: true, type: 'danger'}))
+    })
+  }
+
+  const updateDefaultCard = (payment_method_id: string) => {
+    swalWithBootstrapButtons.fire({
+      title: 'お支払いカードを更新します',
+      text: '更新してもよろしいですか？',
+      icon: 'question',
+      confirmButtonText: '更新する',
+      cancelButtonText: 'キャンセル',
+      showCancelButton: true,
+      showCloseButton: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axios.post(`${process.env.BACKEND_URL}/api/internal/end_users/${payment_method_id}/update_payment_method`,
+        {},
+        {
+          headers: {
+            'Session-Id': cookies._gybuilder_end_user_session
+          }
+        }).then(response => {
+          dispatch(alertChanged({message: 'お支払いカードを変更しました', show: true}))
+          location.reload()
+        }).catch(error => {
+          dispatch(alertChanged({message: "登録失敗しました", show: true, type: 'danger'}))
+        })
+      }
+    })
+  }
 
   return (
     <MerchantCustomLayout>
-
+      <Container>
+        <Row>
+          <Col lg={3} md={3}></Col>
+          <Col lg={6} md={6}>
+            <Card>
+              <Card.Header>商品購入</Card.Header>
+              <Card.Body>
+                {currentEndUserLogintStatus === 'Logout'
+                  ? 
+                    <>
+                      <div></div>
+                      <a href='/customer/login'>カスタマーアカウントでログインしてください</a><br/>
+                      <div className='mt20'>購入にはアカウント登録とクレジットカード登録が必要になります</div>
+                      <div className='mt40'></div>
+                    </>
+                  : <></>
+                }
+                <h3>{name}</h3>
+                <div className='mt10'>{price}円（税込）</div>
+                <div className='mt10'>税率{taxRate}%</div>
+                <div className='mt10'>{description}</div>
+                {s3ObjectPublicUrl
+                && <img
+                    className='d-block w-100 mt30 mb30'
+                    src={s3ObjectPublicUrl}
+                    alt='image' />}
+                {currentEndUserLogintStatus === 'Logout'
+                ?
+                  <></>
+                :
+                  <>
+                  <h4>お支払い方法</h4>
+                  {<ListGroup>
+                      {paymentMethods?.map((pay, i) => {
+                        return (
+                          <ListGroup.Item key={i}>
+                            {pay.card.brand}（************{pay.card.last4} / 有効期限 {pay.card.exp_month} / {pay.card.exp_year}
+                            {defaultPaymentMethodId === pay.id && <><br/><span className='badge bg-info'>お支払いカードに設定されています</span></>}
+                            {defaultPaymentMethodId !== pay.id
+                              &&
+                                <>
+                                  <br/>
+                                  <Button size='sm' onClick={() => updateDefaultCard(pay.id)}>お支払いカードに設定する</Button>
+                                </>}
+                          </ListGroup.Item>
+                        )
+                      })}
+                    </ListGroup>
+                    }
+                  <div className='mt30 '>
+                    <Button onClick={() => execPurchase()}>すぐに購入する</Button>
+                    <a className='btn btn-secondary ml20' href={`/ticket/${router.query.ticket_master_id}/payment`}>カートに入れる</a>
+                  </div>
+                  </>
+                }
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </MerchantCustomLayout>
   )
 }
