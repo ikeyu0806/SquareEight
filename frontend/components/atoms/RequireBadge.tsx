66d6141b9ed96c4e904ed7a0dac0c3e4709e611e
ikeyu0806 <ikeyu0806@gmail.com>
2022-08-08 12:18:56 +0900

    create RequireBadge

---
+++ frontend/components/atoms/RequireBadge.tsx
@@ -0,0 +1,7 @@
+const RequireBadge = (): JSX.Element => {
+  return (
+    <span className='badge bg-secondary ml10'>必須</span>
+  )
+}
+
+export default RequireBadge
--- frontend/components/templates/CreateDeliveryTarget.tsx
+++ frontend/components/templates/CreateDeliveryTarget.tsx
@@ -1,6 +1,7 @@
 import { Container, Row, Col, Form } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from 'redux/store'
+import RequireBadge from 'components/atoms/RequireBadge'
 import {  lastNameChanged,
           firstNameChanged,
           postalCodeChanged,
@@ -31,35 +32,35 @@ const CreateDeliveryTarget = (): JSX.Element => {
         <Row>
           <Col md={2} lg={3}></Col>
           <Col md={8} lg={6}>
-            <Form.Label>名前（性）</Form.Label>
+            <Form.Label>名前（性）<RequireBadge /></Form.Label>
             <Form.Control
               onChange={(e) => dispatch(lastNameChanged(e.target.value))}
               value={lastName}></Form.Control>
-            <Form.Label>名前（名）</Form.Label>
+            <Form.Label>名前（名）<RequireBadge /></Form.Label>
             <Form.Control
               onChange={(e) => dispatch(firstNameChanged(e.target.value))}
               value={firstName}></Form.Control>
-            <Form.Label>携帯電話番号</Form.Label>
+            <Form.Label>携帯電話番号<RequireBadge /></Form.Label>
             <Form.Control
               onChange={(e) => dispatch(phoneNumberChanged(e.target.value))}
               value={phoneNumber}></Form.Control>
-            <Form.Label className='mt10'>郵便番号</Form.Label>
+            <Form.Label className='mt10'>郵便番号<RequireBadge /></Form.Label>
             <Form.Control
               onChange={(e) => dispatch(postalCodeChanged(e.target.value))}
               value={postalCode}></Form.Control>
-            <Form.Label className='mt10'>都道府県</Form.Label>
+            <Form.Label className='mt10'>都道府県<RequireBadge /></Form.Label>
             <Form.Control
               onChange={(e) => dispatch(stateChanged(e.target.value))}
               value={state}></Form.Control>
-            <Form.Label className='mt10'>区市町村</Form.Label>
+            <Form.Label className='mt10'>区市町村<RequireBadge /></Form.Label>
             <Form.Control
               onChange={(e) => dispatch(cityChanged(e.target.value))}
               value={city}></Form.Control>
-            <Form.Label className='mt10'>町名</Form.Label>
+            <Form.Label className='mt10'>町名<RequireBadge /></Form.Label>
             <Form.Control
               onChange={(e) => dispatch(townChanged(e.target.value))}
               value={town}></Form.Control>
-            <Form.Label className='mt10'>番地、号</Form.Label>
+            <Form.Label className='mt10'>番地、号<RequireBadge /></Form.Label>
             <Form.Control
               onChange={(e) => dispatch(line1Changed(e.target.value))}
               value={line1}></Form.Control>
@@ -67,6 +68,12 @@ const CreateDeliveryTarget = (): JSX.Element => {
             <Form.Control
               onChange={(e) => dispatch(line2Changed(e.target.value))}
               value={line2}></Form.Control>
+            <Form.Check
+              className='mt20'
+              checked={isDefault}
+              onChange={() => dispatch(isDefaultChanged(!isDefault))}
+              type='switch'
+              label='デフォルトのお届け先に設定する'></Form.Check>
           </Col>
         </Row>
       </Container>
