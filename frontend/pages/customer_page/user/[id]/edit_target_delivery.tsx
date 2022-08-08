16ef46f8f7eb9791a617e51aadae91ab6e1c13ab
ikeyu0806 <ikeyu0806@gmail.com>
2022-08-08 11:34:37 +0900

    create CreateDeliveryTarget target

--- frontend/pages/customer_page/user/[id]/edit_target_delivery.tsx
+++ frontend/pages/customer_page/user/[id]/edit_target_delivery.tsx
@@ -1,23 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import { NextPage } from 'next'
-import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 import EndUserLoginLayout from 'components/templates/EndUserLoginLayout'
+import CreateDeliveryTarget from 'components/templates/CreateDeliveryTarget'
 import { alertChanged } from 'redux/alertSlice'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from 'redux/store'
 import { useCookies } from 'react-cookie'
 import { useRouter } from 'next/router'
 import axios from 'axios'
-import { lastNameChanged,
-         firstNameChanged,
-         postalCodeChanged,
-         stateChanged,
-         cityChanged,
-         townChanged,
-         line1Changed,
-         line2Changed,
-         phoneNumberChanged,
-         isDefaultChanged } from 'redux/deliveryTargetSlice'
 
 const EditTargetDelivery: NextPage = () => {
   const [cookies] = useCookies(['_gybuilder_end_user_session'])
@@ -66,52 +57,10 @@ const EditTargetDelivery: NextPage = () => {
 
   return (
     <EndUserLoginLayout>
-      <Container className='mt30'>
-        <Row>
-          <Col md={2} lg={3}></Col>
-          <Col md={8} lg={6}>
-            <Form.Label>名前（性）</Form.Label>
-            <Form.Control
-              onChange={(e) => dispatch(lastNameChanged(e.target.value))}
-              value={lastName}></Form.Control>
-            <Form.Label>名前（名）</Form.Label>
-            <Form.Control
-              onChange={(e) => dispatch(firstNameChanged(e.target.value))}
-              value={firstName}></Form.Control>
-            <Form.Label>携帯電話番号</Form.Label>
-            <Form.Control
-              onChange={(e) => dispatch(phoneNumberChanged(e.target.value))}
-              value={phoneNumber}></Form.Control>
-            <Form.Label className='mt10'>郵便番号</Form.Label>
-            <Form.Control
-              onChange={(e) => dispatch(postalCodeChanged(e.target.value))}
-              value={postalCode}></Form.Control>
-            <Form.Label className='mt10'>都道府県</Form.Label>
-            <Form.Control
-              onChange={(e) => dispatch(stateChanged(e.target.value))}
-              value={state}></Form.Control>
-            <Form.Label className='mt10'>区市町村</Form.Label>
-            <Form.Control
-              onChange={(e) => dispatch(cityChanged(e.target.value))}
-              value={city}></Form.Control>
-            <Form.Label className='mt10'>町名</Form.Label>
-            <Form.Control
-              onChange={(e) => dispatch(townChanged(e.target.value))}
-              value={town}></Form.Control>
-            <Form.Label className='mt10'>番地、号</Form.Label>
-            <Form.Control
-              onChange={(e) => dispatch(line1Changed(e.target.value))}
-              value={line1}></Form.Control>
-            <Form.Label className='mt10'>建物・部屋番号・その他</Form.Label>
-            <Form.Control
-              onChange={(e) => dispatch(line2Changed(e.target.value))}
-              value={line2}></Form.Control>
-            <div className='mt30 text-center'>
-              <Button onClick={onSubmit}>更新する</Button>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+      <CreateDeliveryTarget></CreateDeliveryTarget>
+      <div className='mt30 text-center'>
+        <Button onClick={onSubmit}>更新する</Button>
+      </div>
     </EndUserLoginLayout>
   )
 }
