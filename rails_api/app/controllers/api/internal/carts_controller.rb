4f49707d28fa7acb8175ec163c53f0ed58427bef
ikeyu0806 <ikeyu0806@gmail.com>
2022-08-09 22:40:26 +0900

    implement product cart purchase action

--- rails_api/app/controllers/api/internal/carts_controller.rb
+++ rails_api/app/controllers/api/internal/carts_controller.rb
@@ -8,6 +8,19 @@ class Api::Internal::CartsController < ApplicationController
     render json: { statue: 'fail', error: error }, status: 500
   end
 
+  def purchase
+    ActiveRecord::Base.transaction do
+      # 商品購入
+      current_end_user.cart_products.each do |cart|
+        product = cart.product
+        product.stripe_purchase(current_end_user, product_params[:purchase_quantities])
+      end
+      render json: { status: 'success', cart_items: cart_items }, states: 200
+    end
+  rescue => error
+    render json: { statue: 'fail', error: error }, status: 500
+  end
+
   private
 
   def cart_params
--- rails_api/config/routes.rb
+++ rails_api/config/routes.rb
@@ -102,6 +102,7 @@ Rails.application.routes.draw do
       post 'end_user_notifications/:id', to: 'end_user_notifications#update'
       post 'end_user_notifications', to: 'end_user_notifications#create'
       get 'carts/:account_id/account_index', to: 'carts#account_index'
+      post 'carts/purchase', to: 'carts#purchase'
       namespace :merchant do
         get 'sessions', to: 'sessions#login_status'
         post 'sessions/create_by_google_auth', to: 'sessions#create_by_google_auth'
