08d0999564ee5c9e68f93cbdf0b1354361a2da12
ikeyu0806 <ikeyu0806@gmail.com>
2022-08-10 10:11:39 +0900

    merge ticket, monthly_payment_plan cart_items

--- rails_api/app/models/end_user.rb
+++ rails_api/app/models/end_user.rb
@@ -27,7 +27,7 @@ class EndUser < ApplicationRecord
 
   def search_stripe_payment_intents
     Stripe.api_key = Rails.configuration.stripe[:secret_key]
-    Stripe.api_version = '2020-08-27'
+    Stripe.api_version = '2022-08-01'
     query = "customer:" + "\'" + self.stripe_customer_id + "\'"
     result = Stripe::PaymentIntent.search({query: query, limit: 100})
     JSON.parse(result.to_json)["data"]
@@ -71,31 +71,46 @@ class EndUser < ApplicationRecord
       end
     end
     cart_ticket_masters.each do |cart|
-      cart_items.push({
-        id: cart.id,
-        product_name: cart.ticket_master.name,
-        price: cart.ticket_master.price * cart.quantity,
-        effective_month: cart.ticket_master.effective_month,
-        quantity: cart.quantity,
-        s3_object_public_url: cart.ticket_master.s3_object_public_url,
-        is_expired: cart.ticket_master.effective_month.positive? ? false : true,
-        business_name: cart.account.business_name,
-        product_type: 'TicketMaster',
-        parent_ticket_master_id: cart.ticket_master.id
-      })
-      total_price += cart.ticket_master.price
+      merge_destination_item = cart_items.find{|item| item[:parent_ticket_master_id] == cart.ticket_master.id}
+      price = cart.ticket_master.price * cart.quantity
+      if merge_destination_item.blank?
+        cart_items.push({
+          id: cart.id,
+          product_name: cart.ticket_master.name,
+          price: price,
+          effective_month: cart.ticket_master.effective_month,
+          quantity: cart.quantity,
+          s3_object_public_url: cart.ticket_master.s3_object_public_url,
+          is_expired: cart.ticket_master.effective_month.positive? ? false : true,
+          business_name: cart.account.business_name,
+          product_type: 'TicketMaster',
+          parent_ticket_master_id: cart.ticket_master.id
+        })
+        total_price += price
+      else
+        merge_destination_item[:price] = merge_destination_item[:price] += price
+        merge_destination_item[:quantity] = merge_destination_item[:quantity] += cart.quantity
+        total_price += price
+      end
     end
+
     cart_monthly_payment_plans.each do |cart|
-      cart_items.push({
-        id: cart.id,
-        product_name: cart.monthly_payment_plan.plan_text,
-        price: cart.monthly_payment_plan.price,
-        s3_object_public_url: cart.monthly_payment_plan.s3_object_public_url,
-        business_name: cart.account.business_name,
-        product_type: 'MonthlyPaymentPlan',
-        parent_monthly_payment_plan_id: cart.monthly_payment_plan.id
-      })
-      total_price += cart.monthly_payment_plan.price
+      merge_destination_item = cart_items.find{|item| item[:parent_monthly_payment_plan_id] == cart.monthly_payment_plan.id}
+      price = cart.monthly_payment_plan.price
+      if merge_destination_item.blank?
+        cart_items.push({
+          id: cart.id,
+          product_name: cart.monthly_payment_plan.plan_text,
+          price: cart.monthly_payment_plan.price,
+          s3_object_public_url: cart.monthly_payment_plan.s3_object_public_url,
+          business_name: cart.account.business_name,
+          product_type: 'MonthlyPaymentPlan',
+          parent_monthly_payment_plan_id: cart.monthly_payment_plan.id
+        })
+        total_price += cart.monthly_payment_plan.price
+      else
+        merge_destination_item[:price] = merge_destination_item[:price] += price
+      end
     end
     return cart_items, total_price
   end
