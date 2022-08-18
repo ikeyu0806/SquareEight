2460ba8f07af4067f3c3067a1f885e72ad0d8c9e
ikeyu0806 <ikeyu0806@gmail.com>
2022-08-18 10:21:02 +0900

    delete unused calendar method

--- rails_api/app/models/account.rb
+++ rails_api/app/models/account.rb
@@ -11,48 +11,6 @@ class Account < ApplicationRecord
   has_many :reserve_frames
   has_many :orders
 
-  def reserve_calendar_json
-    result = []
-    reserve_frames.each do |frame|
-      if frame.is_repeat
-        case frame.repeat_interval_type
-        when 'Day' then
-          (Date.parse(frame.start_at.to_s)..(Date.parse(frame.repeat_end_date.to_s))).each do |date|
-            result << {
-              start: date,
-              title: frame.title,
-              url: '/reserve/' + frame.id.to_s + '&date=' + date.strftime("%Y-%m-%d")
-            }
-          end
-        when 'Week' then
-          (Date.parse(frame.start_at.to_s)..Date.parse(frame.repeat_end_date.to_s)).select{|d| d.wday == frame.start_at.wday}.each do |date|
-            result << {
-              start: date,
-              title: frame.title,
-              url: '/reserve/' + frame.id.to_s + '&date=' + date.strftime("%Y-%m-%d")
-            }
-          end
-        when 'Month' then
-          (Date.parse(frame.start_at.to_s)..Date.parse(frame.repeat_end_date.to_s)).select{|d| d.day == repeat.repeat_interval_month_date}.each do |date|
-            result << {
-              start: date,
-              title: frame.title,
-              url: '/reserve/' + frame.id.to_s + '&date=' + date.strftime("%Y-%m-%d")
-            }
-          end
-        else
-        end
-      else
-        result << {
-          start: frame.start_at,
-          title: frame.title,
-          url: '/reserve/' + frame.id.to_s + '&date=' + frame.start_at.strftime("%Y-%m-%d")
-        }
-      end
-    end
-    result
-  end
-
   def page_links
     # 作成したWebページ、予約ページ、回数券購入ページ、月額課金プラン加入ページのリンクを返却
     result = []
