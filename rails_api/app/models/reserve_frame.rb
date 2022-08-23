df093a1fe0f8e43ffb806d73d5dcb63a51624458
ikeyu0806 <ikeyu0806@gmail.com>
2022-08-23 17:41:10 +0900

    not create before today reservation menu to calendar

--- rails_api/app/models/reserve_frame.rb
+++ rails_api/app/models/reserve_frame.rb
@@ -116,7 +116,7 @@ class ReserveFrame < ApplicationRecord
     result = []
     range_start_date = Date.new(year, month)
     range_end_date = Date.new(year, month, -1)
-    loop_start_date = Date.parse(self.start_at.to_s)
+    loop_start_date =  Date.parse(self.start_at.to_s) < Date.today ? Date.today : Date.parse(self.start_at.to_s)
     loop_start_date = loop_start_date < range_start_date ? range_start_date : loop_start_date
     loop_end_date = Date.parse(self.repeat_end_date.to_s)
     loop_end_date = loop_end_date > range_end_date ? range_end_date : loop_end_date
