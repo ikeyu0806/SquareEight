e46c58cba38f390f54cd2bd9aed3ab733c1fd9fd
ikeyu0806 <ikeyu0806@gmail.com>
2022-08-24 15:10:07 +0900

    create AddQuestionnaireFormModal

--- frontend/pages/admin/questionnaire/master.tsx
+++ frontend/pages/admin/questionnaire/master.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React from 'react'
+import AddQuestionnaireFormModal from 'components/templates/AddQuestionnaireFormModal'
 import type { NextPage } from 'next'
 import MerchantUserAdminLayout from 'components/templates/MerchantUserAdminLayout'
 import PlusCircleIcon from 'components/atoms/PlusCircleIcon'
@@ -10,8 +11,6 @@ import { RootState } from 'redux/store'
 
 const Master: NextPage = () => {
   const dispatch = useDispatch()
-  const showAddFormModal = useSelector((state: RootState) => state.questionnaireMaster.showAddFormModal)
-  const selectedFormType = useSelector((state: RootState) => state.questionnaireMaster.selectedFormType)
 
   return (
     <MerchantUserAdminLayout>
@@ -38,68 +37,7 @@ const Master: NextPage = () => {
           </Col>
         </Row>
       </Container>
-      <Modal show={showAddFormModal}>
-        <Modal.Header>質問を追加</Modal.Header>
-        <Modal.Body>
-          <Form.Label>フォーム種別を選択してください</Form.Label>
-          <Form.Check
-            type='radio'
-            label='テキスト'
-            checked={selectedFormType === FORM_TYPE.TEXT}
-            onChange={() => dispatch(selectedFormTypeChanged(FORM_TYPE.TEXT))}
-            value={selectedFormType}></Form.Check>
-          <Form.Check
-            type='radio'
-            label='プルダウン'
-            checked={selectedFormType === String(FORM_TYPE.SELECT)}
-            onChange={() => dispatch(selectedFormTypeChanged(FORM_TYPE.SELECT))}
-            value={selectedFormType}></Form.Check>
-          <Form.Check
-            type='radio'
-            label='チェックボックス'
-            checked={selectedFormType === FORM_TYPE.CHECKBOX}
-            onChange={() => dispatch(selectedFormTypeChanged(FORM_TYPE.CHECKBOX))}
-            value={selectedFormType}></Form.Check>
-          <Form.Check
-            type='radio'
-            label='ラジオボタン'
-            checked={selectedFormType === FORM_TYPE.RADIO}
-            onChange={() => dispatch(selectedFormTypeChanged(FORM_TYPE.RADIO))}
-            value={selectedFormType}></Form.Check>
-          <Form.Check
-            type='radio'
-            label='日付'
-            checked={selectedFormType === FORM_TYPE.DATE}
-            onChange={() => dispatch(selectedFormTypeChanged(FORM_TYPE.DATE))}
-            value={selectedFormType}></Form.Check>
-          <Form.Check
-            type='radio'
-            label='時刻'
-            checked={selectedFormType === FORM_TYPE.DATETIME}
-            onChange={() => dispatch(selectedFormTypeChanged(FORM_TYPE.DATETIME))}
-            value={selectedFormType}></Form.Check>
-          {selectedFormType === FORM_TYPE.TEXT && <><Form.Label
-            className='mt20'>
-            質問を入力してください
-          </Form.Label>
-          <Form.Control
-            placeholder='例) 年齢 ご要望 商品の感想'></Form.Control>
-          <Form.Label
-            className='mt20'>
-            回答フォームの行数を入力してください
-          </Form.Label></>}
-          <Form.Control
-            type='number'></Form.Control>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button>追加する</Button>
-          <Button
-            onClick={() => dispatch(showAddFormModalChanged(false))}
-            variant='secondary'>
-            閉じる
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      <AddQuestionnaireFormModal></AddQuestionnaireFormModal>
     </MerchantUserAdminLayout>
   )
 }
