dc17632f91ac07c3ef179ff13bc99e7aa79e445e
ikeyu0806 <ikeyu0806@gmail.com>
2022-08-28 23:53:14 +0900

    add content to SendMessageTemplateModal

--- frontend/components/templates/SendMessageTemplateModal.tsx
+++ frontend/components/templates/SendMessageTemplateModal.tsx
@@ -2,9 +2,8 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useCookies } from 'react-cookie'
 import { RootState } from 'redux/store'
-import { Container, Modal, Button } from 'react-bootstrap'
+import { Container, Modal, Button, Form, FormControl, Row, Col, ListGroup } from 'react-bootstrap'
 import { showSendMessageTemplateModalChanged } from 'redux/messageTemplateSlice'
-import CreateMessageTemplateForm from 'components/organisms/CreateMessageTemplateForm'
 import { alertChanged } from 'redux/alertSlice'
 import axios from 'axios'
 
@@ -35,21 +34,50 @@ const SendMessageTemplateModal = (): JSX.Element => {
     })
   }
 
-  const validateSubmit = () => {
-    if (!name || !content) {
-      return true
-    }
-    return false
-  }
-
   return (
     <Modal show={showSendMessageTemplateModal} fullscreen={true}>
       <Modal.Header>メッセージ送信</Modal.Header>
       <Modal.Body>
+        <Row>
+          <Col md={5}>
+            <Form.Label>宛先メールアドレス</Form.Label>
+            <Form.Control
+              placeholder='宛先メールアドレスを入力してください。'></Form.Control>
+            <Form.Label className='mt10'>送信内容</Form.Label>
+            <FormControl
+              disabled={true}
+              value={content}
+              as='textarea'
+              rows={40} />
+          </Col>
+          <Col md={3}>
+          <div>変数</div>
+          <div>
+            <ListGroup>
+              <ListGroup.Item>
+                <div>%customer_name</div>
+                <div className='mt10'>顧客名に変換されます</div>
+                <div>顧客一覧からの送信時に反映されます</div>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <div>%customer_postalcode</div>
+                <div className='mt10'>顧客の郵便番号に変換されます</div>
+                <div>顧客一覧からの送信時に反映されます</div>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <div>%customer_address</div>
+                <div className='mt10'>顧客の住所に変換されます</div>
+                <div>顧客一覧からの送信時に反映されます</div>
+              </ListGroup.Item>
+            </ListGroup>
+          </div>
+          </Col>
+          <Col md={4}></Col>
+        </Row>
       </Modal.Body>
       <Modal.Footer>
         <Button>
-          登録する
+          送信する
         </Button>
         <Button variant='secondary' onClick={() => dispatch(showSendMessageTemplateModalChanged(false))}>
           閉じる
