ec2a50e2839039aba02be1a6728c0e0f54f47a10
ikeyu0806 <ikeyu0806@gmail.com>
2022-09-02 21:18:55 +0900

    add no customer condition to dashboard chart

--- rails_api/app/controllers/api/internal/accounts_controller.rb
+++ rails_api/app/controllers/api/internal/accounts_controller.rb
@@ -11,13 +11,17 @@ class Api::Internal::AccountsController < ApplicationController
     week_days =  (0..6).to_a.map{|i| (Time.now - i.days).strftime("%Y/%m/%d")}.reverse
     # 顧客数グラフ
     customers = account.customers.where(created_at: 1.week.ago.beginning_of_day..Time.zone.now.end_of_day)
-    group_by_customers_count = customers.map{|customer| customer.created_at.strftime("%Y/%m/%d")}.group_by(&:itself).transform_values(&:size)
-    customer_count_array = week_days.map do |day|
-      group_by_customers_count[day].present? ? group_by_customers_count[day] : 0
-    end
-    # 新規顧客が0の日付を埋める
-    customer_count_array = customer_count_array.each_with_index do |a, i|
-      customer_count_array[i] = customer_count_array[i] + customer_count_array[i - 1] if i > 0
+    # if customers.present?
+      group_by_customers_count = customers.map{|customer| customer.created_at.strftime("%Y/%m/%d")}.group_by(&:itself).transform_values(&:size)
+      customer_count_array = week_days.map do |day|
+        group_by_customers_count[day].present? ? group_by_customers_count[day] : 0
+      end
+      # 新規顧客が0の日付を埋める
+      customer_count_array = customer_count_array.each_with_index do |a, i|
+        customer_count_array[i] = customer_count_array[i] + customer_count_array[i - 1] if i > 0
+      end
+    else
+      customer_count_array = []
     end
     # 売上グラフ
     payment_intents = account.search_stripe_payment_intents
