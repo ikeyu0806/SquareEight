efabf60b2837e7d84f7743a2650cbb0d6d2aa461
ikeyu0806 <ikeyu0806@gmail.com>
2022-09-06 23:13:41 +0900

    fix sharedComponent register

--- rails_api/app/controllers/api/internal/shared_components_controller.rb
+++ rails_api/app/controllers/api/internal/shared_components_controller.rb
@@ -20,7 +20,7 @@ class Api::Internal::SharedComponentsController < ApplicationController
         shared_component.navbar_brand_image_s3_object_public_url = put_s3_http_request_data(shared_component_params[:navbar_brand_image], ENV["SHARED_COMPONENT_IMAGE_BUCKET"], file_name)
         shared_component.nabvar_brand_image_s3_object_name = file_name
       end
-      shared_component.attributes = shared_component_params.except(:navbar_brand_image)
+      shared_component.attributes = shared_component_params.except(:navbar_brand_image, :is_update_navbar_brand_image)
       shared_component.save!
       render json: { status: 'success' }, states: 200
     end
