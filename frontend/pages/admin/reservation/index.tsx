363e0e2d0f48a92bbe5654b19069960ae47cc73e
ikeyu0806 <ikeyu0806@gmail.com>
2022-09-12 21:55:46 +0900

    add targetDate state to reservation

--- frontend/pages/admin/reservation/index.tsx
+++ frontend/pages/admin/reservation/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import React, { useEffect, useState } from 'react'
-import { Container, Row, Col, ListGroup, Card } from 'react-bootstrap'
+import { Container, Row, Col, ListGroup, Card, Form } from 'react-bootstrap'
 import MerchantUserAdminLayout from 'components/templates/MerchantUserAdminLayout'
 import { useCookies } from 'react-cookie'
 import { ReservationParam } from 'interfaces/ReservationParam'
@@ -9,10 +9,13 @@ import axios from 'axios'
 import { receptionTypeText } from 'functions/receptionTypeText'
 import CheckIcon from 'components/atoms/CheckIcon'
 import { swalWithBootstrapButtons } from 'constants/swalWithBootstrapButtons'
+import { getZeroPaddingDate } from 'functions/getZeroPaddingDatetime'
 
 const Index: NextPage = () => {
   const [cookies] = useCookies(['_square_eight_merchant_session'])
   const [reservations, setReservatons] = useState<ReservationParam[]>([])
+  const date = new Date()
+  const [targetDate, setTargetDate] = useState(getZeroPaddingDate())
 
   useEffect(() => {
     axios.get(`${process.env.BACKEND_URL}/api/internal/account/reservations`,
@@ -62,40 +65,43 @@ const Index: NextPage = () => {
           <Row>
             <Col lg={3}></Col>
             <Col lg={6}>
-              <Card>
-                <Card.Header>予約一覧</Card.Header>
-                {reservations && reservations.map((reservation, i) => {
-                  return (
-                    <ListGroup.Item key={i}>
-                      <Row>
-                        <Col>
-                          <div className='mb10'>{reservation.reserve_frame_title}</div>
-                          <div>{reservation.display_reservation_datetime}</div>
-                          <div>人数: {reservation.number_of_people}</div>
-                          <div>支払い方法: {paymentMethodText(reservation.payment_method, reservation.price, reservation.ticket_consume_number, reservation.number_of_people)}</div>
-                          <div className='mt20'>
-                            受付設定: {receptionTypeText(reservation.reception_type)}
-                          </div>
-                          <br/>
-                          <span>ステータス:
-                          {reservation.status === 'confirm' && <><CheckIcon width={20} height={20} fill={'#00ff00'}/>予約確定</>}</span>
-                          {reservation.status === 'pendingVerifivation'
-                            && <>
-                                <span className='text-danger'>予約確定待ち</span><br/>
-                                <a className='badge bg-primary' onClick={() => updateReservationStatus(reservation.id, 'confirm')}>予約を確定する</a>
-                              </>}
-                        </Col>
-                        <Col>
-                          <div className='mb10'>顧客情報</div>
-                          <div>{reservation.customer_name}</div>
-                          <div>メールアドレス: {reservation.customer_email}</div>
-                          <div>電話番号: {reservation.customer_phone_number}</div>
-                        </Col>
-                      </Row>                    
-                    </ListGroup.Item>
-                  )
-                })}
-              </Card>
+              <Form.Label>対象日付</Form.Label>
+              <Form.Control
+                onChange={(e) => setTargetDate(e.target.value)}
+                type='date'
+                value={targetDate}></Form.Control>
+              &emsp;
+              {reservations && reservations.map((reservation, i) => {
+                return (
+                  <ListGroup.Item key={i}>
+                    <Row>
+                      <Col>
+                        <div className='mb10'>{reservation.reserve_frame_title}</div>
+                        <div>{reservation.display_reservation_datetime}</div>
+                        <div>人数: {reservation.number_of_people}</div>
+                        <div>支払い方法: {paymentMethodText(reservation.payment_method, reservation.price, reservation.ticket_consume_number, reservation.number_of_people)}</div>
+                        <div className='mt20'>
+                          受付設定: {receptionTypeText(reservation.reception_type)}
+                        </div>
+                        <br/>
+                        <span>ステータス:
+                        {reservation.status === 'confirm' && <><CheckIcon width={20} height={20} fill={'#00ff00'}/>予約確定</>}</span>
+                        {reservation.status === 'pendingVerifivation'
+                          && <>
+                              <span className='text-danger'>予約確定待ち</span><br/>
+                              <a className='badge bg-primary' onClick={() => updateReservationStatus(reservation.id, 'confirm')}>予約を確定する</a>
+                            </>}
+                      </Col>
+                      <Col>
+                        <div className='mb10'>顧客情報</div>
+                        <div>{reservation.customer_name}</div>
+                        <div>メールアドレス: {reservation.customer_email}</div>
+                        <div>電話番号: {reservation.customer_phone_number}</div>
+                      </Col>
+                    </Row>                    
+                  </ListGroup.Item>
+                )
+              })}
             </Col>
           </Row>
         </Container>
