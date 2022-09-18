7d910d14ae86edbffc6e0a849d2da04b3651e759
ikeyu0806 <ikeyu0806@gmail.com>
2022-09-18 21:12:31 +0900

    add share icons to merchantCustom Footer

--- frontend/components/molecules/MerchantCustomFooter.tsx
+++ frontend/components/molecules/MerchantCustomFooter.tsx
@@ -1,14 +1,37 @@
 import { RootState } from 'redux/store'
 import { useSelector } from 'react-redux'
-import { FacebookIcon } from 'react-share'
+import { FacebookIcon,
+         FacebookShareButton,
+         LineIcon,
+         LineShareButton,
+         TwitterShareButton,
+         TwitterIcon  } from 'react-share'
+import { useRouter } from 'next/router'
 
 const MerchantCustomFooter = () => {
+  const router = useRouter()
   const footerCopyRightText =  useSelector((state: RootState) => state.sharedComponent.footerCopyRightText)
 
   return (
     <footer className='content text-center'>
-      <FacebookIcon
-        round={true} />
+      <LineShareButton
+        url={process.env.FRONTEND_URL + router.pathname}
+        title='SquareEight'>
+        <LineIcon
+          round={true}/>
+      </LineShareButton>
+      <TwitterShareButton
+        url={process.env.FRONTEND_URL + router.pathname}
+        title='SquareEight'>
+        <TwitterIcon
+          round={true} />
+      </TwitterShareButton>
+      <FacebookShareButton
+        url={process.env.FRONTEND_URL + router.pathname}
+        title='SquareEight'>
+        <FacebookIcon
+          round={true} />
+      </FacebookShareButton>
       <hr />
       <p className='footer-margin'>Copyright {footerCopyRightText} {new Date().getFullYear()}</p>
     </footer>
