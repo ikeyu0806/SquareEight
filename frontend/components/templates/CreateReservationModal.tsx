a227df22e16653b4407d9e63f9bc479aba6228e6
ikeyu0806 <ikeyu0806@gmail.com>
2022-09-19 08:59:30 +0900

    add reserveFrame fetch to reservation modal

--- frontend/components/templates/CreateReservationModal.tsx
+++ frontend/components/templates/CreateReservationModal.tsx
@@ -1,11 +1,38 @@
+import { useEffect, useState } from 'react'
 import { Modal, Button, Form, Row, Col } from 'react-bootstrap'
 import { RootState } from 'redux/store'
 import { useSelector, useDispatch } from 'react-redux'
 import { showRegisterReservationModalChanged } from 'redux/reservationSlice'
+import axios from 'axios'
+import { useCookies } from 'react-cookie'
+import { ReserveFrameParam } from 'interfaces/ReserveFrameParam'
 
 const CreateReservationModal = (): JSX.Element => {
   const dispatch = useDispatch()
   const showRegisterReservationModal = useSelector((state: RootState) => state.reservation.showRegisterReservationModal)
+  const [cookies] = useCookies(['_square_eight_merchant_session'])
+  const [reserveFrames, setReserveFrames] = useState<ReserveFrameParam[]>([])
+
+  useEffect(() => {
+    const fetchReserveFrames = () => {
+      axios.get(
+        `${process.env.BACKEND_URL}/api/internal/reserve_frames`, {
+          headers: { 
+            'Session-Id': cookies._square_eight_merchant_session
+          },
+        }
+      )
+      .then(function (response) {
+        const reserveFrameResponse: ReserveFrameParam[] = response.data.reserve_frames
+        console.log(response.data)
+        setReserveFrames(reserveFrameResponse)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
+    fetchReserveFrames()
+  }, [cookies._square_eight_merchant_session])
 
   return (
 
@@ -14,7 +41,11 @@ const CreateReservationModal = (): JSX.Element => {
       <Modal.Body>
         <Form.Label>予約メニュー</Form.Label>
         <Form.Select>
-          <option value='demo'>demo</option>
+          {reserveFrames.map((reserveFrame, i) => {
+            return (
+              <option value={reserveFrame.id} key={i}>{reserveFrame.title}</option>
+            )
+          })}
         </Form.Select>
         <Form.Label className='mt10'>予約者の名前（姓）</Form.Label>
         <Form.Control></Form.Control>
