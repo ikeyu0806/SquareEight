276c7f761bf738181f4c107b036123d76dde842a
ikeyu0806 <ikeyu0806@gmail.com>
2022-09-18 21:00:52 +0900

    add react-share button

--- frontend/components/molecules/MerchantCustomFooter.tsx
+++ frontend/components/molecules/MerchantCustomFooter.tsx
@@ -1,11 +1,14 @@
 import { RootState } from 'redux/store'
 import { useSelector } from 'react-redux'
+import { FacebookIcon } from 'react-share'
 
 const MerchantCustomFooter = () => {
   const footerCopyRightText =  useSelector((state: RootState) => state.sharedComponent.footerCopyRightText)
 
   return (
     <footer className='content text-center'>
+      <FacebookIcon
+        round={true} />
       <hr />
       <p className='footer-margin'>Copyright {footerCopyRightText} {new Date().getFullYear()}</p>
     </footer>
