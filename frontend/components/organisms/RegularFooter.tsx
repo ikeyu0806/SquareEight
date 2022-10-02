1513f55d33adcfaa3f4bc0cfb9cf0c251b675153
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-02 10:53:37 +0900

    create administrator page

--- frontend/components/organisms/RegularFooter.tsx
+++ frontend/components/organisms/RegularFooter.tsx
@@ -4,7 +4,10 @@ const RegularFooter = (): JSX.Element => {
   return (
     <footer className='content text-center'>
       <hr />
-      <p className='footer-margin'>Copyright SquareEight {new Date().getFullYear()}</p>
+      <p className='footer-margin'>
+        
+        Copyright <a href='/administrator'>SquareEight Administrator</a> {new Date().getFullYear()}
+      </p>
     </footer>
   )
 }
---
+++ frontend/pages/administrator/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import type { NextPage } from 'next'
+import WithoutSessionLayout from 'components/templates/WithoutSessionLayout'
+import { Container, Card, Row, Col } from 'react-bootstrap'
+
+const Index: NextPage = () => {
+  return (
+    <WithoutSessionLayout>
+      <Container className='mt50 mb50'>
+        <Row>
+          <Col lg={3} md={3}></Col>
+          <Col lg={6}>
+            <Card>
+              <Card.Header>運営者情報</Card.Header>
+              <Card.Body>
+                <div>サービス管理人: 池谷祐貴</div>
+                <hr/>
+                <div><a href='https://ikeyu0806.hatenablog.com/'>ブログ</a></div>
+                <hr />
+                <div><a href='https://jp.linkedin.com/in/%E7%A5%90%E8%B2%B4-%E6%B1%A0%E8%B0%B7-61a31661'>Linkdin</a></div>
+                <hr/>
+                <div><a href='https://ikeyu0806.github.io/'>ポートフォリオ</a></div>
+                <hr/>
+                <div>お問い合わせは<a href='/inquiry'>こちら</a></div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </WithoutSessionLayout>
+  )
+}
+
+export default Index
