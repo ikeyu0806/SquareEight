89e67e99234a01345ecbc06218dec1931425ff29
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-02 19:49:52 +0900

    add state to delivery_datetime

--- frontend/pages/admin/delivery_datetime/index.tsx
+++ frontend/pages/admin/delivery_datetime/index.tsx
@@ -1,12 +1,53 @@
+import React, { useState } from 'react'
 import { Container, Row, Col, Button, Form } from 'react-bootstrap'
 import type { NextPage } from 'next'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from 'redux/store'
 import MerchantUserAdminLayout from 'components/templates/MerchantUserAdminLayout'
 import SetTargetProductModal from 'components/templates/SetTargetProductModal'
-import { showSetTargetProductModalChanged } from 'redux/deliveryDatetimeSlice'
+import { 
+  shortestDeliveryDayChanged,
+  longestDeliveryDayChanged,
+  deadlineTimeChanged,
+  isSetPerAreaDeliveryDateChanged,
+  isHolidaySunChanged,
+  isHolidayMonChanged,
+  isHolidayTueChanged,
+  isHolidayWedChanged,
+  isHolidayThuChanged,
+  isHolidayFriChanged,
+  isHolidaySatChanged,
+  temporaryHolidaysChanged,
+  deliveryTimeTypeChanged,
+  targetProductsChanged,
+  showSetTargetProductModalChanged } from 'redux/deliveryDatetimeSlice'
 
 const Index: NextPage = () => {
   const dispatch = useDispatch()
+
+  const [inputTemporaryHoliday, setInputTemporaryHoliday] = useState('')
+
+  const shortestDeliveryDay = useSelector((state: RootState) => state.deliveryDatetime.shortestDeliveryDay)
+  const longestDeliveryDay = useSelector((state: RootState) => state.deliveryDatetime.longestDeliveryDay)
+  const deadlineTime = useSelector((state: RootState) => state.deliveryDatetime.deadlineTime)
+  const isSetPerAreaDeliveryDate = useSelector((state: RootState) => state.deliveryDatetime.isSetPerAreaDeliveryDate)
+  const isHolidaySun = useSelector((state: RootState) => state.deliveryDatetime.isHolidaySun)
+  const isHolidayMon = useSelector((state: RootState) => state.deliveryDatetime.isHolidayMon)
+  const isHolidayTue = useSelector((state: RootState) => state.deliveryDatetime.isHolidayTue)
+  const isHolidayWed = useSelector((state: RootState) => state.deliveryDatetime.isHolidayWed)
+  const isHolidayThu = useSelector((state: RootState) => state.deliveryDatetime.isHolidayThu)
+  const isHolidayFri = useSelector((state: RootState) => state.deliveryDatetime.isHolidayFri)
+  const isHolidaySat = useSelector((state: RootState) => state.deliveryDatetime.isHolidaySat)
+  const temporaryHolidays = useSelector((state: RootState) => state.deliveryDatetime.temporaryHolidays)
+  const deliveryTimeType = useSelector((state: RootState) => state.deliveryDatetime.deliveryTimeType)
+  const targetProducts = useSelector((state: RootState) => state.deliveryDatetime.targetProducts)
+
+  const addIemporaryHolidays = () => {
+    let updateTemporaryHolidays: string[]
+    updateTemporaryHolidays = temporaryHolidays
+    updateTemporaryHolidays.push(inputTemporaryHoliday)
+    dispatch(temporaryHolidaysChanged(updateTemporaryHolidays))
+  }
 
   return (
     <MerchantUserAdminLayout>
@@ -25,12 +66,13 @@ const Index: NextPage = () => {
                 注文から
               </Col>
               <Col>
-                <Form.Select>
-                {[...Array(30)].map((_, i) => {
-                  return (
-                    <option key={i} value={i + 1}>{i + 1}</option>
-                  )
-                })}
+                <Form.Select
+                  onChange={(e) => dispatch(shortestDeliveryDayChanged(Number(e.target.value)))}>
+                  {[...Array(30)].map((_, i) => {
+                    return (
+                      <option key={i} value={i + 1}>{i + 1}</option>
+                    )
+                  })}
                 </Form.Select>
               </Col>
               <Col>営業日</Col>
@@ -43,7 +85,8 @@ const Index: NextPage = () => {
                 注文から
               </Col>
               <Col>
-                <Form.Select>
+                <Form.Select
+                  onChange={(e) => dispatch(longestDeliveryDayChanged(Number(e.target.value)))}>
                 {[...Array(30)].map((_, i) => {
                   return (
                     <option key={i} value={i + 1}>{i + 1}</option>
@@ -60,7 +103,10 @@ const Index: NextPage = () => {
             <div>設定した時間を過ぎた注文の注文日を翌日扱いにして、お届け可能日が計算されるようになります。</div>
             <Row>
               <Col>
-                <Form.Control type='time'></Form.Control>
+                <Form.Control
+                  value={deadlineTime}
+                  onChange={(e) => dispatch(deadlineTimeChanged(e.target.value))}
+                  type='time'></Form.Control>
               </Col>
               <Col sm={8}></Col>
             </Row>
@@ -68,7 +114,11 @@ const Index: NextPage = () => {
 
             <div className='mb10'>エリア別追加お届け日数</div>
             <div>都道府県ごとに、「最短お届け日」から追加でかかるお届け日数を設定できます。</div>
-            <Form.Check label='エリア別の追加お届け日数を設定する' id='setAreaDeliveryDay'></Form.Check>
+            <Form.Check
+              onChange={(e) => dispatch(isSetPerAreaDeliveryDateChanged(!isSetPerAreaDeliveryDate))}
+              checked={isSetPerAreaDeliveryDate}
+              label='エリア別の追加お届け日数を設定する'
+              id='setAreaDeliveryDay'></Form.Check>
             <hr />
 
             <div className='mb10'>休業日の設定</div>
@@ -76,49 +126,73 @@ const Index: NextPage = () => {
             <div>定休日</div>
               <Form.Check
                 inline
+                onChange={() => dispatch(isHolidaySunChanged(!isHolidaySun))}
+                checked={isHolidaySun}
                 label='日'
                 name='holidayWday'
                 type='checkbox'
                 id='holidaySun' />
               <Form.Check
                 inline
+                onChange={() => dispatch(isHolidayMonChanged(!isHolidayMon))}
+                checked={isHolidayMon}
                 label='月'
                 name='holidayWday'
                 type='checkbox'
                 id='holidayMon' />
               <Form.Check
                 inline
+                onChange={() => dispatch(isHolidayTueChanged(!isHolidayTue))}
+                checked={isHolidayTue}
                 label='火'
                 name='holidayWday'
                 type='checkbox'
                 id='holidayTue' />
               <Form.Check
                 inline
+                onChange={() => dispatch(isHolidayWedChanged(!isHolidayWed))}
+                checked={isHolidayWed}
                 label='水'
                 name='holidayWday'
                 type='checkbox'
                 id='holidayWed' />
               <Form.Check
                 inline
+                onChange={() => dispatch(isHolidayThuChanged(!isHolidayThu))}
+                checked={isHolidayThu}
                 label='木'
                 name='holidayWday'
                 type='checkbox'
                 id='holidayThu' />
               <Form.Check
                 inline
+                onChange={() => dispatch(isHolidayFriChanged(!isHolidayFri))}
+                checked={isHolidayFri}
                 label='金'
                 name='holidayWday'
                 type='checkbox'
                 id='holidayFri' />
               <Form.Check
                 inline
+                onChange={() => dispatch(isHolidaySatChanged(!isHolidaySat))}
+                checked={isHolidaySat}
                 label='土'
                 name='holidayWday'
                 type='checkbox'
                 id='holidaySat' />
               <div className='mt20'>臨時休業日</div>
-              <Form.Control type='date'></Form.Control>
-              <Button className='mt20'>臨時休業日に追加</Button>
+              <Form.Control
+                value={inputTemporaryHoliday}
+                onChange={(e) => setInputTemporaryHoliday(e.target.value)}
+                type='date'></Form.Control>
+              <Button
+                onChange={(e) => addIemporaryHolidays()}
+                className='mt20'>臨時休業日に追加</Button>
+              {temporaryHolidays.map((holiday, i) => {
+                return (
+                  <div key={i}>{holiday}</div>
+                )
+              })}
             <hr/>
 
             <div className='mb10'>配送時間の設定</div>
@@ -126,24 +200,32 @@ const Index: NextPage = () => {
               type='radio'
               id='yamato'
               name='setDeliveryTime'
+              onChange={() => dispatch(deliveryTimeTypeChanged('yamato'))}
+              checked={deliveryTimeType === 'yamato'}
               label='ヤマト宅急便'></Form.Check>
             <div className='ml30 mb10'>午前中 / 14時〜16時 / 16時〜18時 / 18時〜20時 / 19時〜21時</div>
             <Form.Check
               type='radio'
-              id='yamato'
+              id='sagawa'
               name='setDeliveryTime'
+              onChange={() => dispatch(deliveryTimeTypeChanged('sagawa'))}
+              checked={deliveryTimeType === 'sagawa'}
               label='佐川急便'></Form.Check>
             <div className='ml30 mb10'>午前中 / 12時〜14時 / 14時〜16時 / 16時〜18時 / 18時〜20時 / 18時〜21時 / 19時〜21時</div>
             <Form.Check
               type='radio'
-              id='yamato'
+              id='yupack'
               name='setDeliveryTime'
+              onChange={() => dispatch(deliveryTimeTypeChanged('yupack'))}
+              checked={deliveryTimeType === 'yupack'}
               label='ゆうパック'></Form.Check>
             <div className='ml30 mb10'>午前中 / 12時〜14時 / 14時〜16時 / 16時〜18時 / 18時〜20時 / 19時〜21時 / 20時〜21時</div>
             <Form.Check
               type='radio'
-              id='yamato'
+              id='other'
               name='setDeliveryTime'
+              onChange={() => dispatch(deliveryTimeTypeChanged('other'))}
+              checked={deliveryTimeType === 'other'}
               label='その他'></Form.Check>
             <div className='ml30 mb10'>時間区分を自分で設定する</div>
             <hr />
