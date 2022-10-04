223ddff4fce6a67045ea4fedb6c0ceade5e8d4bc
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-04 16:01:33 +0900

    change delivery_time data type

--- rails_api/db/migrate/20221003063805_create_custom_delivery_times.rb
+++ rails_api/db/migrate/20221003063805_create_custom_delivery_times.rb
@@ -2,7 +2,7 @@ class CreateCustomDeliveryTimes < ActiveRecord::Migration[7.0]
   def change
     create_table :custom_delivery_times do |t|
       t.integer :delivery_datetime_setting_id, null: false
-      t.string :delivery_time
+      t.time :delivery_time
 
       t.timestamps
     end
--- rails_api/db/schema.rb
+++ rails_api/db/schema.rb
@@ -65,7 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_03_064609) do
 
   create_table "custom_delivery_times", force: :cascade do |t|
     t.integer "delivery_datetime_setting_id", null: false
-    t.string "delivery_time"
+    t.time "delivery_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
