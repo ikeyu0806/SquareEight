e04908df409aa13cc571356fb594aa5d2ccd4c5e
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-13 21:18:24 +0900

    fix StripeWebhookMailer text condition

--- rails_api/app/mailers/stripe_webhook_mailer.rb
+++ rails_api/app/mailers/stripe_webhook_mailer.rb
@@ -2,7 +2,7 @@ class StripeWebhookMailer < ApplicationMailer
   def account_update_mail(email, account_id, payouts_enabled)
     @account = Account.find(account_id)
     subject = payouts_enabled ? "Stripeへの決済有効申請が承認されました。" : "Stripeへの決済有効申請がまだ完了しておりません。追加必要項目を確認してください"
-    @payouts_enabled = @payouts_enabled
+    @payouts_enabled = payouts_enabled
     mail(to: email, subject: subject)
   end
 end
