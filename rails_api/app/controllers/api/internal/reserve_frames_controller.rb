7309eaa938e7f7b52e651bb7b49d7a73e8b90cdc
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-14 14:05:58 +0900

    create reserve_frame_image

--- rails_api/app/controllers/api/internal/reserve_frames_controller.rb
+++ rails_api/app/controllers/api/internal/reserve_frames_controller.rb
@@ -68,8 +68,10 @@ class Api::Internal::ReserveFramesController < ApplicationController
       end
       if reserve_frame_params[:base64_image].present?
         file_name = "reserve_frame_image_" + Time.zone.now.strftime('%Y%m%d%H%M%S%3N')
-        reserve_frame.s3_object_public_url = put_s3_http_request_data(reserve_frame_params[:base64_image], ENV["PRODUCT_IMAGE_BUCKET"], file_name)
-        reserve_frame.s3_object_name = file_name
+        account_image = reserve_frame.account_s3_images.new
+        account_image.account = current_merchant_user.account
+        account_image.s3_object_public_url = put_s3_http_request_data(reserve_frame_params[:base64_image], ENV["PRODUCT_IMAGE_BUCKET"], file_name)
+        account_image.s3_object_name = file_name
       end
       if reserve_frame_params[:repeat_wdays].present?
         reserve_frame.is_repeat_sun = true if reserve_frame_params[:repeat_wdays].include?("Sun")
@@ -139,9 +141,12 @@ class Api::Internal::ReserveFramesController < ApplicationController
         end
       end
       if reserve_frame_params[:base64_image].present?
+        reserve_frame.reserve_frame_image_relations.update_all(relation_status: "Sub")
         file_name = "reserve_frame_image_" + Time.zone.now.strftime('%Y%m%d%H%M%S%3N')
-        reserve_frame.s3_object_public_url = put_s3_http_request_data(reserve_frame_params[:base64_image], ENV["PRODUCT_IMAGE_BUCKET"], file_name)
-        reserve_frame.s3_object_name = file_name
+        account_image = reserve_frame.account_s3_images.new
+        account_image.account = current_merchant_user.account
+        account_image.s3_object_public_url = put_s3_http_request_data(reserve_frame_params[:base64_image], ENV["PRODUCT_IMAGE_BUCKET"], file_name)
+        account_image.s3_object_name = file_name
       end
       if reserve_frame_params[:repeat_wdays].present?
         reserve_frame.is_repeat_sun = true if reserve_frame_params[:repeat_wdays].include?("Sun")
