18341231165af472f331dbeb1447b9c64c04df5e
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-17 13:47:04 +0900

    show message template to payment request page

--- frontend/pages/admin/payment_request/new.tsx
+++ frontend/pages/admin/payment_request/new.tsx
@@ -1,39 +1,57 @@
 import { NextPage } from 'next'
 import MerchantUserAdminLayout from 'components/templates/MerchantUserAdminLayout'
 import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+import MessageTemplateVariables from 'components/molecules/MessageTemplateVariables'
 
 const New: NextPage = () => {
   return (
     <MerchantUserAdminLayout>
       <Container>
+        <h3>決済リクエスト作成</h3>
+        <Form.Label>対象顧客</Form.Label>
+        <Form.Check
+          type='radio'
+          label='登録済みの顧客に送信'
+          name='targetCustomerCheck'
+          id='registeredCustomer'></Form.Check>
+        <Form.Check
+          type='radio'
+          label='顧客グループに送信'
+          name='targetCustomerCheck'
+          id='targetCustomerCustomer'></Form.Check>
+        <Form.Check
+          type='radio'
+          label='未登録の顧客に送信'
+          name='targetCustomerChe1ck'
+          id='newCustomer'></Form.Check>
+        <hr />
+        <Form.Label>請求金額</Form.Label>
+        <Form.Control type='number'></Form.Control>
+        <hr />
+        <Form.Label>メール本文</Form.Label>
+        <Form.Check
+          type='radio'
+          label='登録済みのメッセージテンプレートから選択'
+          name='messageTemplate'
+          id='selectRegisteredMessageTemplate'></Form.Check>
+        <Form.Check
+          type='radio'
+          label='メッセージを入力する'
+          name='messageTemplate'
+          id='createMessageTemplate'></Form.Check>
         <Row>
-          <Col lg={3} md={3}></Col>
-          <Col lg={6} md={6}>
-            <h3>決済リクエスト作成</h3>
-            <Form.Label>対象顧客</Form.Label>
-            <Form.Check
-              type='radio'
-              label='登録済みの顧客に送信'
-              name='targetCustomerCheck'
-              id='registeredCustomer'></Form.Check>
-            <Form.Check
-              type='radio'
-              label='顧客グループに送信'
-              name='targetCustomerCheck'
-              id='targetCustomerCustomer'></Form.Check>
-            <Form.Check
-              type='radio'
-              label='未登録の顧客に送信'
-              name='targetCustomerCheck'
-              id='newCustomer'></Form.Check>
-            <hr />
-            <Form.Label>金額</Form.Label>
-            <Form.Control type='number'></Form.Control>
-            <div className='text-center mt20'>
-              <Button>確定して送信する</Button>
-            </div>
+          <Col md={8}>
+            <Form.Control
+              as='textarea'
+              rows={20}></Form.Control>
+          </Col>
+          <Col md={4}>
+            <MessageTemplateVariables />
           </Col>
         </Row>
+        <div className='text-center mt20'>
+          <Button>確定して送信する</Button>
+        </div>
       </Container>
     </MerchantUserAdminLayout>
   )
