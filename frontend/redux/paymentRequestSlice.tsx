3015c538f2e9b5006aff260965cacc4b11b770be
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-17 16:07:27 +0900

    add customer state to paymentRequestSlice

--- frontend/redux/paymentRequestSlice.tsx
+++ frontend/redux/paymentRequestSlice.tsx
@@ -1,11 +1,17 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { CustomerParam } from 'interfaces/CustomerParam'
+import { CustomerGroupParam } from 'interfaces/CustomerGroupParam'
 
 export const paymentRequestSlice = createSlice({
   name: 'paymentRequest',
   initialState: {
     price: 1000,
     targetCustomerType: 'registeredCustomer',
-    messageContentType: 'inputMessage'
+    messageContentType: 'inputMessage',
+    customers:  [] as CustomerParam[],
+    selectedCustomers:  [] as CustomerParam[],
+    customerGroups:  [] as CustomerGroupParam[],
+    selectedCustomerGroups:  [] as CustomerGroupParam[],
   },
   reducers: {
     priceChanged: (state, action: PayloadAction<number>) => {
@@ -17,11 +23,27 @@ export const paymentRequestSlice = createSlice({
     messageContentTypeChanged: (state, action: PayloadAction<string>) => {
       state.messageContentType = action.payload
     },
+    customersChanged: (state, action: PayloadAction<CustomerParam[]>) => {
+      state.customers = action.payload
+    },
+    selectedCustomersChanged: (state, action: PayloadAction<CustomerParam[]>) => {
+      state.selectedCustomers = action.payload
+    },
+    customerGroupsChanged: (state, action: PayloadAction<CustomerGroupParam[]>) => {
+      state.customerGroups = action.payload
+    },
+    selectedCustomerGroupsChanged: (state, action: PayloadAction<CustomerGroupParam[]>) => {
+      state.selectedCustomerGroups = action.payload
+    },
   },
 })
 
 export const { priceChanged } = paymentRequestSlice.actions
 export const { targetCustomerTypeChanged } = paymentRequestSlice.actions
 export const { messageContentTypeChanged } = paymentRequestSlice.actions
+export const { customersChanged } = paymentRequestSlice.actions
+export const { selectedCustomersChanged } = paymentRequestSlice.actions
+export const { customerGroupsChanged } = paymentRequestSlice.actions
+export const { selectedCustomerGroupsChanged } = paymentRequestSlice.actions
 
 export default paymentRequestSlice.reducer
