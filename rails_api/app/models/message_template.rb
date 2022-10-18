c793f15a124bef7ae0364c86143702a5055294ef
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-18 17:38:51 +0900

    add nil guard to message_template convert_content

--- rails_api/app/models/message_template.rb
+++ rails_api/app/models/message_template.rb
@@ -5,9 +5,13 @@ class MessageTemplate < ApplicationRecord
   validates :content, presence: true
 
   def self.convert_content(content, last_name='', first_name='', price='', payment_request_url='')
+    last_name = '' if last_name.nil?
+    first_name = '' if first_name.nil?
+    price = 0 if price.nil?
+    payment_request_url = '' if payment_request_url.nil?
     content
     .gsub(/%customer_name/, last_name + first_name)
-    .gsub(/%price/, price)
+    .gsub(/%price/, price.to_s)
     .gsub(/%payment_request_url/, payment_request_url)
   end
 end
