9cc7a22f18e545612ed848ec6ac0a5f543d204c7
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-22 07:55:07 +0900

    add tests to account request spec

--- rails_api/spec/requests/api/internal/accounts_spec.rb
+++ rails_api/spec/requests/api/internal/accounts_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Api::Internal::AccountsController', type: :request do
     create(:merchant_user, account: account)
   }
 
-  describe 'GET /api/internal/account_info' do
+  describe 'GET /api/internal/accounts/account_info' do
     context 'login as merchant_user' do
       it 'should return 200' do
         allow_any_instance_of(ApplicationController).to receive(:current_merchant_user).and_return(merchant_user)
@@ -47,4 +47,39 @@ RSpec.describe 'Api::Internal::AccountsController', type: :request do
       end
     end
   end
+
+  describe 'GET /api/internal/accounts/dashboard_contents' do
+    context 'login as merchant_user' do
+      it 'should return 200' do
+        allow_any_instance_of(ApplicationController).to receive(:current_merchant_user).and_return(merchant_user)
+        get '/api/internal/accounts/dashboard_contents'
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'without login' do
+      it 'should return 401' do
+        get '/api/internal/accounts/dashboard_contents'
+        expect(response.status).to eq 401
+      end
+    end
+  end
+
+  describe 'GET /api/internal/accounts/payment_methods' do
+    context 'login as merchant_user' do
+      it 'should return 200' do
+        allow_any_instance_of(ApplicationController).to receive(:current_merchant_user).and_return(merchant_user)
+        get '/api/internal/accounts/payment_methods'
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'without login' do
+      it 'should return 401' do
+        get '/api/internal/accounts/payment_methods'
+        expect(response.status).to eq 401
+      end
+    end
+  end
+  
 end
