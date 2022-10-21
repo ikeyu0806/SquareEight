3b4f2145e6156ffaeeb680ebb042ced00b480286
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-22 07:16:31 +0900

    add validation to account_notifications_controller

--- rails_api/app/controllers/api/internal/account_notifications_controller.rb
+++ rails_api/app/controllers/api/internal/account_notifications_controller.rb
@@ -1,13 +1,15 @@
 class Api::Internal::AccountNotificationsController < ApplicationController
+  before_action :merchant_login_only!
+
   def index
-    account_notifications = AccountNotification.all
+    account_notifications = current_merchant_user.account.account_notifications
     render json: { status: 'success', account_notifications: account_notifications }, status: 200
   rescue => error
     render json: { statue: 'fail', error: error }, status: 500
   end
 
   def show
-    account_notification = AccountNotification.find_by(public_id: params[:public_id])
+    account_notification = current_merchant_user.account.account_notifications.find_by(public_id: params[:public_id])
     render json: { status: 'success', account_notification: account_notification }, status: 200
   rescue => error
     render json: { statue: 'fail', error: error }, status: 500
