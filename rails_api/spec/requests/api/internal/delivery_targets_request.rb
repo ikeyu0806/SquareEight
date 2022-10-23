c29d9ebf43e7681ad2d8bac321dd2bcdc633441f
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-23 15:44:44 +0900

    add delivery_target destroy test

--- rails_api/spec/requests/api/internal/delivery_targets_request.rb
+++ rails_api/spec/requests/api/internal/delivery_targets_request.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Api::Internal::DeliveryTargetsController', type: :request do
   let(:account) { create(:business_account) }
   let(:end_user) { create(:end_user) }
-  let!(:delivery_datetime_setting) { create(:delivery_datetime_setting, account: account) }
+  let!(:delivery_target) { create(:delivery_target, end_user_id: end_user.id) }
 
   describe 'GET /api/internal/delivery_targets' do
     context 'login as merchant_user' do
@@ -48,6 +48,23 @@ RSpec.describe 'Api::Internal::DeliveryTargetsController', type: :request do
     context 'not login' do
       it 'should return 401' do
         post '/api/internal/delivery_targets', params: params
+        expect(response.status).to eq 401
+      end
+    end
+  end
+
+  describe 'DELETE /api/internal/delivery_targets/:public_id' do
+    context 'login as merchant_user' do
+      it 'should return 200' do
+        allow_any_instance_of(ApplicationController).to receive(:current_end_user).and_return(end_user)
+        delete "/api/internal/delivery_targets/#{delivery_target.public_id}"
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'not login' do
+      it 'should return 401' do
+        delete "/api/internal/delivery_targets/#{delivery_target.public_id}"
         expect(response.status).to eq 401
       end
     end
