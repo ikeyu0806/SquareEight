87ed69f1a487006dd6dbac12de636b0b3bf760b8
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-23 17:31:01 +0900

    add confirm_verification_code request spec

--- rails_api/spec/factories/end_users.rb
+++ rails_api/spec/factories/end_users.rb
@@ -5,5 +5,7 @@ FactoryBot.define do
     first_name { '一郎' }
     password { 'Pass1234' }
     email_authentication_status { 'Enabled' }
+    verification_code { '123456' }
+    verification_code_expired_at { Time.zone.now + 1.days }
   end
 end
