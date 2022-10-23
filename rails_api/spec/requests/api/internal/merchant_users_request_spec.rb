2652b643a57412c89d13c8652c6df0631139b90a
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-23 20:55:15 +0900

    add merchant_user current_merchant_user_info test

--- rails_api/spec/requests/api/internal/merchant_users_request_spec.rb
+++ rails_api/spec/requests/api/internal/merchant_users_request_spec.rb
@@ -146,4 +146,21 @@ RSpec.describe 'Api::Internal::MerchantUserController', type: :request do
       end
     end
   end
+
+  describe 'GET /api/internal/merchant_users/current_merchant_user_info' do
+    context 'login as merchant_user' do
+      it 'should return 200' do
+        allow_any_instance_of(ApplicationController).to receive(:current_merchant_user).and_return(merchant_user)
+        get '/api/internal/merchant_users/current_merchant_user_info'
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'not login' do
+      it 'should return 401' do
+        get '/api/internal/merchant_users/current_merchant_user_info'
+        expect(response.status).to eq 401
+      end
+    end
+  end
 end
