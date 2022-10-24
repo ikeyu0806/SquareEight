57aa31009866a73dd7ae76d634440c8c04985458
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-24 17:10:25 +0900

    create questionnaire_master request spec

---
+++ rails_api/spec/requests/api/internal/questionnaire_masters_request.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Api::Internal::QuestionnaireMastersController', type: :request do
+  let(:account) { create(:business_account) }
+  let(:merchant_user) {
+    create(:merchant_user, account: account)
+  }
+  let(:customer) { create(:customer, account_id: account.id) }
+  let!(:questionnaire_master) { create(:questionnaire_master, account: account) }
+
+  describe 'GET /api/internal/questionnaire_masters' do
+    context 'login as merchant_user' do
+      it 'should return 200' do
+        allow_any_instance_of(ApplicationController).to receive(:current_merchant_user).and_return(merchant_user)
+        get '/api/internal/questionnaire_masters'
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'not login' do
+      it 'should return 401' do
+        get '/api/internal/questionnaire_masters'
+        expect(response.status).to eq 401
+      end
+    end
+  end
+end
