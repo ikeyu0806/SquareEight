52d956253ca66c3d4f2b0467beab66a6f1b22709
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-24 17:28:18 +0900

    add questionnaire_master update action

--- rails_api/spec/requests/api/internal/questionnaire_masters_request.rb
+++ rails_api/spec/requests/api/internal/questionnaire_masters_request.rb
@@ -50,7 +50,38 @@ RSpec.describe 'Api::Internal::QuestionnaireMastersController', type: :request d
 
     context 'not login' do
       it 'should return 401' do
-        get '/api/internal/questionnaire_masters'
+        post '/api/internal/questionnaire_masters', params: params
+        expect(response.status).to eq 401
+      end
+    end
+  end
+
+  describe 'POST /api/internal/questionnaire_masters/:public_id/update' do
+    let(:params) {
+      {
+        questionnaire_master: {
+          title: 'title update',
+          description: 'description update',
+          publish_status: 'Publish',
+          question_form_json: [ question: 'いつ',
+                                formType: 'date',
+                                textFormRowCount: 1,
+                                sortOrder: 1,
+                                questionId: '183f999960f']
+        }
+      }
+    }
+    context 'login as merchant_user' do
+      it 'should return 200' do
+        allow_any_instance_of(ApplicationController).to receive(:current_merchant_user).and_return(merchant_user)
+        post "/api/internal/questionnaire_masters/#{questionnaire_master.public_id}/update", params: params
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'not login' do
+      it 'should return 401' do
+        post "/api/internal/questionnaire_masters/#{questionnaire_master.public_id}/update", params: params
         expect(response.status).to eq 401
       end
     end
