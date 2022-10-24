2c8141c014d00b5d90591f1fcd469ad938a946f0
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-24 11:05:15 +0900

    add monthly_payment_plan test

--- rails_api/spec/requests/api/internal/monthly_payment_plans_request.rb
+++ rails_api/spec/requests/api/internal/monthly_payment_plans_request.rb
@@ -23,4 +23,61 @@ RSpec.describe 'Api::Internal::MonthlyPaymentPlansController', type: :request do
       end
     end
   end
+
+  describe 'GET /api/internal/monthly_payment_plan/:public_id' do
+    context 'login as merchant_user' do
+      it 'should return 200' do
+        allow_any_instance_of(ApplicationController).to receive(:current_merchant_user).and_return(merchant_user)
+        get "/api/internal/monthly_payment_plans/#{monthly_payment_plan.public_id}"
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'not login' do
+      it 'should return 401' do
+        get "/api/internal/monthly_payment_plans/#{monthly_payment_plan.public_id}"
+        expect(response.status).to eq 401
+      end
+    end
+  end
+
+  describe 'GET /api/internal/monthly_payment_plans/:public_id/purchase_info' do
+    context 'not login' do
+      it 'should return 200' do
+        get "/api/internal/monthly_payment_plans/#{monthly_payment_plan.public_id}/purchase_info"
+        expect(response.status).to eq 200
+      end
+    end
+  end
+
+  describe 'POST /api/internal/monthly_payment_plans' do
+    let(:params) {
+      {
+        monthly_payment_plans: {
+          name: 'demo_name',
+          price: 1000,
+          reserve_is_unlimited: false,
+          enable_reserve_count: 1,
+          reserve_interval_number: 1,
+          reserve_interval_unit: 'Week',
+          publish_status: 'Publish',
+        }
+      }
+    }
+
+    context 'login as merchant_user' do
+      it 'should return 200' do
+        allow_any_instance_of(ApplicationController).to receive(:current_merchant_user).and_return(merchant_user)
+        post '/api/internal/monthly_payment_plans', params: params
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'not login' do
+      it 'should return 401' do
+        post '/api/internal/monthly_payment_plans', params: params
+        expect(response.status).to eq 401
+      end
+    end
+  end
 end
