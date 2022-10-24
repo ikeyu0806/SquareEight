797d63d4304c44cf0052f591ec0f61fccc8f245c
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-24 17:31:25 +0900

    add questionnaire_master request spec

--- rails_api/spec/requests/api/internal/questionnaire_masters_request.rb
+++ rails_api/spec/requests/api/internal/questionnaire_masters_request.rb
@@ -86,4 +86,30 @@ RSpec.describe 'Api::Internal::QuestionnaireMastersController', type: :request d
       end
     end
   end
+
+  describe 'GET /api/internal/questionnaire_masters/:public_id' do
+    context 'not login' do
+      it 'should return 200' do
+        get "/api/internal/questionnaire_masters/#{questionnaire_master.public_id}"
+        expect(response.status).to eq 200
+      end
+    end
+  end
+
+  describe 'GET /api/internal/questionnaire_masters/:public_id/edit_info' do
+    context 'not login' do
+      it 'should return 200' do
+        allow_any_instance_of(ApplicationController).to receive(:current_merchant_user).and_return(merchant_user)
+        get "/api/internal/questionnaire_masters/#{questionnaire_master.public_id}/edit_info"
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'not login' do
+      it 'should return 401' do
+        get "/api/internal/questionnaire_masters/#{questionnaire_master.public_id}/edit_info"
+        expect(response.status).to eq 401
+      end
+    end
+  end
 end
