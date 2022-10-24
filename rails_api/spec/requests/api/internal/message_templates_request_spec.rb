76c4101b35242802f4a466b723b2cd671abd27af
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-24 09:23:38 +0900

    add create test to message_template

--- rails_api/spec/requests/api/internal/message_templates_request_spec.rb
+++ rails_api/spec/requests/api/internal/message_templates_request_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'Api::Internal::MessageTemplatesController', type: :request do
   let(:customer_group) { create(:customer_group, account_id: account.id) }
   let!(:customer_group_relation) { create(:customer_group_relation, customer_id: customer.id, customer_group_id: customer_group.id) }
 
-  describe 'GET /api/internal/merchant_users/current_merchant_user_info' do
+  describe 'GET /api/internal/message_templates' do
     context 'login as merchant_user' do
       it 'should return 200' do
         allow_any_instance_of(ApplicationController).to receive(:current_merchant_user).and_return(merchant_user)
@@ -21,6 +21,33 @@ RSpec.describe 'Api::Internal::MessageTemplatesController', type: :request do
     context 'not login' do
       it 'should return 401' do
         get '/api/internal/message_templates'
+        expect(response.status).to eq 401
+      end
+    end
+  end
+
+  describe 'POST /api/internal/message_templates' do
+    let(:params) {
+      {
+        message_template: {
+          name: 'demo_name',
+          title: 'demo_title',
+          content: 'demo_content',
+        }
+      }
+    }
+
+    context 'login as merchant_user' do
+      it 'should return 200' do
+        allow_any_instance_of(ApplicationController).to receive(:current_merchant_user).and_return(merchant_user)
+        post '/api/internal/message_templates', params: params
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'not login' do
+      it 'should return 401' do
+        post '/api/internal/message_templates', params: params
         expect(response.status).to eq 401
       end
     end
