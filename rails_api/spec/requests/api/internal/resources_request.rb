fe96de0792c1f2e7e312bc8848a0f05816bf917c
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-25 12:00:17 +0900

    add resource request test

--- rails_api/spec/requests/api/internal/resources_request.rb
+++ rails_api/spec/requests/api/internal/resources_request.rb
@@ -23,4 +23,73 @@ RSpec.describe 'Api::Internal::ResourcesController', type: :request do
       end
     end
   end
+
+  describe 'GET /api/internal/resources/:public_id/edit' do
+    context 'login as merchant_user' do
+      it 'should return 200' do
+        allow_any_instance_of(ApplicationController).to receive(:current_merchant_user).and_return(merchant_user)
+        get "/api/internal/resources/#{room_resource.public_id}/edit"
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'not login' do
+      it 'should return 401' do
+        get "/api/internal/resources/#{room_resource.public_id}/edit"
+        expect(response.status).to eq 401
+      end
+    end
+  end
+
+  describe 'POST /api/internal/resources' do
+    let(:params) {
+      {
+        resources: {
+          name: '山田花子',
+          quantity: 5
+        }
+      }
+    }
+
+    context 'login as merchant_user' do
+      it 'should return 200' do
+        allow_any_instance_of(ApplicationController).to receive(:current_merchant_user).and_return(merchant_user)
+        post '/api/internal/resources', params: params
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'not login' do
+      it 'should return 401' do
+        post '/api/internal/resources', params: params
+        expect(response.status).to eq 401
+      end
+    end
+  end
+
+  describe 'POST /api/internal/resources/:public_id/update' do
+    let(:params) {
+      {
+        resources: {
+          name: '会議室更新',
+          quantity: 15
+        }
+      }
+    }
+
+    context 'login as merchant_user' do
+      it 'should return 200' do
+        allow_any_instance_of(ApplicationController).to receive(:current_merchant_user).and_return(merchant_user)
+        post "/api/internal/resources/#{room_resource.public_id}/update", params: params
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'not login' do
+      it 'should return 401' do
+        post "/api/internal/resources/#{room_resource.public_id}/update", params: params
+        expect(response.status).to eq 401
+      end
+    end
+  end
 end
