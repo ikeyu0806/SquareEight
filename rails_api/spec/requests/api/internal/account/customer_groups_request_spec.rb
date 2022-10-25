c50c327f4bd00064947477e6cc86306f1c4e2f81
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-25 17:54:31 +0900

    create account/customer_groups_request_spec

--- rails_api/spec/requests/api/internal/account/customer_groups_request_spec.rb
+++ rails_api/spec/requests/api/internal/account/customer_groups_request_spec.rb
@@ -26,4 +26,71 @@ RSpec.describe 'Api::Internal::CustomersController', type: :request do
       end
     end
   end
+
+  describe 'GET /api/internal/account/customer_groups/:public_id' do
+    context 'login as merchant_user' do
+      it 'should return 200' do
+        allow_any_instance_of(ApplicationController).to receive(:current_merchant_user).and_return(merchant_user)
+        get '/api/internal/account/customer_groups/' + customer_group.public_id
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'not login' do
+      it 'should return 401' do
+        get '/api/internal/account/customer_groups/' + customer_group.public_id
+        expect(response.status).to eq 401
+      end
+    end
+  end
+
+  describe 'POST /api/internal/account/customer_groups' do
+    let(:params) {
+      {
+        customer_group: {
+          name: 'demo customer_group',
+          selected_customers: [customer]
+        }
+      }
+    }
+    context 'login as merchant_user' do
+      it 'should return 200' do
+        allow_any_instance_of(ApplicationController).to receive(:current_merchant_user).and_return(merchant_user)
+        post '/api/internal/account/customer_groups', params: params
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'not login' do
+      it 'should return 401' do
+        post '/api/internal/account/customer_groups', params: params
+        expect(response.status).to eq 401
+      end
+    end
+  end
+
+  describe 'POST /api/internal/account/customer_groups' do
+    let(:params) {
+      {
+        customer_group: {
+          name: 'update demo customer_group',
+          selected_customers: [customer]
+        }
+      }
+    }
+    context 'login as merchant_user' do
+      it 'should return 200' do
+        allow_any_instance_of(ApplicationController).to receive(:current_merchant_user).and_return(merchant_user)
+        post "/api/internal/account/customer_groups/#{customer_group.public_id}/update", params: params
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'not login' do
+      it 'should return 401' do
+        post "/api/internal/account/customer_groups/#{customer_group.public_id}/update", params: params
+        expect(response.status).to eq 401
+      end
+    end
+  end
 end
