09daa375218567677f35244b46fd00478c5942db
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-25 16:17:33 +0900

    add shared_component register test

--- rails_api/spec/requests/api/internal/shared_components_request.rb
+++ rails_api/spec/requests/api/internal/shared_components_request.rb
@@ -23,4 +23,33 @@ RSpec.describe 'Api::Internal::SharedComponentsController', type: :request do
       end
     end
   end
+
+  describe 'POST /api/internal/shared_components/register' do
+    let(:params) {
+      {
+        shared_component: {
+          navbar_brand_text: 'Update Test',
+          navbar_brand_type: 'text',
+          navbar_brand_background_color: 'dark',
+          navbar_brand_variant_color: 'dark',
+          footer_copyright_text: 'Update Test'
+        }
+      }
+    }
+
+    context 'login as merchant_user' do
+      it 'should return 200' do
+        allow_any_instance_of(ApplicationController).to receive(:current_merchant_user).and_return(merchant_user)
+        post '/api/internal/shared_components/register', params: params
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'not login' do
+      it 'should return 401' do
+        post '/api/internal/shared_components/register', params: params
+        expect(response.status).to eq 401
+      end
+    end
+  end
 end
