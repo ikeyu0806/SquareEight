92d6089a88d541fc588d3c78d6a2e38d016cf075
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-26 16:38:32 +0900

    replace reserveFrame index page table to listItem

--- frontend/pages/admin/reserve_frame/index.tsx
+++ frontend/pages/admin/reserve_frame/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useCookies } from 'react-cookie'
-import { Container, Table, Button } from 'react-bootstrap'
+import { Container, Table, Button, ListGroup, Row, Col } from 'react-bootstrap'
 import MerchantUserAdminLayout from 'components/templates/MerchantUserAdminLayout'
 import axios from 'axios'
 import { useDispatch } from 'react-redux'
@@ -48,61 +48,51 @@ const Index = (): JSX.Element => {
           <Button
             className='mb30'
             onClick={() => dispatch(showCreateReserveFrameModalChanged(true))}>新規登録</Button>
-          <Table bordered>
-            <thead>
-              <tr>
-                <th className='text-center'>予約メニュー名</th>
-                <th className='text-center'>定員</th>
-                <th className='text-center'>開始日-終了日/繰り返し設定</th>
-                <th className='text-center'>お支払い方法</th>
-                <th className='text-center'>公開設定/受付設定</th>
-                <th className='text-center'></th>
-                <th className='text-center'></th>
-              </tr>
-            </thead>
-            <tbody>
-              {reserveFrames.map((reserveFrame, i) => {
-                return (
-                  <tr key={i}>
-                    <td className='text-center'>
-                      {reserveFrame.title}
-                    </td>
-                    <td className='text-center'>
-                      {reserveFrame.capacity}
-                    </td>
-                    <td className='text-center'>
-                      {reserveFrame.display_start_at}{reserveFrame.display_end_at && <><br/>{reserveFrame.display_end_at}</>}<br/>
-                      {reserveFrame.repeat_setting_text}
-                    </td>
-                    <td className='text-center'>
+          <ListGroup>
+            {reserveFrames.map((reserveFrame, i) => {
+              return (
+                <ListGroup.Item key={i}>
+                  <Row>
+                    <Col>
+                      <div>予約メニュー名: {reserveFrame.title}</div>
+                      <hr />
+                      <div>定員: {reserveFrame.capacity}</div>
+                      <hr />
+                      <div>開始日-終了日</div>
+                      <div>{reserveFrame.display_start_at}{reserveFrame.display_end_at && <><br/>{reserveFrame.display_end_at}</>}<br/></div>
+                      <hr />
+                      <div>お支払い方法</div>
+                      <div>
                       {reserveFrame.payment_methods_text && reserveFrame.payment_methods_text.map((text, i) => {
                         return (
                           <span key={i}>{text}<br/></span>
                         )
-                      })}
-                    </td>
-                    <td className='text-center'>
-                      {reserveFrame.publish_status === 'Publish' ? '公開' : '非公開'}<br/>
-                      {reserveFrame.reception_type_text}
-                    </td>
-                    <td>
-                      <a className='btn btn-primary'
+                      })}</div>
+                    </Col>
+                    <Col>
+                      <div>公開設定/受付設定</div>
+                      <div>
+                        {reserveFrame.publish_status === 'Publish' ? '公開' : '非公開'}<br/>
+                        {reserveFrame.reception_type_text}
+                      </div>
+                      <hr />
+                      <div>
+                        <a className='btn btn-primary'
                          onClick={() => {
                           dispatch(showEditReserveFrameModalChanged(true))
                           dispatch(publicIdChanged(reserveFrame.public_id))
                         }}>編集</a>
-                    </td>
-                    <td>
-                    <a className='btn btn-primary ml10'
-                       target='_blank'
-                       rel='noreferrer'
-                       href={`/reserve_frame/${reserveFrame.public_id}/calendar`}>プレビュー</a>
-                    </td>
-                  </tr>
-                )
-              })}
-            </tbody>
-          </Table>
+                        <a className='btn btn-primary ml20'
+                           target='_blank'
+                           rel='noreferrer'
+                           href={`/reserve_frame/${reserveFrame.public_id}/calendar`}>プレビュー</a>
+                      </div>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              )
+            })}
+          </ListGroup>
           <CreateReserveFrameModal></CreateReserveFrameModal>
           <EditReserveFrameModal></EditReserveFrameModal>
         </Container>
