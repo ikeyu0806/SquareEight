bc9213702bd0ae2ad64ad7f71a08504ae37339d3
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-28 11:40:04 +0900

    create reserve_frame relation factory

---
+++ rails_api/spec/factories/reserve_frame_monthly_payment_plans.rb
@@ -0,0 +1,4 @@
+FactoryBot.define do
+  factory :reserve_frame_monthly_payment_plan, class: ReserveFrameMonthlyPaymentPlan do
+  end
+end
---
+++ rails_api/spec/factories/reserve_frame_ticket_masters.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :reserve_frame_ticket_master, class: ReserveFrameTicketMaster do
+    consume_number { 1 }
+  end
+end
