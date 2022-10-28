f177502d88d7d5a68a9309f33ac0b93944d2584a
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-28 11:00:11 +0900

    create payment_methods test

--- rails_api/spec/models/reserve_frame.rb
+++ rails_api/spec/models/reserve_frame.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe ReserveFrame, type: :model do
+  let(:account) { create(:business_account) }
+  let(:reserve_frame) { create(:reserve_frame, account: account) }
+
   describe 'payment_methods' do
-    it 'should return expect text' do
+    it 'should return expect value' do
+      payment_methods = reserve_frame.payment_methods
+      expect(payment_methods[:local_payment_price]).to eq 1000
+      expect(payment_methods[:credit_card_payment_price]).to eq 1000
     end
   end
 end
