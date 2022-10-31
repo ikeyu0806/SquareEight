119060346f2812b7cd21b57076f3aeb7aaf70e0d
ikeyu0806 <ikeyu0806@gmail.com>
2022-10-31 17:32:08 +0900

    fix remaining_capacity_count_within_range bug

--- rails_api/app/models/reserve_frame.rb
+++ rails_api/app/models/reserve_frame.rb
@@ -191,7 +191,7 @@ class ReserveFrame < ApplicationRecord
   end
 
   def remaining_capacity_count_within_range(start_datetime, end_datetime)
-    reservation_count = self.reservations.where(start_at: start_datetime, end_at: end_datetime, status: 'confirm').count
+    reservation_count = self.reservations.where(start_at: start_datetime, end_at: end_datetime, status: 'confirm').pluck(:number_of_people).inject(:+)
     return self.capacity - reservation_count
   end
 
