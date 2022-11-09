7b22a21a56dce87231d848a67f53c3fb3dba68ed
ikeyu0806 <ikeyu0806@gmail.com>
2022-11-09 09:09:32 +0900

    fix payment_request complete mail, add loading state to payment

--- rails_api/app/controllers/api/internal/payment_requests_controller.rb
+++ rails_api/app/controllers/api/internal/payment_requests_controller.rb
@@ -3,7 +3,7 @@ class Api::Internal::PaymentRequestsController < ApplicationController
   before_action :end_user_login_only!, only: [:exec_payment]
 
   def index
-    payment_requests = current_merchant_user.account.stripe_payment_requests
+    payment_requests = current_merchant_user.account.stripe_payment_requests.order(id: :desc)
     payment_requests = JSON.parse(payment_requests.to_json(methods: [:display_status, :request_url, :billing_customer_name, :billing_customer_email]))
     render json: {  status: 'success', payment_requests: payment_requests }, status: 200
   rescue => error
