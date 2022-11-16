431439d8431771dae95550afd0ff24e4a792f6cd
ikeyu0806 <ikeyu0806@gmail.com>
2022-11-16 14:04:46 +0900

    add ses setting

--- rails_api/config/environments/production.rb
+++ rails_api/config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :ses
-  config.action_mailer.default_url_options = { host: ENV["AWS_SES_DOMAIN"] }
+  config.action_mailer.default_url_options = { host: ENV["AWS_SES_DOMAIN"], :protocol=>"https" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
