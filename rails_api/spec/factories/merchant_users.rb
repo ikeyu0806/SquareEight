1467e09a29fbd3716edce99f0d8a232c9415afab
ikeyu0806 <ikeyu0806@gmail.com>
2022-12-01 10:18:13 +0900

    fix merchant_user factory

--- rails_api/spec/factories/merchant_users.rb
+++ rails_api/spec/factories/merchant_users.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     last_name { 'デモ' }
     first_name { '一郎' }
     password { 'Pass1234' }
-    authority_category { 'MerchantAdmin' }
+    authority_category { 'RootUser' }
     email_authentication_status { 'Enabled' }
     verification_code { '123456' }
     verification_code_expired_at { Time.zone.now + 1.days }
