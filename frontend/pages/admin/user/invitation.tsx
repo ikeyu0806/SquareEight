7ec0c21406edcfd2472a40a86c7895c7a34e2dba
ikeyu0806 <ikeyu0806@gmail.com>
2022-12-01 10:56:44 +0900

    add form to user/invirtation page

--- frontend/pages/admin/user/invitation.tsx
+++ frontend/pages/admin/user/invitation.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import MerchantUserAdminLayout from 'components/templates/MerchantUserAdminLayout'
-import { Container, Row, Col, Table, Card, Button } from 'react-bootstrap'
+import { Container, Row, Col, Button, Form } from 'react-bootstrap'
 import { showPermissionGuideModalChanged } from 'redux/merchantUserPermissionSlice'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from 'redux/store'
@@ -14,11 +14,25 @@ const Invitation: NextPage = () => {
     <MerchantUserAdminLayout>
       <Container>
         <Row>
-          <Col md={2}></Col>
-          <Col md={8}>
-            <Button onClick={() => dispatch(showPermissionGuideModalChanged(true))}>
-              ユーザ権限について
-            </Button>
+          <Col md={3}></Col>
+          <Col md={6}>
+            <div className='mb20'>
+              <Button onClick={() => dispatch(showPermissionGuideModalChanged(true))}>
+                ユーザ権限について
+              </Button>
+            </div>
+            <Form.Label>名前(姓)</Form.Label>
+            <Form.Control></Form.Control>
+            <Form.Label>名前(名)</Form.Label>
+            <Form.Control></Form.Control>
+            <Form.Label>メールアドレス</Form.Label>
+            <Form.Control></Form.Control>
+            <Form.Label>管理者権限</Form.Label>
+            <Form.Select>
+              <option>管理者</option>
+              <option>一般ユーザ</option>
+            </Form.Select>
+            <Button className='mt30'>登録する</Button>
           </Col>
         </Row>
       </Container>
