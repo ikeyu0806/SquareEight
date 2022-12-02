ddb55f9c2981f12ac24503ca45c38617530b8d66
ikeyu0806 <ikeyu0806@gmail.com>
2022-12-02 13:45:29 +0900

    add permission switch button

--- frontend/pages/admin/user/permission.tsx
+++ frontend/pages/admin/user/permission.tsx
@@ -1,10 +1,58 @@
 import type { NextPage } from 'next'
 import MerchantUserAdminLayout from 'components/templates/MerchantUserAdminLayout'
+import { Container, Row, Col, ListGroup, Form } from 'react-bootstrap'
 
 const Permission: NextPage = () => {
   return (
     <MerchantUserAdminLayout>
-
+      <Container>
+        <Row>
+          <Col lg={4}>
+          </Col>
+          <Col lg={4}>
+            <ListGroup>
+              <ListGroup.Item>
+                <Row>
+                  <Col>
+                    ユーザ閲覧
+                  </Col>
+                  <Col>
+                    <Row>
+                      <Col sm={2}></Col>
+                      <Col>
+                        <Form.Check type='switch'></Form.Check>
+                      </Col>
+                      <Col>
+                        <span>有効</span>
+                      </Col>
+                      <Col sm={1}></Col>
+                    </Row>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>
+                    ユーザ登録
+                  </Col>
+                  <Col>
+                    <Row>
+                      <Col sm={2}></Col>
+                      <Col>
+                        <Form.Check type='switch'></Form.Check>
+                      </Col>
+                      <Col>
+                        <span>有効</span>
+                      </Col>
+                      <Col sm={1}></Col>
+                    </Row>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
     </MerchantUserAdminLayout>
   )
 }
