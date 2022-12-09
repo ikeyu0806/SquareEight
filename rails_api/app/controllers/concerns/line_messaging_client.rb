c929e66319fe5a7adc79e5968c13bec7c5346577
ikeyu0806 <ikeyu0806@gmail.com>
2022-12-09 11:47:20 +0900

    create LINEClientModule

---
+++ rails_api/app/controllers/concerns/line_messaging_client.rb
@@ -0,0 +1,11 @@
+module LINEClientModule
+  extend ActiveSupport::Concern
+
+  def line_messaging_client
+      @line_messaging_client ||= Line::Bot::Client.new { |config|
+        config.channel_id = ENV["LINE_CHANNEL_ID"]
+        config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+        config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      }
+  end
+end
