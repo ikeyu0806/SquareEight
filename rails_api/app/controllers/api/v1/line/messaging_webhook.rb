20b7187208a83957164621bc2fd0307a929f29f0
ikeyu0806 <ikeyu0806@gmail.com>
2022-12-09 13:10:59 +0900

    fix line webhook controller name

--- rails_api/app/controllers/api/v1/line/messaging_webhook.rb
+++
@@ -1,7 +0,0 @@
-include LINEClientModule
-
-class Api::V1::Line::MessagingWebhookController < ApplicationController
-  def index
-    @line_messaging_client
-  end
-end
---
+++ rails_api/app/controllers/api/v1/line/messaging_webhook_controller.rb
@@ -0,0 +1,34 @@
+include LINEClientModule
+
+class Api::V1::Line::MessagingWebhookController < ApplicationController
+  def index
+    @line_messaging_client
+
+    binding.pry
+    body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
+  
+    events = client.parse_events_from(body)
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          response = client.get_message_content(event.message['id'])
+          tf = Tempfile.open("content")
+          tf.write(response.body)
+        end
+      end
+    end
+  end
+end
