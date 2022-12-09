3c07a2e18564ca253b49d1f02334ef5ac5bff84e
ikeyu0806 <ikeyu0806@gmail.com>
2022-12-09 16:55:21 +0900

    show webhook url to line_official_account index page

---
+++ frontend/interfaces/LineOfficialAccountParam.tsx
@@ -0,0 +1,6 @@
+export interface LineOfficialAccountParam {
+  channel_id: string
+  channel_secret: string
+  channel_token: string
+  messaging_api_webhook_url: string
+}
--- frontend/pages/admin/line_official_account/index.tsx
+++ frontend/pages/admin/line_official_account/index.tsx
@@ -2,8 +2,28 @@ import React, { useEffect, useState } from 'react'
 import { NextPage } from 'next'
 import { Container, Row, Col, ListGroup, Card, Button, Form, Alert } from 'react-bootstrap'
 import MerchantUserAdminLayout from 'components/templates/MerchantUserAdminLayout'
+import { LineOfficialAccountParam } from 'interfaces/LineOfficialAccountParam'
+import axios from 'axios'
+import { useCookies } from 'react-cookie'
 
 const Index: NextPage = () => {
+  const [cookies] = useCookies(['_square_eight_merchant_session'])
+  const [lineOfficialAccounts, setLineOfficialAccounts] = useState<LineOfficialAccountParam[]>([])
+
+  useEffect(() => {
+    axios.get(`${process.env.BACKEND_URL}/api/internal/line_official_accounts`,
+    {
+      headers: {
+        'Session-Id': cookies._square_eight_merchant_session
+      }
+    }).then((response) => {
+      console.log(response.data)
+      setLineOfficialAccounts(response.data.line_official_accounts)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }, [cookies._square_eight_merchant_session])
+
   return (
     <MerchantUserAdminLayout>
       <Container>
@@ -11,6 +31,14 @@ const Index: NextPage = () => {
           <Col lg={2}></Col>
           <Col lg={8}>
             <a className='btn btn-primary' href='/admin/line_official_account/new'>LINE公式アカウントを登録する</a>
+            <div>公式アカウント一覧</div>
+            <ListGroup>
+              {lineOfficialAccounts.map((account, i) => {
+                return (
+                  <ListGroup.Item key={i}>{account.messaging_api_webhook_url}</ListGroup.Item>
+                )
+              })}
+            </ListGroup>
           </Col>
         </Row>
       </Container>
