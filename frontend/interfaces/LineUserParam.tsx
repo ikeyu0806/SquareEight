3854510089a5337eadf79a085c8962355f715243
ikeyu0806 <ikeyu0806@gmail.com>
2022-12-11 22:39:59 +0900

    show line_profile, line_display_name

---
+++ frontend/interfaces/LineUserParam.tsx
@@ -0,0 +1,5 @@
+export interface LineUserParam {
+  line_user_id: string
+  line_display_name: string
+  line_picture_url: string
+}
--- frontend/pages/admin/line_official_account/[public_id]/line_user.tsx
+++ frontend/pages/admin/line_official_account/[public_id]/line_user.tsx
@@ -5,18 +5,22 @@ import MerchantUserAdminLayout from 'components/templates/MerchantUserAdminLayou
 import axios from 'axios'
 import { useRouter } from 'next/router'
 import { useCookies } from 'react-cookie'
+import { LineUserParam } from 'interfaces/LineUserParam'
 
 const LineUser: NextPage = () => {
   const [cookies] = useCookies(['_square_eight_merchant_session'])
   const router = useRouter()
+  const [lineUsers, setLineUsers] = useState<LineUserParam[]>([])
 
   useEffect(() => {
-    axios.get(`${process.env.BACKEND_URL}/api/internal/line_official_accounts/${router.query.public_id}/line_user`,
+    axios.get(`${process.env.BACKEND_URL}/api/internal/line_official_accounts/${router.query.public_id}/line_users`,
     {
       headers: {
         'Session-Id': cookies._square_eight_merchant_session
       }
     }).then((response) => {
+      console.log(response.data)
+      setLineUsers(response.data.line_users)
     }).catch((error) => {
       console.log(error)
     })
@@ -28,7 +32,25 @@ const LineUser: NextPage = () => {
         <Row>
           <Col lg={2}></Col>
           <Col lg={8}>
-
+            <ListGroup>
+              {lineUsers.map((line_user, i) => {
+                return (
+                  <ListGroup.Item key={i}>
+                    <Row>
+                      <Col>
+                        <img
+                          className='linePictureUrl'
+                          src={line_user.line_picture_url}
+                          alt='line_picture_url' />
+                      </Col>
+                      <Col>
+                        <div>{line_user.line_display_name}</div>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )
+              })}
+            </ListGroup>
           </Col>
         </Row>
       </Container>
