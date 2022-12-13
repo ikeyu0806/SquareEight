a85bea7bfbbd2f69ace857b2429cd15403898a7a
ikeyu0806 <ikeyu0806@gmail.com>
2022-12-13 15:55:45 +0900

    add paymentRequest variable to customer send mail

--- rails_api/app/controllers/api/internal/customers_controller.rb
+++ rails_api/app/controllers/api/internal/customers_controller.rb
@@ -14,12 +14,15 @@ class Api::Internal::CustomersController < ApplicationController
   def send_mail
     customer = Customer.find_by(public_id: params[:public_id])
     title = customer_params[:mail_title]
+    price = ''
+    payment_request_url = ''
+
     if customer_params[:is_send_payment_request]
       price = customer_params[:price]
       stripe_payment_request = current_merchant_user.account.stripe_payment_requests.create!(name: customer_params[:payment_request_name], price: price, customer: customer)
       payment_request_url = ENV["FRONTEND_URL"] + '/payment_request/' + stripe_payment_request.public_id
     end
-    content = MessageTemplate.convert_content(customer_params[:message], customer.last_name, customer.first_name)
+    content = MessageTemplate.convert_content(customer_params[:message], customer.last_name, customer.first_name, price, payment_request_url)
     MessageTemplateMailer.send_mail(customer.email, title, content).deliver_now
     render json: { status: 'success' }, status: 200
   rescue => error
