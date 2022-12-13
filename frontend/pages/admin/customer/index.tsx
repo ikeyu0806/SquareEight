30dca1bbb3126261e03951910f101aeb10109209
ikeyu0806 <ikeyu0806@gmail.com>
2022-12-13 16:11:04 +0900

    apply table customer index component

--- frontend/pages/admin/customer/index.tsx
+++ frontend/pages/admin/customer/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import React, { useEffect, useState } from 'react'
-import { Container, Row, Col, ListGroup, Card, Button, Form, Alert } from 'react-bootstrap'
+import { Container, Row, Col, ListGroup, Card, Button, Form, Table  } from 'react-bootstrap'
 import MerchantUserAdminLayout from 'components/templates/MerchantUserAdminLayout'
 import { useCookies } from 'react-cookie'
 import { CustomerParam } from 'interfaces/CustomerParam'
@@ -69,89 +69,89 @@ const Index: NextPage = () => {
   return (
     <>
       <MerchantUserAdminLayout>
-        {allowReadCustomer === 'Allow' && <Container>
-          <Row>
-            <Col lg={2}></Col>
-            <Col lg={8}>
-              {/* {servicePlan === 'Free' && <Alert variant='warning'>フリープランでは登録顧客の最大表示数は50件となります</Alert>} */}
-              <Card className='mb20'>
-                <Card.Header>絞り込み</Card.Header>
-                <Card.Body>
-                <Row>
-                  <Col lg={8} className='mb10'>
-                    <label>
-                      検索したい内容を入力してください<br/>
-                      (お名前・メールアドレス・電話番号)
-                    </label>
-                    <Form.Control className='mt20' onChange={(e) => setSearchWord(e.target.value)}></Form.Control>
-                    <Button className='mt20' size='lg' onClick={execSearch}>検索</Button>
-                  </Col>
-                </Row>
-                </Card.Body>
-              </Card>
-              {allowCreateCustomer === 'Allow' && <Button
-                className='mb20'
-                onClick={() => dispatch(showCreateCustomerModalChanged(true))}>顧客新規登録</Button>}
-              <h3>顧客一覧</h3>
-              <ListGroup>
-                
-                {customers.map((customer, i) => {
-                  return (
-                    <ListGroup.Item key={i}>
-                      <Row>
-                        <Col>
-                          <div>お名前: {customer.last_name}{customer.first_name}</div>
-                          <br></br>
-                          <div>メールアドレス: {customer.email}</div>
-                            <Col>
-                              <div>携帯電話番号: {customer.phone_number}</div>
-                            </Col>
-                            <Col></Col>
-                        </Col>
-                        <Col>
-                          <Row>
-                            <Col>
-                              {customer.email &&
-                                <Button
-                                  className='mb20'
-                                  onClick={() => {
-                                    dispatch(showCustomerMailSendModalChanged(true));
-                                    dispatch(customerPublicIdChanged(customer.public_id));
-                                  }}>メール送信</Button>}
-                              {allowUpdateCustomer === 'Allow' && <a onClick={() => showEditModal(customer)}
-                                className='btn btn-primary mb20'>
-                                編集
-                              </a>}
-                              <br/>
-                              {allowReadQuestionnaireAnswer === 'Allow' && <a href={`/admin/customer/${customer.public_id}/questionnaire_answers`}
-                                className='btn btn-primary'>
-                                アンケート回答
-                              </a>}
-                            </Col>
-                            <Col>
-                              <a href={`/admin/customer/${customer.public_id}/order`}
-                                className='btn btn-primary mb20'>
-                                注文履歴
-                              </a>
-                              <br/>
-                              <a href={`/admin/customer/${customer.public_id}/charges`}
-                                className='btn btn-primary'>
-                                お支払い履歴
-                              </a>
-                            </Col>
-                          </Row>
-                        </Col>
-                      </Row>
-                    </ListGroup.Item>
-                  )
-                })}
-              </ListGroup>
-            </Col>
-          </Row>
-          <CreateCustomerModal></CreateCustomerModal>
-          <EditCustomerModal></EditCustomerModal>
-          <CustomerMailSendModal></CustomerMailSendModal>
+        {allowReadCustomer === 'Allow' &&
+        <Container>
+        <>
+          <Card className='mb20'>
+            <Card.Header>絞り込み</Card.Header>
+            <Card.Body>
+            <Row>
+              <Col lg={8} className='mb10'>
+                <label>
+                  検索したい内容を入力してください<br/>
+                  (お名前・メールアドレス・電話番号)
+                </label>
+                <Form.Control className='mt20' onChange={(e) => setSearchWord(e.target.value)}></Form.Control>
+                <Button className='mt20' size='lg' onClick={execSearch}>検索</Button>
+              </Col>
+            </Row>
+            </Card.Body>
+          </Card>
+          <Table>
+            <thead>
+              <tr>
+                <th>お名前</th>
+                <th>メールアドレス</th>
+                <th>携帯電話番号</th>
+                <th>LINE</th>
+                <th>アンケート</th>
+                <th>注文/お支払い</th>
+                <th>編集</th>
+              </tr>
+            </thead>
+            <tbody>
+            {customers.map((customer, i) => {
+              return (
+                <tr key={i}>
+                  <td>{customer.last_name}{customer.first_name}</td>
+                  <td>{customer.email &&
+                    <>
+                      <div>{customer.email}</div>
+                      <Button
+                        variant='secondary'
+                        className='mt10'
+                        size='sm'
+                        onClick={() => {
+                          dispatch(showCustomerMailSendModalChanged(true));
+                          dispatch(customerPublicIdChanged(customer.public_id));
+                        }}>メール送信</Button>
+                    </>}</td>
+                  <td>{customer.phone_number}</td>
+                  <td></td>
+                  <td>
+                    {allowReadQuestionnaireAnswer === 'Allow' &&
+                      <a href={`/admin/customer/${customer.public_id}/questionnaire_answers`}
+                        className='btn btn-secondary btn-sm'>
+                        アンケート回答
+                      </a>}
+                  </td>
+                  <td>
+                    <a href={`/admin/customer/${customer.public_id}/order`}
+                      className='btn btn-secondary btn-sm mb20'>
+                      注文履歴
+                    </a>
+                    <br/>
+                    <a href={`/admin/customer/${customer.public_id}/charges`}
+                      className='btn btn-secondary btn-sm'>
+                      お支払い履歴
+                    </a>
+                  </td>
+                  <td>
+                    {allowUpdateCustomer === 'Allow' && <a onClick={() => showEditModal(customer)}
+                      className='btn btn-secondary btn-sm mb20'>
+                      編集
+                    </a>}
+                  </td>
+                </tr>
+              )
+            })}
+            </tbody>
+          </Table>
+        </>
         </Container>}
+        <CreateCustomerModal></CreateCustomerModal>
+        <EditCustomerModal></EditCustomerModal>
+        <CustomerMailSendModal></CustomerMailSendModal>
         {allowReadCustomer === 'Forbid' && <Unauthorized />}
       </MerchantUserAdminLayout>
     </>
