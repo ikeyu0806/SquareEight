b4bd76f1cc5a848d306ca80daaec4c7f4fc3937d
ikeyu0806 <ikeyu0806@gmail.com>
2022-12-17 09:33:51 +0900

    create CreateImageWithTextListHtmlTemplate

---
+++ frontend/components/templates/CreateImageWithTextListHtmlTemplate.tsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react'
+import { Button, Form, Row, Col } from 'react-bootstrap'
+import { useRouter } from 'next/router'
+import { getBase64 } from 'functions/getBase64'
+import { ImageWithTextTemplateContent } from 'interfaces/HtmlMailTemplate'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from 'redux/store'
+import { htmlMailTemplateChanged, currentMaxSortOrderChanged } from 'redux/htmlMailTemplateSlice'
+import UpdateHtmlMailBlockStateIcons from 'components/organisms/UpdateHtmlMailBlockStateIcons'
+import htmlMailTemplateStyles from 'styles/HtmlMailTemplate.module.css'
+
+const CreateImageWithTextListHtmlTemplate = (): JSX.Element => {
+  const router = useRouter()
+  const dispatch = useDispatch()
+  const [image, setImage] = useState('/images/noimage.jpeg')
+  const [base64Image, setBase64Image] = useState<any>('')
+  const [draftText, setDraftText] = useState('')
+  const htmlMailTemplate = useSelector((state: RootState) => state.htmlMailTemplate.htmlMailTemplate)
+  const currentMaxSortOrder = useSelector((state: RootState) => state.htmlMailTemplate.currentMaxSortOrder)
+
+  useEffect(() => {
+    switch(router.query.template_type) {
+      case 'ImageWithText':
+        dispatch(htmlMailTemplateChanged({templateType: 'ImageWithText', content: [], }))
+        break
+      default:
+        console.log('invalid template_type')
+    }
+  }, [router.query.template_type, dispatch])
+
+  const handleChangeFile = (e: any) => {
+    const { files } = e.target
+    setImage(window.URL.createObjectURL(files[0]))
+    getBase64(files[0]).then(
+      data => setBase64Image(data)
+    )
+  }
+
+  const addTemplateContent = () => {
+    let blockID = new Date().getTime().toString(16)
+    let updateHtmlMailTemplate: ImageWithTextTemplateContent[]
+    dispatch(currentMaxSortOrderChanged(currentMaxSortOrder + 1))
+    updateHtmlMailTemplate = htmlMailTemplate.content
+    let additionalHtmlMailTemplate: ImageWithTextTemplateContent
+    additionalHtmlMailTemplate = { text: draftText, image: image, base64Image: base64Image, blockID: blockID, sortOrder: currentMaxSortOrder }
+    updateHtmlMailTemplate = [...updateHtmlMailTemplate, additionalHtmlMailTemplate]
+    dispatch(htmlMailTemplateChanged({content: updateHtmlMailTemplate, templateType: 'ImageWithText'}))
+  }
+  return (
+    <>
+      {htmlMailTemplate.content && htmlMailTemplate.content.map((c, i) => {
+          return [
+            <Row key={i}>
+              <Col>
+                <img
+                  className={htmlMailTemplateStyles.list_picture_url}
+                  src={c.base64Image}
+                  alt='image'
+                />
+              </Col>
+              <Col>
+                <div>{c.text}</div>
+              </Col>
+            </Row>,
+            <UpdateHtmlMailBlockStateIcons
+              key={i + 1}
+              blockID={c.blockID}
+              sortOrder={c.sortOrder}></UpdateHtmlMailBlockStateIcons>
+          ]
+        })}
+        <hr />
+        <Form.Group controlId='formFile' className='mb5 mt10'>
+          <Form.Label>画像をアップロード</Form.Label>
+          <Form.Control type='file' onChange={handleChangeFile} />
+        </Form.Group>
+        <div className='mt10'>テキスト</div>
+        <Form.Control
+          value={draftText}
+          onChange={(e) => setDraftText(e.target.value)}
+          as='textarea'
+          rows={20} />
+        <Button
+          onClick={addTemplateContent}
+          className='mt20'>本文に画像とテキストを追加</Button>
+    </>
+  )
+}
+
+export default CreateImageWithTextListHtmlTemplate
--- frontend/pages/admin/html_mail_template/new.tsx
+++ frontend/pages/admin/html_mail_template/new.tsx
@@ -1,57 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import type { NextPage } from 'next'
-import { Container, Button, Form, Row, Col } from 'react-bootstrap'
+import { Container, Button, Row, Col } from 'react-bootstrap'
 import MerchantUserAdminLayout from 'components/templates/MerchantUserAdminLayout'
 import { useRouter } from 'next/router'
-import { getBase64 } from 'functions/getBase64'
-import { ImageWithTextTemplateContent, HtmlMailTemplate } from 'interfaces/HtmlMailTemplate'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from 'redux/store'
-import { htmlMailTemplateChanged, currentMaxSortOrderChanged } from 'redux/htmlMailTemplateSlice'
-import UpdateHtmlMailBlockStateIcons from 'components/organisms/UpdateHtmlMailBlockStateIcons'
+import { htmlMailTemplateChanged } from 'redux/htmlMailTemplateSlice'
 import CreateImageWithTextHtmlTemplate from 'components/templates/CreateImageWithTextHtmlTemplate'
+import CreateImageWithTextListHtmlTemplate from 'components/templates/CreateImageWithTextListHtmlTemplate'
 
 const New: NextPage = () => {
   const router = useRouter()
   const dispatch = useDispatch()
-  const [imageLink, setImageLink] = useState('')
-  const [image, setImage] = useState('/images/noimage.jpeg')
-  const [base64Image, setBase64Image] = useState<any>('')
-  const [draftText, setDraftText] = useState('')
-  const [ImageWithTextTemplateContent, setImageWithTextTemplateContent] = useState<ImageWithTextTemplateContent>()
-
-  const [htmlMailTemplateDraft, setHtmlMailTemplateDraft] = useState<HtmlMailTemplate>()
   const htmlMailTemplate = useSelector((state: RootState) => state.htmlMailTemplate.htmlMailTemplate)
-  const currentMaxSortOrder = useSelector((state: RootState) => state.htmlMailTemplate.currentMaxSortOrder)
 
   useEffect(() => {
     switch(router.query.template_type) {
       case 'ImageWithText':
         dispatch(htmlMailTemplateChanged({templateType: 'ImageWithText', content: [], }))
         break
+      case 'ImageWithTextList':
+        dispatch(htmlMailTemplateChanged({templateType: 'ImageWithTextList', content: [], }))
+        break
       default:
-        console.log('invalid template_type')
+        dispatch(htmlMailTemplateChanged({templateType: 'ImageWithText', content: [], }))
     }
   }, [router.query.template_type, dispatch])
-
-  const handleChangeFile = (e: any) => {
-    const { files } = e.target
-    setImage(window.URL.createObjectURL(files[0]))
-    getBase64(files[0]).then(
-      data => setBase64Image(data)
-    )
-  }
-
-  const addTemplateContent = () => {
-    let blockID = new Date().getTime().toString(16)
-    let updateHtmlMailTemplate: ImageWithTextTemplateContent[]
-    dispatch(currentMaxSortOrderChanged(currentMaxSortOrder + 1))
-    updateHtmlMailTemplate = htmlMailTemplate.content
-    let additionalHtmlMailTemplate: ImageWithTextTemplateContent
-    additionalHtmlMailTemplate = { text: draftText, image: image, base64Image: base64Image, blockID: blockID, sortOrder: currentMaxSortOrder }
-    updateHtmlMailTemplate = [...updateHtmlMailTemplate, additionalHtmlMailTemplate]
-    dispatch(htmlMailTemplateChanged({content: updateHtmlMailTemplate, templateType: 'ImageWithText'}))
-  }
 
   return (
     <MerchantUserAdminLayout>
@@ -66,9 +40,10 @@ const New: NextPage = () => {
                 <Button>登録を完了する</Button>
               </Col>
             </Row>
-            {htmlMailTemplate.content.length === 0 && <div className='mt20 mb10'>本文が登録されていません</div>}
-            {htmlMailTemplate.templateType === 'ImageWithText'
+            {router.query.template_type === 'ImageWithText'
               && <CreateImageWithTextHtmlTemplate />}
+            {router.query.template_type === 'ImageWithTextList'
+              && <CreateImageWithTextListHtmlTemplate />}
           </Col>
         </Row>
       </Container>
