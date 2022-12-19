12d50929e5ca160d0505eff352186e85c4de82cf
ikeyu0806 <ikeyu0806@gmail.com>
2022-12-19 10:08:57 +0900

    add ImageWithTextList consition to HtmlMailTemplatePreview.tsx

--- frontend/components/organisms/HtmlMailTemplatePreview.tsx
+++ frontend/components/organisms/HtmlMailTemplatePreview.tsx
@@ -1,13 +1,15 @@
 import { useSelector } from 'react-redux'
 import { RootState } from 'redux/store'
 import { ImageWithTextListTypeTemplateContent, ImageWithTextTemplateContent } from 'interfaces/HtmlMailTemplate'
+import { Row, Col } from 'react-bootstrap'
 
 const HtmlMailTemplatePreview = (): JSX.Element => {
   const selectedHtmlMailTemplate = useSelector((state: RootState) => state.sendMail.selectedHtmlMailTemplate)
 
   return (
     <>
-      {selectedHtmlMailTemplate.content && (JSON.parse(selectedHtmlMailTemplate.content) as ImageWithTextTemplateContent[] | ImageWithTextListTypeTemplateContent[]).map((c, i) => {
+      {selectedHtmlMailTemplate.template_type === 'ImageWithText'
+        && selectedHtmlMailTemplate.content && (JSON.parse(selectedHtmlMailTemplate.content) as ImageWithTextTemplateContent[] | ImageWithTextListTypeTemplateContent[]).map((c, i) => {
         return (
           <div key={i}>
             <img
@@ -17,6 +19,23 @@ const HtmlMailTemplatePreview = (): JSX.Element => {
             />
             <div>{c.text}</div>
           </div>
+        )})
+      }
+      {selectedHtmlMailTemplate.template_type === 'ImageWithTextList'
+        && selectedHtmlMailTemplate.content && (JSON.parse(selectedHtmlMailTemplate.content) as ImageWithTextTemplateContent[] | ImageWithTextListTypeTemplateContent[]).map((c, i) => {
+        return (
+          <Row key={i}>
+            <Col>
+              <img
+                className='d-block w-100 mt30'
+                src={c.image}
+                alt='image'
+              />
+            </Col>
+            <Col>
+              <div>{c.text}</div>
+            </Col>
+          </Row>
         )})
       }
     </>
