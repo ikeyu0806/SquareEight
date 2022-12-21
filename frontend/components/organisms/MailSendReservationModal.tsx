2106c5fc6f7c4d5e32b52c220ef7e335cc67af2b
ikeyu0806 <ikeyu0806@gmail.com>
2022-12-21 11:25:37 +0900

    implement MailSendReservationModal

--- frontend/components/organisms/MailSendReservationModal.tsx
+++ frontend/components/organisms/MailSendReservationModal.tsx
@@ -1,20 +1,64 @@
 import { Modal, Button, Form } from 'react-bootstrap'
 import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from 'redux/store'
-import { showSendMailReservationModalChanged } from 'redux/sendMailReservationSlice'
+import axios from 'axios'
+import { swalWithBootstrapButtons } from 'constants/swalWithBootstrapButtons'
+import { useCookies } from 'react-cookie'
+import { showSendMailReservationModalChanged,
+         scheduledDateChanged,
+         scheduledTimeChanged } from 'redux/sendMailReservationSlice'
 
 const MailSendReservationModal = (): JSX.Element => {
+  const [cookies] = useCookies(['_square_eight_merchant_session'])
   const dispatch = useDispatch()
   const showSendMailReservationModal = useSelector((state: RootState) => state.sendMailReservation.showSendMailReservationModal)
+  const scheduledDate = useSelector((state: RootState) => state.sendMailReservation.scheduledDate)
+  const scheduledTime = useSelector((state: RootState) => state.sendMailReservation.scheduledTime)
+
+  const onSubmit = () => {
+    axios.post(`${process.env.BACKEND_URL}/api/internal/send_mail_schedules`,
+    {
+      send_mail_schedules: {
+        scheduled_date: scheduledDate,
+        scheduled_time: scheduledTime
+      }
+    },
+    {
+      headers: {
+        'Session-Id': cookies._square_eight_merchant_session
+      }
+    }).then(response => {
+      swalWithBootstrapButtons.fire({
+        title: '登録しました',
+        icon: 'info'
+      })
+      location.reload()
+    }).catch(error => {
+      swalWithBootstrapButtons.fire({
+        title: '登録失敗しました',
+        icon: 'error'
+      })
+    })
+  }
+
 
   return (
     <Modal show={showSendMailReservationModal}>
       <Modal.Body>
         <div>送信日時を入力してください</div>
-        <Form.Control type='date' className='mt10'></Form.Control>
-        <Form.Control type='time' className='mt10'></Form.Control>
+        <Form.Control
+          onChange={(e) => dispatch(scheduledDateChanged(e.target.value))}
+          type='date'
+          className='mt10'></Form.Control>
+        <Form.Control
+          onChange={(e) => dispatch(scheduledTimeChanged(e.target.value))}
+          type='time'
+          className='mt10'></Form.Control>
       </Modal.Body>
       <Modal.Footer>
+        <Button onClick={onSubmit}>
+          登録する
+        </Button>
         <Button onClick={() => dispatch(showSendMailReservationModalChanged(false))}>
           閉じる
         </Button>
--- frontend/redux/sendMailReservationSlice.tsx
+++ frontend/redux/sendMailReservationSlice.tsx
@@ -4,14 +4,24 @@ export const sendMailReservationSlice = createSlice({
   name: 'sendMailReservation',
   initialState: {
     showSendMailReservationModal: false,
+    scheduledDate: '',
+    scheduledTime: '',
   },
   reducers: {
     showSendMailReservationModalChanged: (state, action: PayloadAction<boolean>) => {
       state.showSendMailReservationModal = action.payload
     },
+    scheduledDateChanged: (state, action: PayloadAction<string>) => {
+      state.scheduledDate = action.payload
+    },
+    scheduledTimeChanged: (state, action: PayloadAction<string>) => {
+      state.scheduledTime = action.payload
+    },
   },
 })
 
 export const { showSendMailReservationModalChanged } = sendMailReservationSlice.actions
+export const { scheduledDateChanged } = sendMailReservationSlice.actions
+export const { scheduledTimeChanged } = sendMailReservationSlice.actions
 
 export default sendMailReservationSlice.reducer
