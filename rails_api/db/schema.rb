8332513bca509454ed1af1fe9a5ccb075bf01d3b
ikeyu0806 <ikeyu0806@gmail.com>
2022-12-25 17:41:03 +0900

    add inventory_allocation to product

--- rails_api/db/schema.rb
+++ rails_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_24_063353) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_25_083213) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -474,6 +474,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_24_063353) do
     t.integer "flat_rate_delivery_charge"
     t.integer "delivery_charge_with_order_number", default: 0
     t.boolean "delivery_datetime_target_flg", default: true
+    t.integer "inventory_allocation", default: 0
   end
 
   create_table "purchased_tickets", force: :cascade do |t|
