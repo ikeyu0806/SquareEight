7e59beb680e8df4b9c4541e770c70706bfcf8918
ikeyu0806 <ikeyu0806@gmail.com>
2022-12-26 17:28:29 +0900

    implement update_read_questionnaire_answers_status_unread function

--- rails_api/app/models/questionnaire_answer.rb
+++ rails_api/app/models/questionnaire_answer.rb
@@ -1,8 +1,16 @@
 class QuestionnaireAnswer < ApplicationRecord
   include PublicIdModule
 
+  before_create :update_read_questionnaire_answers_status_unread
+
   belongs_to :questionnaire_master
   belongs_to :customer
+
+  def update_read_questionnaire_answers_status_unread
+    self.questionnaire_master.account.merchant_users.each do |user|
+      user.read_questionnaire_answers_status_UnreadExist!
+    end
+  end
 
   def parse_answer_json
     JSON.parse self.answers_json.gsub("=>", ":").gsub(" ", "")
