6deb4f504a857c62433fba1c0d3adf434aaf4ffb
ikeyu0806 <ikeyu0806@gmail.com>
2022-12-26 17:36:59 +0900

    implement update_read_dashboard_status_unread

--- rails_api/app/models/account_notification.rb
+++ rails_api/app/models/account_notification.rb
@@ -2,4 +2,12 @@ class AccountNotification < ApplicationRecord
   include PublicIdModule
 
   belongs_to :account
+
+  before_create :update_read_dashboard_status_unread
+
+  def update_read_dashboard_status_unread
+    self.account.merchant_users.each do |user|
+      user.read_reservations_status_UnreadExist!
+    end
+  end
 end
--- rails_api/app/models/system_account_notification.rb
+++ rails_api/app/models/system_account_notification.rb
@@ -1,3 +1,9 @@
 class SystemAccountNotification < ApplicationRecord
   include PublicIdModule
+
+  before_create :update_read_dashboard_status_unread
+
+  def update_read_dashboard_status_unread
+    MerchantUser.update_all(read_dashboard: 'UnreadExist')
+  end
 end
