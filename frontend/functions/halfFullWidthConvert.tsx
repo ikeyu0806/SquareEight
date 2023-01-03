9cbaba90759961a60fbf0e8555f77056ec9fd706
ikeyu0806 <ikeyu0806@gmail.com>
2023-01-03 16:36:57 +0900

    delete halfFullWidthConvert

--- frontend/functions/halfFullWidthConvert.tsx
+++
@@ -1,5 +0,0 @@
-export const convertHalfWidthToFullWidth = (halfWidthText: string)  => {
-  return halfWidthText.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
-    return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
-})
-}
