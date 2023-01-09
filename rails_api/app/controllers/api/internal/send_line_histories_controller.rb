705bdc77546e0d66be613edd983093798b990c7f
ikeyu0806 <ikeyu0806@gmail.com>
2023-01-09 15:45:14 +0900

    add order to message histories

--- rails_api/app/controllers/api/internal/send_line_histories_controller.rb
+++ rails_api/app/controllers/api/internal/send_line_histories_controller.rb
@@ -2,7 +2,7 @@ class Api::Internal::SendLineHistoriesController < ApplicationController
   before_action :merchant_login_only!
 
   def index
-    send_line_histories = current_merchant_user.account.send_line_histories
+    send_line_histories = current_merchant_user.account.send_line_histories.order(created_at: :desc)
     send_line_histories = JSON.parse(send_line_histories.to_json(methods: [:line_official_account_name, :line_user_display_name]))
     render json: { status: 'success', send_line_histories: send_line_histories }, status: 200
   rescue => error
--- rails_api/app/controllers/api/internal/send_mail_histories_controller.rb
+++ rails_api/app/controllers/api/internal/send_mail_histories_controller.rb
@@ -2,7 +2,7 @@ class Api::Internal::SendMailHistoriesController < ApplicationController
   before_action :merchant_login_only!
 
   def index
-    send_mail_histories = current_merchant_user.account.send_mail_histories
+    send_mail_histories = current_merchant_user.account.send_mail_histories.order(created_at: :desc)
     send_mail_histories = JSON.parse(send_mail_histories.to_json(methods: [:send_at, :customer_fullname, :parsed_message_body]))
     render json: { status: 'success', send_mail_histories: send_mail_histories }, status: 200
   rescue => error
