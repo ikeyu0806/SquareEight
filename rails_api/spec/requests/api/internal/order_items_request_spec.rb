fbcfd7d608e0dd60ee768da60042409c64a2ec69
ikeyu0806 <ikeyu0806@gmail.com>
2023-01-17 10:53:51 +0900

    fix order_item spec error

--- rails_api/spec/requests/api/internal/order_items_request_spec.rb
+++ rails_api/spec/requests/api/internal/order_items_request_spec.rb
@@ -6,8 +6,9 @@ RSpec.describe 'Api::Internal::OrderItemsController', type: :request do
     create(:merchant_user, account: account)
   }
   let(:end_user) { create(:end_user) }
+  let(:product) { create(:product, account: account) }
   let(:order) { create(:order, end_user_id: end_user.id) }
-  let!(:product_order_item) { create(:product_order_item, account_id: account.id, order: order) }
+  let!(:product_order_item) { create(:product_order_item, account_id: account.id, order: order, product_id: product.id) }
   let!(:monthly_payment_plan_order_item) { create(:monthly_payment_plan_order_item, account_id: account.id, order: order) }
   let!(:ticket_master_order_item) { create(:ticket_master_order_item, account_id: account.id, order: order) }
 
