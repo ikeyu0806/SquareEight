b4f16635c1f18c0218d1c38b42ff1c96db779d86
ikeyu0806 <ikeyu0806@gmail.com>
2023-01-18 19:34:46 +0900

    fix resource migration

--- rails_api/db/migrate/20230118081944_add_columns_resource.rb
+++ rails_api/db/migrate/20230118081944_add_columns_resource.rb
@@ -9,7 +9,7 @@ class AddColumnsResource < ActiveRecord::Migration[7.0]
   def down
     remove_column :resources, :resouce_type, :integer, default: 99
     remove_column :resources, :account_s3_image_id, :integer
-    remove_column :resources, :is_show_frontend, :integer default: false
+    remove_column :resources, :is_show_frontend, :integer, default: false
     remove_column :resources, :description, :string
   end
 end
