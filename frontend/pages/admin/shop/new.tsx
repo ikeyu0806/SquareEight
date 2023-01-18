87b3e01915c9b0a90e8ff192a9b85c2ccc9427af
ikeyu0806 <ikeyu0806@gmail.com>
2023-01-18 19:34:32 +0900

    create shop/new.tsx

---
+++ frontend/pages/admin/shop/new.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next'
+import MerchantUserAdminLayout from 'components/templates/MerchantUserAdminLayout'
+
+const New: NextPage = () => {
+  return (
+    <MerchantUserAdminLayout>
+    </MerchantUserAdminLayout>
+  )
+}
+
+export default New
