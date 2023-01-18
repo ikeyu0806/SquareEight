b49a849e3011b9767390cac81f723ce2bfea6e0f
ikeyu0806 <ikeyu0806@gmail.com>
2023-01-18 20:28:46 +0900

    add form to CreateShop component

--- frontend/components/templates/CreateShop.tsx
+++ frontend/components/templates/CreateShop.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { FormControl, Form, Button } from 'react-bootstrap'
+import { FormControl, Form, Button, Row, Col } from 'react-bootstrap'
 import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from 'redux/store'
+import RequireBadge from 'components/atoms/RequireBadge'
 import { nameChanged, quantityChanged } from 'redux/resourceSlice'
 import ResourceLimitAlert from 'components/molecules/ResourceLimitAlert'
 
@@ -16,9 +17,16 @@ const CreateShop = (): JSX.Element => {
         <h3 className='mb15'>店舗情報登録</h3>
         <div>登録した情報を元に店舗紹介ページが自動生成されます。</div>
         <div className='mt5'>店舗紹介ページには登録した商品。予約メニュー、回数券、月額課金の紹介とWebページのリンクも挿入されます。</div>
-        <Button className='mt10 mb10 text-white' variant='info'>店舗紹介ページのプレビューを表示</Button>
-        <br />
-      <Form.Label>店舗名</Form.Label>
+        <Row>
+          <Col>
+            <Button className='mt10 mb10 text-white' variant='info'>店舗紹介ページのデモを表示</Button>
+          </Col>
+          <Col>
+            <Button className='mt10 mb10 text-white' variant='info'>店舗紹介ページのプレビューを表示</Button>
+          </Col>
+        </Row>
+        <hr />
+      <Form.Label>店舗名<RequireBadge /></Form.Label>
       <FormControl />
       <Form.Label className='mt10'>店舗説明</Form.Label>
       <FormControl />
@@ -42,6 +50,13 @@ const CreateShop = (): JSX.Element => {
       <Form.Label className='mt10'>カバー画像2</Form.Label>
       <Form.Control type='file' />
       <Form.Label className='mt10'>カバー画像3</Form.Label>
+      <Form.Control type='file' />
+      <hr />
+      <Form.Label className='mt10'>店舗イメージ画像1</Form.Label>
+      <Form.Control type='file' />
+      <Form.Label className='mt10'>店舗イメージ画像2</Form.Label>
+      <Form.Control type='file' />
+      <Form.Label className='mt10'>店舗イメージ画像3</Form.Label>
       <Form.Control type='file' />
     </>
   )
