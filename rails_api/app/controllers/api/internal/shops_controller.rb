942702c579395baa9829d1d9f2f46c438c46f9ff
ikeyu0806 <ikeyu0806@gmail.com>
2023-01-19 16:33:24 +0900

    fix shops_controller create action

--- rails_api/app/controllers/api/internal/shops_controller.rb
+++ rails_api/app/controllers/api/internal/shops_controller.rb
@@ -2,6 +2,15 @@ class Api::Internal::ShopsController < ApplicationController
   before_action :merchant_login_only!
 
   def create
+    ActiveRecord::Base.transaction do
+      shop = current_merchant_user.account.shops.new
+      shop.name = params[:name]
+      if params[:page_cover_slide1_file].present?
+        shop.register_s3_image(params[:page_cover_slide1_file], "page_cover_slide1_account_s3_image_id")
+      end
+      show.name = params[:name]
+      show.save!
+    end
     render json: { status: 'success' }, status: 200
   rescue => error
     Rails.logger.error error
--- rails_api/app/models/shop.rb
+++ rails_api/app/models/shop.rb
@@ -1,15 +1,19 @@
 class Shop < ApplicationRecord
   include Base64Image
 
+  belongs_to :account
+
   # S3にputするファイルとs3_object_public_url_column
   def register_s3_image(image_file, s3_object_public_url_column)
-    file_name = "shop_image_" + Time.zone.now.strftime('%Y%m%d%H%M%S%3N')
-    s3_object_public_url = put_s3_http_request_data(image_file, ENV["PRODUCT_IMAGE_BUCKET"], file_name)
-    account_image = AccountS3Image.new
-    account_image.account = current_merchant_user.account
-    account_image.s3_object_public_url = s3_object_public_url
-    account_image.s3_object_name = file_name
-    account_image.save!
-    self.send(s3_object_public_url_column + "=", account_image.id)
+    
+      file_name = "shop_image_" + Time.zone.now.strftime('%Y%m%d%H%M%S%3N')
+      s3_object_public_url = put_s3_http_request_data(image_file, ENV["PRODUCT_IMAGE_BUCKET"], file_name)
+      account_image = AccountS3Image.new
+      account_image.account = current_merchant_user.account
+      account_image.s3_object_public_url = s3_object_public_url
+      account_image.s3_object_name = file_name
+      account_image.save!
+      self.send(s3_object_public_url_column + "=", account_image.id)
+      self.save!
   end
 end
