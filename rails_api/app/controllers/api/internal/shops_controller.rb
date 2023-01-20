512594f97595c2c28a13a6dc65cb5dd860a68c60
ikeyu0806 <ikeyu0806@gmail.com>
2023-01-21 08:15:41 +0900

    add params to shop create action

--- rails_api/app/controllers/api/internal/shops_controller.rb
+++ rails_api/app/controllers/api/internal/shops_controller.rb
@@ -6,9 +6,23 @@ class Api::Internal::ShopsController < ApplicationController
       shop = current_merchant_user.account.shops.new
       shop.name = params[:name]
       shop.description1 = params[:description1]
+      shop.description2 = params[:description2]
+      shop.postal_code = params[:postal_code]
+      shop.state = params[:state]
+      shop.city = params[:city]
+      shop.town = params[:town]
+      shop.line1 = params[:line1]
+      shop.line2 = params[:line2]
+      shop.remarks = params[:remarks]
       shop.save!
       if params[:page_cover_slide1_file].present?
         shop.register_s3_image(params[:page_cover_slide1_file], "page_cover_slide1_account_s3_image_id")
+      end
+      if params[:page_cover_slide2_file].present?
+        shop.register_s3_image(params[:page_cover_slide2_file], "page_cover_slide2_account_s3_image_id")
+      end
+      if params[:page_cover_slide3_file].present?
+        shop.register_s3_image(params[:page_cover_slide3_file], "page_cover_slide3_account_s3_image_id")
       end
       shop.save!
     end
