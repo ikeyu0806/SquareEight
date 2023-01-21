b79512a8104c4aba00323396bb5fbc303f9d01c4
ikeyu0806 <ikeyu0806@gmail.com>
2023-01-21 11:35:39 +0900

    create shop page path

---
+++ frontend/pages/shop/[public_id]/index.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next'
+import { Container } from 'react-bootstrap'
+import MerchantUserAdminLayout from 'components/templates/MerchantUserAdminLayout'
+
+const Index: NextPage = () => {
+  return (
+    <MerchantUserAdminLayout>
+      <Container>
+      </Container>
+    </MerchantUserAdminLayout>
+  )
+}
+
+export default Index
