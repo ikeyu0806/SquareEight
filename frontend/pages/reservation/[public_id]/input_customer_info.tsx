ba5e666d933dc08f277b437a38800d5106ef06ea
ikeyu0806 <ikeyu0806@gmail.com>
2023-01-28 18:44:26 +0900

    add style tp reserve input_customer_info

--- frontend/pages/reservation/[public_id]/input_customer_info.tsx
+++ frontend/pages/reservation/[public_id]/input_customer_info.tsx
@@ -16,6 +16,7 @@ import QuestionnaireItemAnswerForm from 'components/templates/QuestionnaireItemA
 import { hideShareButtonChanged } from 'redux/sharedComponentSlice'
 import { QuestionnaireMasterItem } from 'interfaces/QuestionnaireMasterItem'
 import { redirectEndUserLoginPath } from 'functions/redirectEndUserLoginPath'
+import { brandGreenRgb } from 'constants/brandColors'
 import {  questionnaireMasterItemsChanged,
           answerChangeDetectStateChanged } from 'redux/questionnaireMasterSlice'
 import {  navbarBrandTextChanged,
@@ -194,162 +195,164 @@ const Index: NextPage = () => {
   return (
     <>
       <MerchantCustomLayout>
-        <Container className='mt30'>
-          {publishStatus === 'Unpublish' &&
-            <div className='text-center mt20'>非公開です</div>}
-          {publishStatus === 'Publish' && <Row>
-            <Col lg={3} md={3}></Col>
-            <Col lg={6} md={6}>
-              <Card>
-                <Card.Header>お客様情報の入力</Card.Header>
-                <Card.Body>
-                  {loginValidate() &&<>
-                  <label className='mb40'>クレジットカード支払い、回数券、月額サブスクリプションを使用する場合、ログインする必要があります</label>
-                    <br />
-                    <a
-                      className='link-text'
-                      onClick={() => redirectEndUserLoginPath(router.asPath)}>SquareEightIDでログインしてください</a><br/>
-                    <br/>
-                    <label className='mt10 mb10'>新規作成は<a className='link-text' href='/customer/signup'  target='_blank' rel='noreferrer'>こちら</a></label>
-                    <br/>
-                    <hr />
-                  </>}
-                  <h3>{title}</h3>
-                  <div className='mt10 mb10'>予約時間: {date[0]}年{date[1]}月{date[2]}日 {time}</div>
-                  {/* 支払い方法 */}
-                  {/* 料金一律の場合 */}
-                  {multiLocalPaymentPrices.length === 0 &&
-                   multiCreditCardPaymentPrices.length === 0 &&
-                   <>
-                    {isSetPrice &&
-                      <div>
-                        お支払い方法: {paymentMethodText(String(paymentMethod), price, consumeNumber, numberOfPeople)}
-                        {(isSubscribePlan && String(paymentMethod) === 'monthlyPaymentPlan')
-                        && <span className='badge bg-info ml10'>加入済み</span>}
-                      </div>}
-                      {!isSubscribePlan
-                      && (String(paymentMethod) === 'monthlyPaymentPlan')
-                      && <div className='mt20 mb20'>プランに加入していません
-                            <a href={`/monthly_payment/${monthlyPaymentPlanId}/purchase`} target='_blank' rel='noreferrer'>こちら</a>
-                            から加入してください</div>}
-                      {!isPurchaseTicket
-                      && (String(paymentMethod) === 'ticket')
-                      && <div className='mt20 mb20'>チケットを購入していません
-                            <a href={`/ticket/${ticketMasterPublicId}/purchase`} target='_blank' rel='noreferrer'>こちら</a>
-                            から購入してください</div>}
-                      <div className='mt10 mb10'>
-                        {['localPayment', 'creditCardPayment'].includes(String(paymentMethod))
-                          // 予約人数
-                          &&
-                          <>
-                            予約人数: {numberOfPeople}
-                          </>
-                        }
-                      </div>
-                   </>}
-                   {/* 複数料金設定している場合 */}
-                   {/* 現地払い */}
-                   {paymentMethod === 'localPayment'
-                    && multiLocalPaymentPrices.length !== 0
-                    &&
-                      <>
-                        {multiLocalPaymentPrices.map((paymentPrice, i) => {
-                          return (
-                            <div key={i}>{paymentPrice.name} {paymentPrice.reserve_number_of_people}人 ￥{paymentPrice.price}</div>
-                          )
-                        })}
-                        <div>
-                            合計: {multiLocalPaymentPrices.reduce(function(sum, element){ return sum + element.price}, 0)}</div>
-                      </>
-                   }
-                   {/* クレジットカード払い */}
-                   {paymentMethod === 'creditCardPayment'
-                    && multiCreditCardPaymentPrices.length !== 0
-                    &&
-                      <>
-                        {multiCreditCardPaymentPrices.map((paymentPrice, i) => {
-                          return (
-                            <div key={i}>{paymentPrice.name} {paymentPrice.reserve_number_of_people}人 ￥{paymentPrice.price}</div>
-                          )
-                        })}
-                        <div>
-                            合計: {multiCreditCardPaymentPrices.reduce(function(sum, element){ return sum + element.price}, 0)}</div>
-                      </>
-                   }
-                   { (showCustomerFormValidate()) &&
+        <div className='bg-lightgray'>
+          <Container>
+            {publishStatus === 'Unpublish' &&
+              <div className='text-center mt20'>非公開です</div>}
+            {publishStatus === 'Publish' && <Row>
+              <Col lg={3} md={3}></Col>
+              <Col lg={6} md={6}>
+                <Card className='mt30 mb30'>
+                  <Card.Body>
+                  <p><span className='orange_highlighter font-size-25'>お客様情報の入力</span></p>
+                    {loginValidate() &&<>
+                    <label className='mb40'>クレジットカード支払い、回数券、月額サブスクリプションを使用する場合、ログインする必要があります</label>
+                      <br />
+                      <a
+                        className='link-text'
+                        onClick={() => redirectEndUserLoginPath(router.asPath)}>SquareEightIDでログインしてください</a><br/>
+                      <br/>
+                      <label className='mt10 mb10'>新規作成は<a className='link-text' href='/customer/signup'  target='_blank' rel='noreferrer'>こちら</a></label>
+                      <br/>
+                      <hr />
+                    </>}
+                    <h3>{title}</h3>
+                    <div className='mt10 mb10'>予約時間: {date[0]}年{date[1]}月{date[2]}日 {time}</div>
+                    {/* 支払い方法 */}
+                    {/* 料金一律の場合 */}
+                    {multiLocalPaymentPrices.length === 0 &&
+                    multiCreditCardPaymentPrices.length === 0 &&
                     <>
-                    <hr/>
-                    <Form.Label className='mt10'>お名前（姓）</Form.Label>
-                    <Form.Control
-                      value={lastName}
-                      onChange={(e) => {
-                        setLastName(e.target.value)
-                        dispatch(answerChangeDetectStateChanged(answerChangeDetectState + 1))
-                      }}></Form.Control>
-                    <Form.Label className='mt10'>お名前（名）</Form.Label>
-                    <Form.Control
-                      value={firstName}
-                      onChange={(e) => {
-                        setFirstName(e.target.value)
-                        dispatch(answerChangeDetectStateChanged(answerChangeDetectState + 1))
-                      }}></Form.Control>
-                    <Form.Label className='mt10'>メールアドレス</Form.Label>
-                    <Form.Control
-                      value={email}
-                      onChange={(e) => {
-                        setEmail(e.target.value)
-                        dispatch(answerChangeDetectStateChanged(answerChangeDetectState + 1))
-                      }}></Form.Control>
-                    <Form.Label className='mt10'>携帯電話番号</Form.Label>
-                    <Form.Control
-                      value={phoneNumber}
-                      onChange={(e) => {
-                        setPhoneNumber(e.target.value)
-                        dispatch(answerChangeDetectStateChanged(answerChangeDetectState + 1))
-                      }}></Form.Control>
-                    <QuestionnaireItemAnswerForm></QuestionnaireItemAnswerForm>
-                  </>}
-                  {endUserLoginStatus === 'Login' &&
-                  <>
-                    {
-                    !StripePaymentMethods?.length &&
-                    <>
-                      <hr />カードが登録されていません
-                      <br /><a href='/customer_page/payment_method/register' target='_blank' rel='noreferrer'>登録はこちら</a>
-                    </>
-                    }
-                    {paymentMethod === 'creditCardPayment' &&
-                      <>
-                        <hr />
-                        <ListGroup>
-                          {StripePaymentMethods?.map((pay, i) => {
+                      {isSetPrice &&
+                        <div>
+                          お支払い方法: {paymentMethodText(String(paymentMethod), price, consumeNumber, numberOfPeople)}
+                          {(isSubscribePlan && String(paymentMethod) === 'monthlyPaymentPlan')
+                          && <span className='badge bg-info ml10'>加入済み</span>}
+                        </div>}
+                        {!isSubscribePlan
+                        && (String(paymentMethod) === 'monthlyPaymentPlan')
+                        && <div className='mt20 mb20'>プランに加入していません
+                              <a href={`/monthly_payment/${monthlyPaymentPlanId}/purchase`} target='_blank' rel='noreferrer'>こちら</a>
+                              から加入してください</div>}
+                        {!isPurchaseTicket
+                        && (String(paymentMethod) === 'ticket')
+                        && <div className='mt20 mb20'>チケットを購入していません
+                              <a href={`/ticket/${ticketMasterPublicId}/purchase`} target='_blank' rel='noreferrer'>こちら</a>
+                              から購入してください</div>}
+                        <div className='mt10 mb10'>
+                          {['localPayment', 'creditCardPayment'].includes(String(paymentMethod))
+                            // 予約人数
+                            &&
+                            <>
+                              予約人数: {numberOfPeople}
+                            </>
+                          }
+                        </div>
+                    </>}
+                    {/* 複数料金設定している場合 */}
+                    {/* 現地払い */}
+                    {paymentMethod === 'localPayment'
+                      && multiLocalPaymentPrices.length !== 0
+                      &&
+                        <>
+                          {multiLocalPaymentPrices.map((paymentPrice, i) => {
                             return (
-                              <ListGroup.Item key={i}>
-                                {pay.card.brand}（************{pay.card.last4} / 有効期限 {pay.card.exp_month} / {pay.card.exp_year}
-                                {defaultStripePaymentMethodId === pay.id && <><br/><span className='badge bg-info'>お支払いカードに設定されています</span></>}
-                              </ListGroup.Item>
+                              <div key={i}>{paymentPrice.name} {paymentPrice.reserve_number_of_people}人 ￥{paymentPrice.price}</div>
                             )
                           })}
-                        </ListGroup>
-                      </>
+                          <div>
+                              合計: {multiLocalPaymentPrices.reduce(function(sum, element){ return sum + element.price}, 0)}</div>
+                        </>
                     }
-                  </>
-                  }
-                  {!loginValidate() && !isCompleteReservation &&
-                  <div className='text-center'>
-                    <Button
-                      disabled={reserveValidate()}
-                      className='mt30'
-                      onClick={onSubmit}>
-                      {isLoading && <span className='spinner-border spinner-border-sm' role='status' aria-hidden='true'></span>}
-                      予約を確定する
-                    </Button>
-                  </div>}
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>}
-        </Container>
+                    {/* クレジットカード払い */}
+                    {paymentMethod === 'creditCardPayment'
+                      && multiCreditCardPaymentPrices.length !== 0
+                      &&
+                        <>
+                          {multiCreditCardPaymentPrices.map((paymentPrice, i) => {
+                            return (
+                              <div key={i}>{paymentPrice.name} {paymentPrice.reserve_number_of_people}人 ￥{paymentPrice.price}</div>
+                            )
+                          })}
+                          <div>
+                              合計: {multiCreditCardPaymentPrices.reduce(function(sum, element){ return sum + element.price}, 0)}</div>
+                        </>
+                    }
+                    { (showCustomerFormValidate()) &&
+                      <>
+                      <hr/>
+                      <Form.Label className='mt10'>お名前（姓）</Form.Label>
+                      <Form.Control
+                        value={lastName}
+                        onChange={(e) => {
+                          setLastName(e.target.value)
+                          dispatch(answerChangeDetectStateChanged(answerChangeDetectState + 1))
+                        }}></Form.Control>
+                      <Form.Label className='mt10'>お名前（名）</Form.Label>
+                      <Form.Control
+                        value={firstName}
+                        onChange={(e) => {
+                          setFirstName(e.target.value)
+                          dispatch(answerChangeDetectStateChanged(answerChangeDetectState + 1))
+                        }}></Form.Control>
+                      <Form.Label className='mt10'>メールアドレス</Form.Label>
+                      <Form.Control
+                        value={email}
+                        onChange={(e) => {
+                          setEmail(e.target.value)
+                          dispatch(answerChangeDetectStateChanged(answerChangeDetectState + 1))
+                        }}></Form.Control>
+                      <Form.Label className='mt10'>携帯電話番号</Form.Label>
+                      <Form.Control
+                        value={phoneNumber}
+                        onChange={(e) => {
+                          setPhoneNumber(e.target.value)
+                          dispatch(answerChangeDetectStateChanged(answerChangeDetectState + 1))
+                        }}></Form.Control>
+                      <QuestionnaireItemAnswerForm></QuestionnaireItemAnswerForm>
+                    </>}
+                    {endUserLoginStatus === 'Login' &&
+                    <>
+                      {
+                      !StripePaymentMethods?.length &&
+                      <>
+                        <hr />カードが登録されていません
+                        <br /><a href='/customer_page/payment_method/register' target='_blank' rel='noreferrer'>登録はこちら</a>
+                      </>
+                      }
+                      {paymentMethod === 'creditCardPayment' &&
+                        <>
+                          <hr />
+                          <ListGroup>
+                            {StripePaymentMethods?.map((pay, i) => {
+                              return (
+                                <ListGroup.Item key={i}>
+                                  {pay.card.brand}（************{pay.card.last4} / 有効期限 {pay.card.exp_month} / {pay.card.exp_year}
+                                  {defaultStripePaymentMethodId === pay.id && <><br/><span className='badge bg-info'>お支払いカードに設定されています</span></>}
+                                </ListGroup.Item>
+                              )
+                            })}
+                          </ListGroup>
+                        </>
+                      }
+                    </>
+                    }
+                    {!loginValidate() && !isCompleteReservation &&
+                    <div className='text-center'>
+                      <Button
+                        disabled={reserveValidate()}
+                        className='mt30'
+                        onClick={onSubmit}>
+                        {isLoading && <span className='spinner-border spinner-border-sm' role='status' aria-hidden='true'></span>}
+                        予約を確定する
+                      </Button>
+                    </div>}
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>}
+          </Container>
+        </div>
       </MerchantCustomLayout>
     </>
   )
