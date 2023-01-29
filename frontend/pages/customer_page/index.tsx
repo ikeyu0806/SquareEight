162671f85a6c38b8930fd6c52612241d7734fc92
ikeyu0806 <ikeyu0806@gmail.com>
2023-01-29 17:15:03 +0900

    add margin to customer page

--- frontend/pages/customer_page/index.tsx
+++ frontend/pages/customer_page/index.tsx
@@ -38,46 +38,48 @@ const Index: NextPage = () => {
 
   return (
     <EndUserLoginLayout>
-      <Container>
-        <Row>
-          <Col>
-            <ListGroup as='ul'>
-              <ListGroup.Item as='li' active>
-                通知一覧
-              </ListGroup.Item>
-              {notifications && notifications.map((n, i) => {
-                return (
-                  <ListGroup.Item
-                    as='li'
-                    key={i}
-                    className='list-group-hover'
-                    onClick={() => router.push(`${n.url}`)}>
-                    {n.title}
-                  </ListGroup.Item>
-                )
-              })}
-            </ListGroup>
-          </Col>
-          <Col>
-            <ListGroup as='ul'>
-              <ListGroup.Item as='li' active>
-                運営からのお知らせ
-              </ListGroup.Item>
-              {systemNotifications && systemNotifications.map((n, i) => {
-                return (
-                  <ListGroup.Item
-                    as='li'
-                    key={i}
-                    className='list-group-hover'
-                    onClick={() => router.push(`/customer_page/system_notification/${n.id}/`)}>
-                    {n.title}
-                  </ListGroup.Item>
-                )
-              })}
-            </ListGroup>
-          </Col>
-        </Row>
-      </Container>
+      <div className='mt20'>
+        <Container>
+          <Row>
+            <Col>
+              <ListGroup as='ul'>
+                <ListGroup.Item as='li' active>
+                  通知一覧
+                </ListGroup.Item>
+                {notifications && notifications.map((n, i) => {
+                  return (
+                    <ListGroup.Item
+                      as='li'
+                      key={i}
+                      className='list-group-hover'
+                      onClick={() => router.push(`${n.url}`)}>
+                      {n.title}
+                    </ListGroup.Item>
+                  )
+                })}
+              </ListGroup>
+            </Col>
+            <Col>
+              <ListGroup as='ul'>
+                <ListGroup.Item as='li' active>
+                  運営からのお知らせ
+                </ListGroup.Item>
+                {systemNotifications && systemNotifications.map((n, i) => {
+                  return (
+                    <ListGroup.Item
+                      as='li'
+                      key={i}
+                      className='list-group-hover'
+                      onClick={() => router.push(`/customer_page/system_notification/${n.id}/`)}>
+                      {n.title}
+                    </ListGroup.Item>
+                  )
+                })}
+              </ListGroup>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </EndUserLoginLayout>
   )
 }
--- frontend/pages/customer_page/reservation/index.tsx
+++ frontend/pages/customer_page/reservation/index.tsx
@@ -60,41 +60,43 @@ const Index: NextPage = () => {
   return (
     <>
       <EndUserLoginLayout>
-        <Container>
-          <Row>
-            <Col lg={3}></Col>
-            <Col lg={6}>
-              {reservations && reservations.map((reservation, i) => {
-                return (
-                  <div key={i} className='mb30 border-solid padding-20'>
-                    <div className='mb10'>{reservation.reserve_frame_title}</div>
-                    <span>{reservation.display_reservation_datetime}</span>
-                    <hr />
-                    <span>人数: {reservation.number_of_people}</span>
-                    <hr />
-                    <span>支払い方法: {paymentMethodText(reservation.payment_method, reservation.price, reservation.ticket_consume_number, reservation.number_of_people)}</span>
-                    <hr />
-                    <span>
-                      {reservation.status === 'cofirm' && <>キャンセル: {reservation.cancel_reception_text}</>}
-                      {reservation.status === 'cancel' && <span className='badge bg-danger'>キャンセル済み</span>}
-                      {reservation.is_cancelable
-                      &&
-                        <>
-                          <Button
-                            onClick={() => execCancel(reservation.public_id)}
-                            size='sm'
-                            variant='danger'
-                            className='ml10'>キャンセルする</Button>
-                        </>}
-                      {reservation.status === 'waitingForLotteryConfirm' && <span>抽選結果待ち</span>}
-                      {reservation.status === 'lostLottery' && <span>抽選落選</span>}
-                    </span>
-                  </div>
-                )
-              })}
-            </Col>
-          </Row>
-        </Container>
+        <div className='mt20'>
+          <Container>
+            <Row>
+              <Col lg={3}></Col>
+              <Col lg={6}>
+                {reservations && reservations.map((reservation, i) => {
+                  return (
+                    <div key={i} className='mb30 border-solid padding-20'>
+                      <div className='mb10'>{reservation.reserve_frame_title}</div>
+                      <span>{reservation.display_reservation_datetime}</span>
+                      <hr />
+                      <span>人数: {reservation.number_of_people}</span>
+                      <hr />
+                      <span>支払い方法: {paymentMethodText(reservation.payment_method, reservation.price, reservation.ticket_consume_number, reservation.number_of_people)}</span>
+                      <hr />
+                      <span>
+                        {reservation.status === 'cofirm' && <>キャンセル: {reservation.cancel_reception_text}</>}
+                        {reservation.status === 'cancel' && <span className='badge bg-danger'>キャンセル済み</span>}
+                        {reservation.is_cancelable
+                        &&
+                          <>
+                            <Button
+                              onClick={() => execCancel(reservation.public_id)}
+                              size='sm'
+                              variant='danger'
+                              className='ml10'>キャンセルする</Button>
+                          </>}
+                        {reservation.status === 'waitingForLotteryConfirm' && <span>抽選結果待ち</span>}
+                        {reservation.status === 'lostLottery' && <span>抽選落選</span>}
+                      </span>
+                    </div>
+                  )
+                })}
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </EndUserLoginLayout>
     </>
   )
