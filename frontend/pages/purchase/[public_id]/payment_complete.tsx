df75f0e6d1df15186ed9b391e0435c751dc8921d
ikeyu0806 <ikeyu0806@gmail.com>
2023-01-29 17:31:57 +0900

    add style to payment_complete

--- frontend/pages/purchase/[public_id]/payment_complete.tsx
+++ frontend/pages/purchase/[public_id]/payment_complete.tsx
@@ -42,34 +42,36 @@ const PaymentComplete: NextPage = () => {
   return (
     <>
       <EndUserLoginLayout>
-        <Container>
-          <Row>
-            <Col lg={3} md={3}></Col>
-            <Col lg={6} md={6}>
-              <Card>
-                <Card.Header>購入完了しました</Card.Header>
-                <Card.Body>
-                  {orderItems.map((item, i) => {
-                    return (
-                      <span key={i}>
-                        {item.item_type === 'Product'
-                         ? <div>{item.product_name}{item.product_type_name && <>&emsp;{item.product_type_name}</>}&emsp;¥{item.price * item.quantity}<br/></div>
-                         : <div>{item.product_name}{item.product_type_name && <>&emsp;{item.product_type_name}</>}&emsp;¥{item.price}<br/></div>}
-                        {order && order?.delivery_charge > 0 && <div>配送料: ¥{order?.delivery_charge}</div>}
-                        {item.delivery_date_text && <div>配送日時: {item.delivery_date_text}</div>}
-                      </span>
-                    )
-                  })}
-                  <div className='mt30'>合計金額: ¥{totalPrice}</div>
+        <div className='bg-lightgray'>
+          <Container>
+            <Row>
+              <Col lg={3} md={3}></Col>
+              <Col lg={6} md={6}>
+                <Card className='mt30 mb30'>
+                  <p><span className='orange_highlighter font-size-25 ml20 mt20'>購入完了しました</span></p>
+                  <Card.Body>
+                    {orderItems.map((item, i) => {
+                      return (
+                        <span key={i}>
+                          {item.item_type === 'Product'
+                          ? <div>{item.product_name}{item.product_type_name && <>&emsp;{item.product_type_name}</>}&emsp;¥{item.price * item.quantity}<br/></div>
+                          : <div>{item.product_name}{item.product_type_name && <>&emsp;{item.product_type_name}</>}&emsp;¥{item.price}<br/></div>}
+                          {order && order?.delivery_charge > 0 && <div>配送料: ¥{order?.delivery_charge}</div>}
+                          {item.delivery_date_text && <div>配送日時: {item.delivery_date_text}</div>}
+                        </span>
+                      )
+                    })}
+                    <div className='mt30'>合計金額: ¥{totalPrice}</div>
 
-                  <a className='btn btn-primary mt20' href='/customer_page'>
-                    マイページ
-                  </a>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
+                    <a className='btn btn-info text-white mt20' href='/customer_page'>
+                      マイページ
+                    </a>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </EndUserLoginLayout>
     </>
   )
