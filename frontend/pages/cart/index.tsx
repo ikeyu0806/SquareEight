2faf0a2a1ce156777c513bba254c26d9d5c21458
ikeyu0806 <ikeyu0806@gmail.com>
2023-01-29 17:03:48 +0900

    add style to cart

--- frontend/pages/cart/index.tsx
+++ frontend/pages/cart/index.tsx
@@ -54,72 +54,48 @@ const Index: NextPage = () => {
 
   return (
     <EndUserLoginLayout>
-      <Container>
-        <Row>
-          <Col lg={2} md={1}></Col>
-          <Col lg={5} md={6}>
-            <Card>
-              <Card.Body>
-                <p><span className='orange_highlighter font-size-25'>カート</span></p>
-                <ListGroup variant='flush'>
-                  {cartItems && cartItems?.map((item, i) => {
-                    {
-                      switch (item.item_type) {
-                        case 'Product':
-                          return(
-                            <ListGroup.Item key={i}>
-                              <Row>
-                                {item.s3_object_public_url && <Col><img
-                                    className='d-block w-100 mt30'
-                                    src={item.s3_object_public_url}
-                                    alt='image'/></Col>}
-                                <Col>
-                                  {item.business_name}<br/>
-                                  {item.product_name} {item.product_type_name}<br />
-                                  {item.show_type && <>{item.selected_type_name}<br/></>}
-                                  数量: {item.quantity}<br />
-                                  ￥{item.price} 税率{item.tax_rate}%
-                                  {item.delivery_charge_type === 'noSetting'
-                                  && <div className='mt10'>配送料無料</div>}
-                                  {['flatRate', 'perPrefectures'].includes(item.delivery_charge_type)
-                                  && <div className='mt10'>配送料: ￥{item.delivery_charge}</div>}
-                                </Col>
-                                <Col>
-                                  <Button
-                                    onClick={() => deleteCartItem(item.public_id, item.item_type)}
-                                    size='sm'
-                                    variant='danger'>
-                                    カートから削除
-                                  </Button>
-                                </Col>
-                              </Row>
-                            </ListGroup.Item>)
-                        case 'TicketMaster':
-                          return (
-                            <ListGroup.Item key={i}>
-                              <Row>
-                                {item.s3_object_public_url && <Col><img
-                                    className='d-block w-100'
-                                    src={item.s3_object_public_url}
-                                    alt='image'/></Col>}
-                                <Col>
-                                  {item.business_name}<br/>
-                                  {item.product_name} 有効期限: {item.is_expired === false ? `${item.effective_month}ヶ月` : '有効期限なし'} <br />
-                                  数量: {item.quantity}<br />
-                                  ￥{item.price}
-                                </Col>
-                                <Col>
-                                  <Button
-                                    onClick={() => deleteCartItem(item.public_id, item.item_type)}
-                                    size='sm'
-                                    variant='danger'>
-                                    カートから削除
-                                  </Button>
-                                </Col>
-                              </Row>
-                            </ListGroup.Item>
-                          )
-                          case 'MonthlyPaymentPlan':
+      <div className='bg-lightgray'>
+        <Container>
+          <Row>
+            <Col lg={2} md={1}></Col>
+            <Col lg={5} md={6}>
+              <Card className='mt30 mb30'>
+                <Card.Body>
+                  <p><span className='orange_highlighter font-size-25'>カート</span></p>
+                  <ListGroup variant='flush'>
+                    {cartItems && cartItems?.map((item, i) => {
+                      {
+                        switch (item.item_type) {
+                          case 'Product':
+                            return(
+                              <ListGroup.Item key={i}>
+                                <Row>
+                                  {item.s3_object_public_url && <Col><img
+                                      className='d-block w-100 mt30'
+                                      src={item.s3_object_public_url}
+                                      alt='image'/></Col>}
+                                  <Col>
+                                    {item.business_name}<br/>
+                                    {item.product_name} {item.product_type_name}<br />
+                                    {item.show_type && <>{item.selected_type_name}<br/></>}
+                                    数量: {item.quantity}<br />
+                                    ￥{item.price} 税率{item.tax_rate}%
+                                    {item.delivery_charge_type === 'noSetting'
+                                    && <div className='mt10'>配送料無料</div>}
+                                    {['flatRate', 'perPrefectures'].includes(item.delivery_charge_type)
+                                    && <div className='mt10'>配送料: ￥{item.delivery_charge}</div>}
+                                  </Col>
+                                  <Col>
+                                    <Button
+                                      onClick={() => deleteCartItem(item.public_id, item.item_type)}
+                                      size='sm'
+                                      variant='danger'>
+                                      カートから削除
+                                    </Button>
+                                  </Col>
+                                </Row>
+                              </ListGroup.Item>)
+                          case 'TicketMaster':
                             return (
                               <ListGroup.Item key={i}>
                                 <Row>
@@ -129,7 +105,8 @@ const Index: NextPage = () => {
                                       alt='image'/></Col>}
                                   <Col>
                                     {item.business_name}<br/>
-                                    {item.product_name}<br />
+                                    {item.product_name} 有効期限: {item.is_expired === false ? `${item.effective_month}ヶ月` : '有効期限なし'} <br />
+                                    数量: {item.quantity}<br />
                                     ￥{item.price}
                                   </Col>
                                   <Col>
@@ -143,30 +120,55 @@ const Index: NextPage = () => {
                                 </Row>
                               </ListGroup.Item>
                             )
-                        default:
-                          return (<></>)
+                            case 'MonthlyPaymentPlan':
+                              return (
+                                <ListGroup.Item key={i}>
+                                  <Row>
+                                    {item.s3_object_public_url && <Col><img
+                                        className='d-block w-100'
+                                        src={item.s3_object_public_url}
+                                        alt='image'/></Col>}
+                                    <Col>
+                                      {item.business_name}<br/>
+                                      {item.product_name}<br />
+                                      ￥{item.price}
+                                    </Col>
+                                    <Col>
+                                      <Button
+                                        onClick={() => deleteCartItem(item.public_id, item.item_type)}
+                                        size='sm'
+                                        variant='danger'>
+                                        カートから削除
+                                      </Button>
+                                    </Col>
+                                  </Row>
+                                </ListGroup.Item>
+                              )
+                          default:
+                            return (<></>)
+                        }
                       }
-                    }
-                  })}
-                </ListGroup>
-                {<div>{cartItems?.length === 0}</div>}
-                {cartItems?.length === 0 && <div>カートに商品が入っていません</div>}
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col lg={2} md={5}>
-            <Card>
-              <Card.Body>
-                {cartItems && <><div>小計</div>
-                <h5>¥{totalPrice}</h5>
-                <br /></>}
-                {cartItems?.length !== 0 && <a href='/cash_register'
-                   className='btn btn-info text-white'>レジに進む</a>}
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+                    })}
+                  </ListGroup>
+                  {<div>{cartItems?.length === 0}</div>}
+                  {cartItems?.length === 0 && <div>カートに商品が入っていません</div>}
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col lg={2} md={5}>
+              <Card className='mt30 mb30'>
+                <Card.Body>
+                  {cartItems && <><div>小計</div>
+                  <h5>¥{totalPrice}</h5>
+                  <br /></>}
+                  {cartItems?.length !== 0 && <a href='/cash_register'
+                    className='btn btn-info text-white'>レジに進む</a>}
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
       <PrefecturesChargeModal></PrefecturesChargeModal>
     </EndUserLoginLayout>
   )
