738bb65046a7b24d79c56b8262db37be88c9a269
ikeyu0806 <ikeyu0806@gmail.com>
2023-01-31 12:28:38 +0900

    add company mission

--- frontend/pages/company_info/index.tsx
+++ frontend/pages/company_info/index.tsx
@@ -37,7 +37,7 @@ const Index: NextPage = () => {
                   <div className={companyStyles.strong_text}>事業内容</div>
                 </Col>
                 <Col lg={9}>
-                  <div className={companyStyles.content_text}>クラウドサービス「SquareEight」の開発・運営</div>
+                  <div className={companyStyles.content_text}>クラウドサービスの開発・運営</div>
                 </Col>
               </Row>
               <hr />
@@ -71,6 +71,19 @@ const Index: NextPage = () => {
         <div className='mt20'>
           <Row>
             <Col md={6}>
+              <div className={companyStyles.company_overview_text}>企業ミッション</div>
+            </Col>
+            <Col md={6}>
+              <div className='mb20 font-size-25'>
+                多機能で使いやすい高品質、高価格なクラウドサービスを提供します。
+              </div>
+            </Col>
+          </Row>
+        </div>
+        <hr />
+        <div className='mt20'>
+          <Row>
+            <Col md={6}>
               <div className={companyStyles.company_overview_text}>サービス</div>
             </Col>
             <Col md={6}>
@@ -79,13 +92,7 @@ const Index: NextPage = () => {
                 src='/images/service_top_image.png'
                 alt='Saas Top Image' />
               <div className={companyStyles.service_description}>
-                高機能な予約受付や顧客管理を高価格で提供するクラウドサービス。
-              </div>
-              <div className={companyStyles.service_description}>
-                管理画面から入力した情報を元にWebページ、予約ページ、商品販売ページ、アンケートページを作成。
-              </div>
-              <div className={companyStyles.service_description}>
-                メールやLINEでアフターフォローできます。
+                ビジネス業務を効率化、運営コストを削減できるオンラインビジネス運営クラウドサービス。
               </div>
             </Col>
           </Row>
