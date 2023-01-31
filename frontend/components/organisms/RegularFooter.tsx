c661331c056a9a437ea9ed1a99efc7f770da7aed
ikeyu0806 <ikeyu0806@gmail.com>
2023-01-31 15:27:50 +0900

    change footer text

--- frontend/components/organisms/RegularFooter.tsx
+++ frontend/components/organisms/RegularFooter.tsx
@@ -4,7 +4,7 @@ const RegularFooter = (): JSX.Element => {
   return (
     <footer className='content text-center'>
       <p className='footer-margin mt20'>
-        Copyright <a href='/administrator'>SquareEight Administrator</a> {new Date().getFullYear()}
+        Copyright SquareEight.Inc {new Date().getFullYear()}
       </p>
     </footer>
   )
