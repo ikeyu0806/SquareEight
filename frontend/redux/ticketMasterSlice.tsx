d8fab2700589a875df7e43eff581074a003556e9
ikeyu0806 <ikeyu0806@gmail.com>
2023-02-13 19:58:13 +0900

    add image state to ticketMasterSlice

--- frontend/redux/ticketMasterSlice.tsx
+++ frontend/redux/ticketMasterSlice.tsx
@@ -10,7 +10,16 @@ export const ticketMasterSlice = createSlice({
     effectiveMonth: 12,
     description: '',
     publishStatus: '',
-    base64Image: null,
+    ticketMasterImage1File: null as File | unknown,
+    ticketMasterImage2File:  null as File | unknown,
+    ticketMasterImage3File:  null as File | unknown,
+    ticketMasterImage4File:  null as File | unknown,
+    ticketMasterImage5File:  null as File | unknown,
+    ticketMasterImage1ImagePublicUrl: '',
+    ticketMasterImage2ImagePublicUrl: '',
+    ticketMasterImage3ImagePublicUrl: '',
+    ticketMasterImage4ImagePublicUrl: '',
+    ticketMasterImage5ImagePublicUrl: '',
     s3ObjectPublicUrl: '',
     shops: [] as ShopParam[],
     selectedShopIds: [] as number[]
@@ -34,8 +43,35 @@ export const ticketMasterSlice = createSlice({
     publishStatusChanged: (state, action: PayloadAction<string>) => {
       state.publishStatus = action.payload
     },
-    base64ImageChanged: (state, action: PayloadAction<any>) => {
-      state.base64Image = action.payload
+    ticketMasterImage1FileChanged: (state, action: PayloadAction<File>) => {
+      state.ticketMasterImage1File = action.payload
+    },
+    ticketMasterImage2FileChanged: (state, action: PayloadAction<File>) => {
+      state.ticketMasterImage2File = action.payload
+    },
+    ticketMasterImage3FileChanged: (state, action: PayloadAction<File>) => {
+      state.ticketMasterImage3File = action.payload
+    },
+    ticketMasterImage4FileChanged: (state, action: PayloadAction<File>) => {
+      state.ticketMasterImage4File = action.payload
+    },
+    ticketMasterImage5FileChanged: (state, action: PayloadAction<File>) => {
+      state.ticketMasterImage5File = action.payload
+    },
+    ticketMasterImage1ImagePublicUrlChanged: (state, action: PayloadAction<string>) => {
+      state.ticketMasterImage1ImagePublicUrl = action.payload
+    },
+    ticketMasterImage2ImagePublicUrlChanged: (state, action: PayloadAction<string>) => {
+      state.ticketMasterImage2ImagePublicUrl = action.payload
+    },
+    ticketMasterImage3ImagePublicUrlChanged: (state, action: PayloadAction<string>) => {
+      state.ticketMasterImage3ImagePublicUrl = action.payload
+    },
+    ticketMasterImage4ImagePublicUrlChanged: (state, action: PayloadAction<string>) => {
+      state.ticketMasterImage4ImagePublicUrl = action.payload
+    },
+    ticketMasterImage5ImagePublicUrlChanged: (state, action: PayloadAction<string>) => {
+      state.ticketMasterImage5ImagePublicUrl = action.payload
     },
     s3ObjectPublicUrlChanged: (state, action: PayloadAction<any>) => {
       state.s3ObjectPublicUrl = action.payload
@@ -55,7 +91,16 @@ export const { issueNumberChanged } = ticketMasterSlice.actions
 export const { effectiveMonthChanged } = ticketMasterSlice.actions
 export const { descriptionChanged } = ticketMasterSlice.actions
 export const { publishStatusChanged } = ticketMasterSlice.actions
-export const { base64ImageChanged } = ticketMasterSlice.actions
+export const { ticketMasterImage1FileChanged } = ticketMasterSlice.actions
+export const { ticketMasterImage2FileChanged } = ticketMasterSlice.actions
+export const { ticketMasterImage3FileChanged } = ticketMasterSlice.actions
+export const { ticketMasterImage4FileChanged } = ticketMasterSlice.actions
+export const { ticketMasterImage5FileChanged } = ticketMasterSlice.actions
+export const { ticketMasterImage1ImagePublicUrlChanged } = ticketMasterSlice.actions
+export const { ticketMasterImage2ImagePublicUrlChanged } = ticketMasterSlice.actions
+export const { ticketMasterImage3ImagePublicUrlChanged } = ticketMasterSlice.actions
+export const { ticketMasterImage4ImagePublicUrlChanged } = ticketMasterSlice.actions
+export const { ticketMasterImage5ImagePublicUrlChanged } = ticketMasterSlice.actions
 export const { s3ObjectPublicUrlChanged } = ticketMasterSlice.actions
 export const { shopsChanged } = ticketMasterSlice.actions
 export const { selectedShopIdsChanged } = ticketMasterSlice.actions
