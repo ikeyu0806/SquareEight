38abd831d8e4d6998b86ee4f58fae58a615411f3
ikeyu0806 <ikeyu0806@gmail.com>
2023-02-16 16:42:02 +0900

    add stripFile upload style

--- frontend/components/molecules/StripeFileRequirement.tsx
+++ frontend/components/molecules/StripeFileRequirement.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
-
+import StripeFormStyles from 'styles/StripeForm.module.css'
 const StripeFileRequirement = () => {
   return (
     <Card className='mt20'>
       <Card.Body>
-      アップロードするファイルは以下の要件を満たしている必要があります。<br />
+      <p className={StripeFormStyles.upload_image_guide}>アップロードするファイルは以下の要件を満たしている必要があります。</p>
       &emsp;・カラー画像 (8,000 ピクセル x 8,000 ピクセル以下)<br />
       &emsp;・10 MB 以下<br />
       &emsp;・JPG または PNG 形式が使用可能<br />
