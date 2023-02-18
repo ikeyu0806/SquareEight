b993be4e9752215d7744be5bd75dcb32d16d01da
ikeyu0806 <ikeyu0806@gmail.com>
2023-02-18 19:52:03 +0900

    remove image type from stripe image guide

--- frontend/components/molecules/StripeFileRequirement.tsx
+++ frontend/components/molecules/StripeFileRequirement.tsx
@@ -8,7 +8,6 @@ const StripeFileRequirement = () => {
       <p className={StripeFormStyles.upload_image_guide}>アップロードするファイルは以下の要件を満たしている必要があります。</p>
       &emsp;・カラー画像 (8,000 ピクセル x 8,000 ピクセル以下)<br />
       &emsp;・10 MB 以下<br />
-      &emsp;・JPG または PNG 形式が使用可能<br />
       </Card.Body>
     </Card>
   )
