553c1cce52fb7d51caf28ed4819a4087cea44f97
ikeyu0806 <ikeyu0806@gmail.com>
2023-02-21 09:38:32 +0900

    fix implement register navbar image

--- rails_api/app/controllers/api/internal/shared_components_controller.rb
+++ rails_api/app/controllers/api/internal/shared_components_controller.rb
@@ -5,6 +5,7 @@ class Api::Internal::SharedComponentsController < ApplicationController
 
   def show
     shared_component = current_merchant_user.account.shared_component
+    shared_component = JSON.parse(shared_component.to_json(methods: [:navbar_image_account_s3_image_public_url]))
     render json: { status: 'success', shared_component: shared_component }, status: 200
   rescue => error
     Rails.logger.error error
@@ -16,14 +17,8 @@ class Api::Internal::SharedComponentsController < ApplicationController
       shared_component = current_merchant_user.account.shared_component
       shared_component = SharedComponent.new(account: current_merchant_user.account) if shared_component.blank?
       # ブランドイメージはS3に登録
-      if form_type_params[:navbar_brand_image].present? && form_type_params[:is_update_navbar_brand_image]
-        file_name = "shared_component_image_" + Time.zone.now.strftime('%Y%m%d%H%M%S%3N')
-        shared_component.navbar_brand_image_s3_object_public_url = put_s3_http_request_base64_data(form_type_params[:navbar_brand_image], ENV["SHARED_COMPONENT_IMAGE_BUCKET"], file_name)
-        shared_component.nabvar_brand_image_s3_object_name = file_name
-      end
-      if params[:navbar_brand_image].present? && !params[:product_image1_file].eql?("null")
-        file_name = "shared_component_image_" + Time.zone.now.strftime('%Y%m%d%H%M%S%3N')
-        product.register_s3_image(file_name, params[:navbar_brand_image], "image1_account_s3_image_id")
+      if params[:navbar_brand_image].present? && !params[:navbar_brand_image].eql?("null")
+        shared_component.register_navbar_image(params[:navbar_brand_image])
       end
       shared_component.attributes = form_type_params.except(:navbar_brand_image, :is_update_navbar_brand_image)
       shared_component.save!
--- rails_api/app/models/shared_component.rb
+++ rails_api/app/models/shared_component.rb
@@ -1,5 +1,6 @@
 class SharedComponent < ApplicationRecord
   include PublicIdModule
+  include Base64Image
 
   belongs_to :account
 
@@ -7,5 +8,17 @@ class SharedComponent < ApplicationRecord
     return nil if navbar_image_account_s3_image_id.blank?
     account_image = AccountS3Image.find(self.navbar_image_account_s3_image_id)
     AccountS3Image.find(self.navbar_image_account_s3_image_id).s3_object_public_url
+  end
+
+  def register_navbar_image(image_file)
+    file_name = "shared_component_image_" + Time.zone.now.strftime('%Y%m%d%H%M%S%3N')
+    navbar_image_account_s3_image_public_url = put_s3_http_request_form_data(image_file, ENV["PRODUCT_IMAGE_BUCKET"], file_name)
+    account_image = AccountS3Image.new
+    account_image.account = self.account
+    account_image.s3_object_public_url = navbar_image_account_s3_image_public_url
+    account_image.s3_object_name = file_name
+    account_image.save!
+    self.navbar_image_account_s3_image_id = account_image.id
+    self.save!
   end
 end
