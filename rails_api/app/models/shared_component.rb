4e6c6685b1292e8d80cfd726163b729edb931483
ikeyu0806 <ikeyu0806@gmail.com>
2023-02-21 09:22:59 +0900

    add navbar_image_account_s3_image_public_url

--- rails_api/app/models/shared_component.rb
+++ rails_api/app/models/shared_component.rb
@@ -2,4 +2,10 @@ class SharedComponent < ApplicationRecord
   include PublicIdModule
 
   belongs_to :account
+
+  def navbar_image_account_s3_image_public_url
+    return nil if navbar_image_account_s3_image_id.blank?
+    account_image = AccountS3Image.find(self.navbar_image_account_s3_image_id)
+    AccountS3Image.find(self.navbar_image_account_s3_image_id).s3_object_public_url
+  end
 end
