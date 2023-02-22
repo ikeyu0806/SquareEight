28040dfc6d84ab4a4aa6fd4c80ab4b8578e62734
ikeyu0806 <ikeyu0806@gmail.com>
2023-02-23 08:40:47 +0900

    replace order/[public_id]/index.tsx to Table

--- frontend/pages/admin/order/[public_id]/index.tsx
+++ frontend/pages/admin/order/[public_id]/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import React, { useEffect, useState } from 'react'
 import { useCookies } from 'react-cookie'
-import { Container, Row, Col, Card } from 'react-bootstrap'
+import { Container, Row, Col, Card, Table } from 'react-bootstrap'
 import axios from 'axios'
 import { useDispatch } from 'react-redux'
 import { useRouter } from 'next/router'
@@ -41,33 +41,30 @@ const Index: NextPage = () => {
     <MerchantUserAdminLayout>
       <br />
       <Container>
-        <Row>
-          <Col lg={3}></Col>
-          <Col lg={6}>
-            <Card>
-              <Card.Header>注文詳細</Card.Header>
-              <Card.Body>
-                <Row>
-                  <Col>
-                    合計金額: ¥{order && order.total_price}
-                    <br />注文日: {order && order.order_date}
-                  </Col>
-                  <Col>
-                  {orderItems && orderItems.map((item, i) => {
-                    return (
-                      <span key={i}>
-                        <div>購入先: {item.business_name}</div>
-                        <div>{item.product_name} ￥{item.price}</div>
-                        {item.delivery_date_text && <div>指定配送日時: {item.delivery_date_text}</div>}
-                      </span>
-                    )
-                  })}
-                  </Col>
-                </Row>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
+        <Table bordered>
+          <thead>
+            <tr>
+              <th>商品名</th>
+              <th>金額</th>
+              <th>注文日</th>
+              <th>購入先</th>
+              <th>配送日時指定</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orderItems && orderItems.map((item, i) => {
+            return (
+              <tr key={i}>
+                <td>{item.product_name}</td>
+                <td>¥{item.price}</td>
+                <td>{order && order.order_date}</td>
+                <td>{item.business_name}</td>
+                <td>{item.delivery_date_text}</td>
+              </tr>
+            )
+            })}
+          </tbody>
+        </Table>
       </Container>
     </MerchantUserAdminLayout>
   )
