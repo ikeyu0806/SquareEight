117443ab9b496e31a31560b25c825395637f9a9d
ikeyu0806 <ikeyu0806@gmail.com>
2023-03-02 08:52:57 +0900

    fix customer_page/system_notification bug

--- frontend/pages/customer_page/system_notification/[public_id]/index.tsx
+++ frontend/pages/customer_page/system_notification/[public_id]/index.tsx
@@ -15,15 +15,15 @@ const Index: NextPage = () => {
   useEffect(() => {
     const fetchNotification = () => {
       axios.get(
-        `${process.env.BACKEND_URL}/api/internal/end_user_notifications/${router.query.public_id}`, {
+        `${process.env.BACKEND_URL}/api/internal/system_end_user_notifications/${router.query.public_id}`, {
           headers: { 
             'Session-Id': cookies._square_eight_end_user_session
           },
         }
       )
       .then( (response) => {
-        setTitle(response.data.end_user_notification.title)
-        setContent(response.data.end_user_notification.content)
+        setTitle(response.data.system_end_user_notification.title)
+        setContent(response.data.system_end_user_notification.content)
       })
       .catch(error => {
         console.log(error)
