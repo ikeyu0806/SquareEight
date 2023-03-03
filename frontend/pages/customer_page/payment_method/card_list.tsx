7f4d52d4cab4da7f20e972b0c622d2140639b03c
ikeyu0806 <ikeyu0806@gmail.com>
2023-03-03 14:08:24 +0900

    add margin to customer_page/payment_method/card_list

--- frontend/pages/customer_page/payment_method/card_list.tsx
+++ frontend/pages/customer_page/payment_method/card_list.tsx
@@ -93,7 +93,7 @@ const CardList: NextPage = () => {
           <Row>
             <Col lg={3} md={3}></Col>
             <Col lg={6} md={8}>
-              <Card>
+              <Card className='mt20'>
                 <Card.Header>クレジットカード一覧</Card.Header>
                 <Card.Body>
                   <Card.Text>
