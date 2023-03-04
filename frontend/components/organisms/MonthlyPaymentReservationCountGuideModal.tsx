fed3dc507c3be7735eca9b2f4574191359a5c3e2
ikeyu0806 <ikeyu0806@gmail.com>
2023-03-04 14:18:22 +0900

    add lottery status to reserveFrame validation

--- frontend/components/organisms/MonthlyPaymentReservationCountGuideModal.tsx
+++ frontend/components/organisms/MonthlyPaymentReservationCountGuideModal.tsx
@@ -13,9 +13,15 @@ const MonthlyPaymentReservationCountGuideModal = () => {
       <Modal.Header>月額サブスクリプションの予約可能数設定について</Modal.Header>
       <Modal.Body>
         <div>予約の支払いに月額サブスクリプションを使用した場合の予約可能数を設定することができます。</div>
+        <hr />
+        <div className='font-weight-bold'>予約可能数の判定について</div>
         <div className='mt20'>週間ごとの予約可能数を設定した場合、その週の日曜日から土曜日までの予約可能数に制限がかかります。</div>
         <div className='mt20'>例）「3日に2回予約可能」を設定した場合、予約日から前後2日に予約が2回入っていた場合予約不可能になります。</div>
         <div className='mt20'>例）「3週に2回予約可能」を設定した場合、予約日の2週前の日曜日から予約日の2週後の土曜日に予約が2回入っていた場合予約不可能になります。</div>
+        <hr />
+        <div className='font-weight-bold'>対象の予約ステータスについて</div>
+        <div className='mt20'>予約ステータスが「確定」「抽選結果待ち」の予約に対して制限がかかります。</div>
+        <div className='mt20'>予約ステータスが「キャンセル」「予約確定待ち」の予約は判定から除外されます。</div>
       </Modal.Body>
       <Modal.Footer>
         <Button
--- rails_api/app/models/reserve_frame.rb
+++ rails_api/app/models/reserve_frame.rb
@@ -564,17 +564,17 @@ class ReserveFrame < ApplicationRecord
           if reservation.monthly_payment_plan.enable_reserve_count == 1
             target_day_reservation_count = self.reservations
               .where(start_at: reservation.start_at..reservation.start_at.end_of_day)
-              .where(status: 'confirm')
+              .where(status: ['confirm', 'waitingForLotteryConfirm'])
               .where(monthly_payment_plan_id: monthly_payment_plan.id).count
           else
             front_and_back_num = reservation.monthly_payment_plan.enable_reserve_count - 1
             target_day_reservation_count_before = self.reservations
               .where(start_at: (reservation.start_at - front_and_back_num.days)..(reservation.start_at.end_of_day))
-              .where(status: 'confirm')
+              .where(status: ['confirm', 'waitingForLotteryConfirm'])
               .where(monthly_payment_plan_id: monthly_payment_plan.id).count
             target_day_reservation_count_after = self.reservations
               .where(start_at: (reservation.start_at)..(reservation.start_at.end_of_day + front_and_back_num.days))
-              .where(status: 'confirm')
+              .where(status: ['confirm', 'waitingForLotteryConfirm'])
               .where(monthly_payment_plan_id: monthly_payment_plan.id).count
             target_day_reservation_count = target_day_reservation_count_before + target_day_reservation_count_after
           end
@@ -583,17 +583,17 @@ class ReserveFrame < ApplicationRecord
           if reservation.monthly_payment_plan.enable_reserve_count == 1
             target_day_reservation_count = self.reservations
               .where(start_at: reservation.start_at.end_of_week(:sunday)..reservation.start_at.end_of_week(:saturday))
-              .where(status: 'confirm')
+              .where(status: ['confirm', 'waitingForLotteryConfirm'])
               .where(monthly_payment_plan_id: monthly_payment_plan.id).count
           else
             front_and_back_num = reservation.enable_reserve_count - 1
             target_day_reservation_count_before = self.reservations
               .where(start_at: (reservation.start_at - front_and_back_num.week).end_of_week(:sunday)..reservation.start_at.end_of_week(:saturday))
-              .where(status: 'confirm')
+              .where(status: ['confirm', 'waitingForLotteryConfirm'])
               .where(monthly_payment_plan_id: monthly_payment_plan.id).count
             target_day_reservation_count_after = self.reservations
               .where(start_at: (reservation.start_at).end_of_week(:sunday)..(reservation.start_at + front_and_back_num.week).end_of_week(:saturday))
-              .where(status: 'confirm')
+              .where(status: ['confirm', 'waitingForLotteryConfirm'])
               .where(monthly_payment_plan_id: monthly_payment_plan.id).count
             target_day_reservation_count = target_day_reservation_count_before + target_day_reservation_count_after
           end
