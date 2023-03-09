792424bff78eb062315b05f9fe38fcc041d932de
ikeyu0806 <ikeyu0806@gmail.com>
2023-03-10 08:36:33 +0900

    add pagination to notification/account/list

--- rails_api/app/controllers/api/internal/account_notifications_controller.rb
+++ rails_api/app/controllers/api/internal/account_notifications_controller.rb
@@ -3,7 +3,15 @@ class Api::Internal::AccountNotificationsController < ApplicationController
 
   def index
     account_notifications = current_merchant_user.account.account_notifications.order(id: :desc)
-    render json: { status: 'success', account_notifications: account_notifications }, status: 200
+    # ページネーション
+    current_page = params[:current_page].to_i
+    display_count = params[:display_count].to_i
+    last_page, remainder = account_notifications.count.divmod(display_count)
+    last_page += 1 if remainder.positive?
+    account_notifications = account_notifications.first(current_page * display_count).last(display_count)
+    render json: { status: 'success',
+                   account_notifications: account_notifications,
+                   last_page: last_page }, status: 200
   rescue => error
     Rails.logger.error error
     render json: { status: 'fail', error: error }, status: 500
