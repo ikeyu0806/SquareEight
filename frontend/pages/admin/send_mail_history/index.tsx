57eace8f9f814531c4c550565d8792009656a821
ikeyu0806 <ikeyu0806@gmail.com>
2023-03-10 10:35:28 +0900

    add pagination to send_mail_histories

--- frontend/pages/admin/send_mail_history/index.tsx
+++ frontend/pages/admin/send_mail_history/index.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { NextPage } from 'next'
 import { useCookies } from 'react-cookie'
 import MerchantUserAdminLayout from 'components/templates/MerchantUserAdminLayout'
-import { Table, Container, Button } from 'react-bootstrap'
+import { Table, Container, Button, Pagination } from 'react-bootstrap'
 import axios from 'axios'
 import { SendMailHistoryParam } from 'interfaces/SendMailHistoryParam'
 import MessageBodyModal from 'components/templates/MessageBodyModal'
@@ -12,11 +12,23 @@ import { showMessageBodyModalChanged,
          selectedHtmlTemplateTypeChanged,
          selectedParsedMessageBodyChanged } from 'redux/sendMailHistorySlice'
 import { useDispatch } from 'react-redux'
+import { usePaginationNumber } from 'hooks/usePaginationNumber'
 
 const Index: NextPage = () => {
   const dispatch = useDispatch()
   const [cookies] = useCookies(['_square_eight_merchant_session'])
   const [sendMailHistories, setSendMailHistories] = useState<SendMailHistoryParam[]>([])
+  // Pagination用
+  // 表示するレコード数
+  const displayCount = 10
+  const [currentPage, setCurrentPage] = useState(1)
+  const [lastPage, setLastPage] = useState(1000)
+  let usePaginationNumberReturnVal = usePaginationNumber(currentPage, lastPage)
+  let firstPaginationNum: number = usePaginationNumberReturnVal[0]
+  let secondPaginationNum: number = usePaginationNumberReturnVal[1]
+  let thirdPaginationNum: number = usePaginationNumberReturnVal[2]
+  let forthPaginationNum: number = usePaginationNumberReturnVal[3]
+  let fifthPaginationNum: number = usePaginationNumberReturnVal[4]
 
   useEffect(() => {
     const fetchResources = () => {
@@ -25,17 +37,22 @@ const Index: NextPage = () => {
           headers: { 
             'Session-Id': cookies._square_eight_merchant_session
           },
+          params: {
+            current_page: currentPage,
+            display_count: displayCount
+          }
         }
       )
       .then(function (response) {
         setSendMailHistories(response.data.send_mail_histories)
+        setLastPage(response.data.last_page)
       })
       .catch(error => {
         console.log(error)
       })
     }
     fetchResources()
-  }, [cookies._square_eight_merchant_session])
+  }, [cookies._square_eight_merchant_session, currentPage, lastPage])
 
   return (
     <MerchantUserAdminLayout>
@@ -74,6 +91,29 @@ const Index: NextPage = () => {
           })}
           </tbody>
         </Table>
+        <Pagination>
+          <Pagination.First onClick={() => setCurrentPage(1)} />
+          {currentPage > 1 && <Pagination.Prev
+            onClick={() => setCurrentPage(currentPage - 1)} />}
+          <Pagination.Item
+            active={currentPage == firstPaginationNum}
+            onClick={() => setCurrentPage(firstPaginationNum)}>{firstPaginationNum}</Pagination.Item>
+          {lastPage > 1 && <Pagination.Item
+            active={currentPage == secondPaginationNum}
+            onClick={() => setCurrentPage(secondPaginationNum)}>{secondPaginationNum}</Pagination.Item>}
+          {lastPage > 2 && <Pagination.Item
+            active={currentPage == thirdPaginationNum}
+            onClick={() => setCurrentPage(thirdPaginationNum)}>{thirdPaginationNum}</Pagination.Item>}
+          {lastPage > 3 && currentPage < lastPage &&  <Pagination.Item
+            active={currentPage == forthPaginationNum}
+            onClick={() => setCurrentPage(forthPaginationNum)}>{forthPaginationNum}</Pagination.Item>}
+          {lastPage > 4 && currentPage < lastPage - 1 && <Pagination.Item
+            active={currentPage == fifthPaginationNum}
+            onClick={() => setCurrentPage(fifthPaginationNum)}>{fifthPaginationNum}</Pagination.Item>}
+          {currentPage !== lastPage && <Pagination.Next
+            onClick={() => setCurrentPage(currentPage + 1)} />}
+          <Pagination.Last onClick={() => setCurrentPage(lastPage)} />
+        </Pagination>
       </Container>
       <MessageBodyModal></MessageBodyModal>
     </MerchantUserAdminLayout>
--- rails_api/app/controllers/api/internal/send_mail_histories_controller.rb
+++ rails_api/app/controllers/api/internal/send_mail_histories_controller.rb
@@ -3,8 +3,14 @@ class Api::Internal::SendMailHistoriesController < ApplicationController
 
   def index
     send_mail_histories = current_merchant_user.account.send_mail_histories.order(created_at: :desc)
+    # ページネーション
+    current_page = params[:current_page].to_i
+    display_count = params[:display_count].to_i
+    last_page, remainder = send_mail_histories.count.divmod(display_count)
+    last_page += 1 if remainder.positive?
+    send_mail_histories = send_mail_histories.first(current_page * display_count).last(display_count)
     send_mail_histories = JSON.parse(send_mail_histories.to_json(methods: [:send_at, :customer_fullname, :parsed_message_body]))
-    render json: { status: 'success', send_mail_histories: send_mail_histories }, status: 200
+    render json: { status: 'success', send_mail_histories: send_mail_histories, last_page: last_page }, status: 200
   rescue => error
     Rails.logger.error error
     render json: { status: 'fail', error: error }, status: 500
