2199e8c20db609946b71de949a7d2cbbf5b57245
ikeyu0806 <ikeyu0806@gmail.com>
2023-03-11 08:10:33 +0900

    add gtag to GoogleAuthButton

--- frontend/components/atoms/GoogleAuthButton.tsx
+++ frontend/components/atoms/GoogleAuthButton.tsx
@@ -1,14 +1,26 @@
 import GoogleIcon from 'components/atoms/GoogleIcon'
+import { useRouter } from 'next/router'
 interface Props {
   buttonText: string
   buttonHref: string
+  isMerchantUserSignUp?: boolean
 }
 
-const GoogleAuthButton = ({buttonText, buttonHref}: Props): JSX.Element => {
+const GoogleAuthButton = ({buttonText, buttonHref, isMerchantUserSignUp}: Props): JSX.Element => {
+  const router = useRouter()
+
+  const onSubmit = () => {
+    isMerchantUserSignUp && (window as any).gtag("event", "business_account_signup_complete", {
+      // event_category: "hoge",
+      // event_label: "huga",
+    })
+    router.push(buttonHref)
+  }
+
   return (
     <div className='mt20 text-center'>
       <a className='btn btn-outline-primary'
-         href={buttonHref}>
+         onClick={() => onSubmit()}>
         <GoogleIcon width={20} height={20} className={'mr10'}></GoogleIcon>{buttonText}
       </a>
     </div>
