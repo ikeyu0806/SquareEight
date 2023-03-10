bbabb9fa3a1b10a87f76bb5ef1fe1796f8ac26f6
ikeyu0806 <ikeyu0806@gmail.com>
2023-03-10 13:49:13 +0900

    add pagination to send_line_histories

--- frontend/pages/admin/send_line_history.tsx/index.tsx
+++ frontend/pages/admin/send_line_history.tsx/index.tsx
@@ -2,17 +2,29 @@ import React, { useState, useEffect } from 'react'
 import { NextPage } from 'next'
 import { useCookies } from 'react-cookie'
 import MerchantUserAdminLayout from 'components/templates/MerchantUserAdminLayout'
-import { Table, Container, Button } from 'react-bootstrap'
+import { Table, Container, Button, Pagination } from 'react-bootstrap'
 import axios from 'axios'
 import { SendLineHistoryParam } from 'interfaces/SendLineHistoryParam'
 import { useDispatch } from 'react-redux'
 import { showLineMessageModalChanged, selectedMessageChanged } from 'redux/sendLineSlice'
 import LineMessageBodyModal from 'components/templates/LineMessageBodyModal'
+import { usePaginationNumber } from 'hooks/usePaginationNumber'
 
 const Index: NextPage = () => {
   const dispatch = useDispatch()
   const [cookies] = useCookies(['_square_eight_merchant_session'])
   const [sendLineHistories, setSendLineHistories] = useState<SendLineHistoryParam[]>([])
+  // Pagination用
+  // 表示するレコード数
+  const displayCount = 10
+  const [currentPage, setCurrentPage] = useState(1)
+  const [lastPage, setLastPage] = useState(1000)
+  let usePaginationNumberReturnVal = usePaginationNumber(currentPage, lastPage)
+  let firstPaginationNum: number = usePaginationNumberReturnVal[0]
+  let secondPaginationNum: number = usePaginationNumberReturnVal[1]
+  let thirdPaginationNum: number = usePaginationNumberReturnVal[2]
+  let forthPaginationNum: number = usePaginationNumberReturnVal[3]
+  let fifthPaginationNum: number = usePaginationNumberReturnVal[4]
 
   useEffect(() => {
     const fetchResources = () => {
@@ -21,11 +33,16 @@ const Index: NextPage = () => {
           headers: { 
             'Session-Id': cookies._square_eight_merchant_session
           },
+          params: {
+            current_page: currentPage,
+            display_count: displayCount
+          }
         }
       )
       .then(function (response) {
         console.log(response.data)
         setSendLineHistories(response.data.send_line_histories)
+        setLastPage(response.data.last_page)
       })
       .catch(error => {
         console.log(error)
@@ -65,6 +82,29 @@ const Index: NextPage = () => {
             })}
           </tbody>
         </Table>
+        <Pagination>
+          <Pagination.First onClick={() => setCurrentPage(1)} />
+          {currentPage > 1 && <Pagination.Prev
+            onClick={() => setCurrentPage(currentPage - 1)} />}
+          <Pagination.Item
+            active={currentPage == firstPaginationNum}
+            onClick={() => setCurrentPage(firstPaginationNum)}>{firstPaginationNum}</Pagination.Item>
+          {lastPage > 1 && <Pagination.Item
+            active={currentPage == secondPaginationNum}
+            onClick={() => setCurrentPage(secondPaginationNum)}>{secondPaginationNum}</Pagination.Item>}
+          {lastPage > 2 && <Pagination.Item
+            active={currentPage == thirdPaginationNum}
+            onClick={() => setCurrentPage(thirdPaginationNum)}>{thirdPaginationNum}</Pagination.Item>}
+          {lastPage > 3 && currentPage < lastPage &&  <Pagination.Item
+            active={currentPage == forthPaginationNum}
+            onClick={() => setCurrentPage(forthPaginationNum)}>{forthPaginationNum}</Pagination.Item>}
+          {lastPage > 4 && currentPage < lastPage - 1 && <Pagination.Item
+            active={currentPage == fifthPaginationNum}
+            onClick={() => setCurrentPage(fifthPaginationNum)}>{fifthPaginationNum}</Pagination.Item>}
+          {currentPage !== lastPage && <Pagination.Next
+            onClick={() => setCurrentPage(currentPage + 1)} />}
+          <Pagination.Last onClick={() => setCurrentPage(lastPage)} />
+        </Pagination>
       </Container>
       <LineMessageBodyModal></LineMessageBodyModal>
     </MerchantUserAdminLayout>
--- rails_api/app/controllers/api/internal/send_line_histories_controller.rb
+++ rails_api/app/controllers/api/internal/send_line_histories_controller.rb
@@ -3,8 +3,16 @@ class Api::Internal::SendLineHistoriesController < ApplicationController
 
   def index
     send_line_histories = current_merchant_user.account.send_line_histories.order(created_at: :desc)
+    # ページネーション
+    current_page = params[:current_page].to_i
+    display_count = params[:display_count].to_i
+    last_page, remainder = send_line_histories.count.divmod(display_count)
+    last_page += 1 if remainder.positive?
+    send_line_histories = send_line_histories.first(current_page * display_count).last(display_count)
     send_line_histories = JSON.parse(send_line_histories.to_json(methods: [:line_official_account_name, :line_user_display_name]))
-    render json: { status: 'success', send_line_histories: send_line_histories }, status: 200
+    render json: { status: 'success',
+                   send_line_histories: send_line_histories,
+                   last_page: last_page }, status: 200
   rescue => error
     Rails.logger.error error
     render json: { status: 'fail', error: error }, status: 500
