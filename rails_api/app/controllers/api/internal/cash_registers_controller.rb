1ab5955cf10f73acbd57566cc397a7b118c66839
ikeyu0806 <ikeyu0806@gmail.com>
2023-03-12 10:57:58 +0900

    fix product commision bug

--- rails_api/app/controllers/api/internal/cash_registers_controller.rb
+++ rails_api/app/controllers/api/internal/cash_registers_controller.rb
@@ -72,7 +72,7 @@ class Api::Internal::CashRegistersController < ApplicationController
             payment_method_types: ['card'],
             payment_method: default_payment_method_id,
             customer: current_end_user.stripe_customer_id,
-            application_fee_amount: commission * cart[:quantity],
+            application_fee_amount: commission,
             metadata: {
               'order_date': current_date_text,
               'product_id': product.id,
