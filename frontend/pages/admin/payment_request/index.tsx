6610330625f9502a288ae243b8a69eced613fdcd
ikeyu0806 <ikeyu0806@gmail.com>
2023-03-13 08:23:54 +0900

    add empty condition to paymentRequest index page

--- frontend/pages/admin/payment_request/index.tsx
+++ frontend/pages/admin/payment_request/index.tsx
@@ -63,50 +63,50 @@ const Index: NextPage = () => {
       {allowReadPaymentRequest === 'Allow' &&
       <Container>
         <h3>決済リクエスト一覧</h3>
-        <Table bordered>
-        <thead>
-          <tr>
-            <th>金額</th>
-            <th>送信方法</th>
-            <th>請求先顧客</th>
-            <th>請求先LINEユーザ</th>
-            <th>メールアドレス</th>
-            <th>決済ステータス</th>
-            <th>決済URL</th>
-          </tr>
-        </thead>
-        <tbody>
-          {paymentRequests.map((request, i) => {
-            return (
-              <tr key={i}>
-                <td>￥{request.price}</td>
-                <td>{messageSendMethodText(request.send_method)}</td>
-                <td>{request.billing_customer_name}</td>
-                <td>
-                  {request.line_picture_url && <img
-                    className={lineUserStyles.line_picture_url}
-                    src={request.line_picture_url}
-                    alt='line_picture_url' />}
-                  <span className='ml10'>{request.line_display_name}</span>
-                </td>
-                <td>{request.billing_customer_email}</td>
-                <td>
-                  {request.status === 'Pending' && <span  className='badge bg-danger'>未払い</span>}
-                  {request.status === 'Paid' && <span  className='badge bg-info'>支払い済み</span>}
-                </td>
-                <td>
-                  <a
-                    href={request.request_url}
-                    target='_blank'
-                    rel='noreferrer'
-                    className='btn btn-primary'>決済URL</a>
-                </td>
+        {paymentRequests.length > 0 && <>
+          <Table bordered>
+            <thead>
+              <tr>
+                <th>金額</th>
+                <th>送信方法</th>
+                <th>請求先顧客</th>
+                <th>請求先LINEユーザ</th>
+                <th>メールアドレス</th>
+                <th>決済ステータス</th>
+                <th>決済URL</th>
               </tr>
-            )
-          })}
-        </tbody>
+            </thead>
+            <tbody>
+              {paymentRequests.map((request, i) => {
+                return (
+                  <tr key={i}>
+                    <td>￥{request.price}</td>
+                    <td>{messageSendMethodText(request.send_method)}</td>
+                    <td>{request.billing_customer_name}</td>
+                    <td>
+                      {request.line_picture_url && <img
+                        className={lineUserStyles.line_picture_url}
+                        src={request.line_picture_url}
+                        alt='line_picture_url' />}
+                      <span className='ml10'>{request.line_display_name}</span>
+                    </td>
+                    <td>{request.billing_customer_email}</td>
+                    <td>
+                      {request.status === 'Pending' && <span  className='badge bg-danger'>未払い</span>}
+                      {request.status === 'Paid' && <span  className='badge bg-info'>支払い済み</span>}
+                    </td>
+                    <td>
+                      <a
+                        href={request.request_url}
+                        target='_blank'
+                        rel='noreferrer'
+                        className='btn btn-primary'>決済URL</a>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
         </Table>
-        <br />
         <Pagination>
           <Pagination.First onClick={() => setCurrentPage(1)} />
           {currentPage > 1 && <Pagination.Prev
@@ -130,6 +130,10 @@ const Index: NextPage = () => {
             onClick={() => setCurrentPage(currentPage + 1)} />}
           <Pagination.Last onClick={() => setCurrentPage(lastPage)} />
         </Pagination>
+        <br />
+       </>}
+       {paymentRequests.length === 0 &&
+       <div className='text-center font-size-25'>決済リクエスト送信履歴がありません</div>}
       </Container>}
       {allowReadPaymentRequest === 'Forbid' && <Unauthorized />}
       </MerchantUserAdminLayout>
