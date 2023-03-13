51fbf143fdbfbebbb9f9180a41f43da91c3c9540
ikeyu0806 <ikeyu0806@gmail.com>
2023-03-13 09:25:26 +0900

    change json-ld default type

--- frontend/constants/jsonLdData.tsx
+++ frontend/constants/jsonLdData.tsx
@@ -1,6 +1,6 @@
 export const defaultJsonLdData = {
   '@context': 'http://schema.org/',
-  '@type': 'Product',
+  '@type': 'Service',
   'name': 'SquareEight',
   'description': 'オンラインショップ開設、予約システム、回数券発行、サブスクリプション支払い対応、オンラインアンケート機能、顧客管理をオールインワンで提供。',
 }
