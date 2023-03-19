0cf8f5c8d14df073e0dec3238845ca31357b90b8
ikeyu0806 <ikeyu0806@gmail.com>
2023-03-19 16:33:25 +0900

    implement SystemSubscriptionsController exec_payment action

--- rails_api/app/controllers/api/batch/system_subscriptions_controller.rb
+++ rails_api/app/controllers/api/batch/system_subscriptions_controller.rb
@@ -2,5 +2,26 @@ class Api::Batch::SystemSubscriptionsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def exec_payment
+    ActiveRecord::Base.transaction do
+      Stripe.api_key = Rails.configuration.stripe[:secret_key]
+      Stripe.api_version = '2022-08-01'
+      SystemStripeSubscription.each do |subscription|
+        account = subscription.account
+        payment_intent = Stripe::PaymentIntent.create({
+          amount: account.plan_price,
+          currency: 'jpy',
+          payment_method_types: ['card'],
+          customer: account.stripe_customer_id,
+          metadata: account.stripe_serivice_plan_subscription_metadata
+        })
+        Stripe::PaymentIntent.confirm(
+          payment_intent.id
+        )
+      end
+      render json: { status: 'success' }, status: 200
+    end
+  rescue => error
+    Rails.logger.error error
+    render json: { status: 'fail', error: error }, status: 500
   end
 end
--- rails_api/app/models/system_stripe_subscription.rb
+++ rails_api/app/models/system_stripe_subscription.rb
@@ -1,5 +1,7 @@
 class SystemStripeSubscription < ApplicationRecord
   include PublicIdModule
 
+  belongs_to :account
+
   enum service_plan: { Free: 0, Light: 1, Standard: 2, Premium: 3 }
 end
