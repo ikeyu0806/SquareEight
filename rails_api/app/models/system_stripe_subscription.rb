ef244439c0015b7f547625e7ca5a3d0898252e0c
ikeyu0806 <ikeyu0806@gmail.com>
2023-03-20 09:37:01 +0900

    implement prorated_plan_price

--- rails_api/app/models/system_stripe_subscription.rb
+++ rails_api/app/models/system_stripe_subscription.rb
@@ -9,33 +9,15 @@ class SystemStripeSubscription < ApplicationRecord
   # 最終支払い日付から現在の日付までを計算
   # 請求日が29~31日の場合は前月の月末日から今月の月末日を確認。
   def prorated_plan_price
-    # 現在時刻
-    current_date = Time.zone.now
-    # 先月
-    last_month_date = Time.zone.now - 1.months
-    # 先月の最終日
-    last_month_end_date = last_month_date.end_of_month
-    # 先月の最終日の日を数値で取得。28~31が出てくる
-    last_month_end_day = last_month_end_date.day
-    # 請求日を数値で取得。1~31が出てくる
-    billing_cycle_anchor_day = billing_cycle_anchor_datetime.day
-    # 先月の請求日取得
-    # 例: 請求日が31日で先月が30日までしかなければ30日が請求日
-    if (billing_cycle_anchor_day > 27) && (billing_cycle_anchor_day >= last_month_end_day)
-      last_month_billing_end_day = last_month_end_day
-    else
-      last_month_billing_end_day = billing_cycle_anchor_day
-    end
-    # 支払いが一度もなければ加入日起点
-    last_month_paid_date = last_paid_at.blank? ? created_at : last_paid_at
-    last_month_billing_date = Date.new(last_month_date.year, last_month_date.month, billing_date.day)
-
+    plan_price = account.plan_price
     # 請求日と今日の日にちが一致。もしくは請求日が月末で今日も月末なら満額請求
     if require_full_bill
-      plan_price = account.plan_price
-      result = plan_price
+      prorated_price = plan_price
+    # それ以外なら前回の請求日から今月までの差分で割る
+    else
+      prorated_price = plan_price / last_billing_to_today_days
     end
-    result
+    prorated_price
   end
 
   # 満額請求か判定
@@ -50,5 +32,13 @@ class SystemStripeSubscription < ApplicationRecord
     return true if current_day > last_month_day
     # それ以外はfalse
     return false
+  end
+
+  def last_billing_to_today_days
+    current_date = Time.zone.now
+    # 支払いが一度もなければ加入日起点
+    paid_date = last_paid_at.blank? ? created_at : last_paid_at
+    days = (current_date - paid_date).to_i
+    days
   end
 end
