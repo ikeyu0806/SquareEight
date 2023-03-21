6d930fe12965391c9865175517a8ef3d2c8fe5b9
ikeyu0806 <ikeyu0806@gmail.com>
2023-03-21 14:06:45 +0900

    fix monthly_payment_plan create action error

--- rails_api/app/controllers/api/internal/monthly_payment_plans_controller.rb
+++ rails_api/app/controllers/api/internal/monthly_payment_plans_controller.rb
@@ -113,6 +113,7 @@ class Api::Internal::MonthlyPaymentPlansController < ApplicationController
         file_name = "monthly_payment_plan_image5_" + Time.zone.now.strftime('%Y%m%d%H%M%S%3N')
         monthly_payment_plan.register_s3_image(file_name, params[:monthly_payment_plan_image5_file], "image5_account_s3_image_id")
       end
+      monthly_payment_plan.save!
       if params["reserve_frame_ids"].present?
         params["reserve_frame_ids"].each do |reserve_frame_id|
           monthly_payment_plan.reserve_frame_monthly_payment_plans.create!(reserve_frame_id: reserve_frame_id)
@@ -122,7 +123,6 @@ class Api::Internal::MonthlyPaymentPlansController < ApplicationController
         shop = Shop.find_by(public_id: s[:public_id])
         monthly_payment_plan.shop_monthly_payment_plans.create!(shop_id: shop.id)
       end
-      monthly_payment_plan.save!
       render json: { status: 'success' }, status: 200
     end
   rescue => error
