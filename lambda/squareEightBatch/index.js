f1f9ccc7d0f0971b04c65d2cecd8de10b35a7d20
ikeyu0806 <ikeyu0806@gmail.com>
2023-03-22 16:37:59 +0900

    fix batch lamnda indent

--- lambda/squareEightBatch/index.js
+++ lambda/squareEightBatch/index.js
@@ -30,7 +30,7 @@ exports.handler = (event, context, callback) => {
       path: path,
       method: 'POST',
       headers: {
-          'Content-Type': 'application/json'
+        'Content-Type': 'application/json'
       }
   }
   
@@ -40,20 +40,19 @@ exports.handler = (event, context, callback) => {
     text: "text",
   })
   const req = https.request(options, (res) => {
-      let body = '';
-      console.log('Status:', res.statusCode);
-      console.log('Headers:', JSON.stringify(res.headers));
-      res.setEncoding('utf8');
-      res.on('data', (chunk) => body += chunk);
-      res.on('end', () => {
-          console.log('Successfully processed HTTPS response');
-          // If we know it's JSON, parse it
-          if (res.headers['content-type'] === 'application/json') {
-              body = JSON.parse(body);
-          }
-          callback(null, body);
-      })
-      ;
+    let body = '';
+    console.log('Status:', res.statusCode);
+    console.log('Headers:', JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => body += chunk);
+    res.on('end', () => {
+      console.log('Successfully processed HTTPS response');
+      // If we know it's JSON, parse it
+      if (res.headers['content-type'] === 'application/json') {
+        body = JSON.parse(body);
+      }
+      callback(null, body);
+    });
   });
   req.on('error', callback);
   req.write(JSON.stringify(data));
