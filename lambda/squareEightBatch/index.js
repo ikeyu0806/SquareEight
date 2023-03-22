a0fa1b23d7f509f7d78d5479896fef3c8ebda16e
ikeyu0806 <ikeyu0806@gmail.com>
2023-03-22 16:34:08 +0900

    add subscription_exec_payment to batch lambda

--- lambda/squareEightBatch/index.js
+++ lambda/squareEightBatch/index.js
@@ -1,59 +1,61 @@
 const https = require('https');
 
 exports.handler = (event, context, callback) => {
-    console.log('event', event, event.enviroment, event.enviroment == 'development')
-    let hostname = '';
+  console.log('event', event, event.enviroment, event.enviroment == 'development')
+  let hostname = '';
 
-    if (event.enviroment == 'main') {
-        hostname = process.env.SQUARE_EIGHT_MAIN_HOSTNAME
-    }  else if (event.enviroment == 'staging') {
-        hostname = process.env.SQUARE_EIGHT_STAGING_HOSTNAME
-    }
-    
-    let path = '';
-    
-    if (event.batch_type == 'remind_tommorow_notifications') {
-        path = process.env.REMIND_TOMMOROW_NOTIFICATIONS_PATH
-    } else if (event.batch_type == 'confirm_lottery_reservations') {
-      path = process.env.CONFIRM_LOTTERY_RESERVATION_PATH
-    } else if (event.batch_type == 'send_same_hour_schedule_mail') {
-        path = process.env.SEND_SAME_HOUR_SCHEDULE_MAIL_PATH
-    }  else if (event.batch_type == 'send_same_hour_schedule_line') {
-        path = process.env.SEND_SAME_HOUR_SCHEDULE_LINE_PATH
-    }
-    
-    let options = {
-        hostname: hostname,
-        port: 443,
-        path: path,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }
-    
-    console.log("options", options)
-    
-    const data = JSON.stringify({
-      text: "text",
-    })
-    const req = https.request(options, (res) => {
-        let body = '';
-        console.log('Status:', res.statusCode);
-        console.log('Headers:', JSON.stringify(res.headers));
-        res.setEncoding('utf8');
-        res.on('data', (chunk) => body += chunk);
-        res.on('end', () => {
-            console.log('Successfully processed HTTPS response');
-            // If we know it's JSON, parse it
-            if (res.headers['content-type'] === 'application/json') {
-                body = JSON.parse(body);
-            }
-            callback(null, body);
-        })
-        ;
-    });
-    req.on('error', callback);
-    req.write(JSON.stringify(data));
-    req.end();
+  if (event.enviroment == 'main') {
+    hostname = process.env.SQUARE_EIGHT_MAIN_HOSTNAME
+  }  else if (event.enviroment == 'staging') {
+    hostname = process.env.SQUARE_EIGHT_STAGING_HOSTNAME
+  }
+  
+  let path = '';
+  
+  if (event.batch_type == 'remind_tommorow_notifications') {
+    path = process.env.REMIND_TOMMOROW_NOTIFICATIONS_PATH
+  } else if (event.batch_type == 'confirm_lottery_reservations') {
+    path = process.env.CONFIRM_LOTTERY_RESERVATION_PATH
+  } else if (event.batch_type == 'send_same_hour_schedule_mail') {
+    path = process.env.SEND_SAME_HOUR_SCHEDULE_MAIL_PATH
+  } else if (event.batch_type == 'exec_merchant_subscription_payment') {
+    path = process.env.EXEC_MERCHANT_SUBSCRIPTION_PAYMENT_PATH
+  } else if (event.batch_type == 'exec_system_subscription_payment') {
+    path = process.env.EXEC_SYSTEM_SUBSCRIPTION_PAYMENT_PATH
+  }
+  
+  let options = {
+      hostname: hostname,
+      port: 443,
+      path: path,
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      }
+  }
+  
+  console.log("options", options)
+  
+  const data = JSON.stringify({
+    text: "text",
+  })
+  const req = https.request(options, (res) => {
+      let body = '';
+      console.log('Status:', res.statusCode);
+      console.log('Headers:', JSON.stringify(res.headers));
+      res.setEncoding('utf8');
+      res.on('data', (chunk) => body += chunk);
+      res.on('end', () => {
+          console.log('Successfully processed HTTPS response');
+          // If we know it's JSON, parse it
+          if (res.headers['content-type'] === 'application/json') {
+              body = JSON.parse(body);
+          }
+          callback(null, body);
+      })
+      ;
+  });
+  req.on('error', callback);
+  req.write(JSON.stringify(data));
+  req.end();
 };
