88a9b3bb01cf118bafd290d5b712ba7bdb3d481a
ikeyu0806 <ikeyu0806@gmail.com>
2023-03-24 18:10:02 +0900

    add test to batch/reservations_request

--- rails_api/spec/requests/api/batch/reservations_request.rb
+++ rails_api/spec/requests/api/batch/reservations_request.rb
@@ -66,29 +66,68 @@ RSpec.describe 'Api::Batch::ReservationsController', type: :request do
     end
 
     describe 'reservation count is more than reserve_frame capacity' do
-      let(:lottery_reception_reserve_frame) {
-        create(:reserve_frame,
-               :lottery_reception,
-                lottery_confirmed_day_before: 3,
-                capacity: 10,
-                account_id: standard_plan_account.id)
-      }
-  
-      let!(:local_payment_reservations) {
-        create_list(:local_payment_reservation, 5,
-                    :tomorrow_reservation,
-                    reserve_frame_id: lottery_reception_reserve_frame.id,
-                    customer_id: customer.id,
-                    status: 'waitingForLotteryConfirm',
-                    start_at: Time.zone.now + 3.days,
-                    end_at: Time.zone.now + 3.days + 1.hours)
-      }
-      it do
-        post "/api/batch/reservations/confirm_lottery_reservations"
-        expect(response.status).to eq 200
-        response_body = JSON.parse(response.body)
-        expect(response_body["confirmed_reservations"]["#{lottery_reception_reserve_frame.id.to_s}_confirmed_reservations"].length).to eq(local_payment_reservations.count)
-        expect(response_body["lottery_lost_reservations"]["#{lottery_reception_reserve_frame.id.to_s}_lottery_lost_reservations"].length).to eq(0)
+      describe 'reservation number of people is 1' do
+        let(:lottery_reception_reserve_frame) {
+          create(:reserve_frame,
+                 :lottery_reception,
+                  lottery_confirmed_day_before: 3,
+                  capacity: 10,
+                  account_id: standard_plan_account.id)
+        }
+    
+        let!(:local_payment_reservations) {
+          create_list(:local_payment_reservation, 5,
+                      :tomorrow_reservation,
+                      reserve_frame_id: lottery_reception_reserve_frame.id,
+                      customer_id: customer.id,
+                      status: 'waitingForLotteryConfirm',
+                      start_at: Time.zone.now + 3.days,
+                      end_at: Time.zone.now + 3.days + 1.hours)
+        }
+        it do
+          post "/api/batch/reservations/confirm_lottery_reservations"
+          expect(response.status).to eq 200
+          response_body = JSON.parse(response.body)
+          expect(response_body["confirmed_reservations"]["#{lottery_reception_reserve_frame.id.to_s}_confirmed_reservations"].length).to eq(local_payment_reservations.count)
+          expect(response_body["lottery_lost_reservations"]["#{lottery_reception_reserve_frame.id.to_s}_lottery_lost_reservations"].length).to eq(0)
+        end
+      end
+
+      describe 'include reservation2 more number of people' do
+        let(:lottery_reception_reserve_frame) {
+          create(:reserve_frame,
+                 :lottery_reception,
+                  lottery_confirmed_day_before: 3,
+                  capacity: 10,
+                  account_id: standard_plan_account.id)
+        }    
+        let!(:eight_people_reservations) {
+          create(:local_payment_reservation,
+                 :tomorrow_reservation,
+                 reserve_frame_id: lottery_reception_reserve_frame.id,
+                 customer_id: customer.id,
+                 status: 'waitingForLotteryConfirm',
+                 number_of_people: 8,
+                 start_at: Time.zone.now + 3.days,
+                 end_at: Time.zone.now + 3.days + 1.hours)
+        }
+        let!(:five_people_reservations) {
+          create(:local_payment_reservation,
+                 :tomorrow_reservation,
+                 reserve_frame_id: lottery_reception_reserve_frame.id,
+                 customer_id: customer.id,
+                 status: 'waitingForLotteryConfirm',
+                 number_of_people: 5,
+                 start_at: Time.zone.now + 3.days,
+                 end_at: Time.zone.now + 3.days + 1.hours)
+        }
+        it do
+          post "/api/batch/reservations/confirm_lottery_reservations"
+          expect(response.status).to eq 200
+          response_body = JSON.parse(response.body)
+          expect(response_body["confirmed_reservations"]["#{lottery_reception_reserve_frame.id.to_s}_confirmed_reservations"].length).to eq(1)
+          expect(response_body["lottery_lost_reservations"]["#{lottery_reception_reserve_frame.id.to_s}_lottery_lost_reservations"].length).to eq(1)
+        end
       end
     end
   end
