fab9877734e8d131b4caaadd47c1eef760154f32
ikeyu0806 <ikeyu0806@gmail.com>
2023-03-26 15:13:54 +0900

    add contentes to customer detail page

--- frontend/pages/admin/customer/[public_id]/index.tsx
+++ frontend/pages/admin/customer/[public_id]/index.tsx
@@ -2,20 +2,38 @@ import type { NextPage } from 'next'
 import React, { useEffect, useState } from 'react'
 import { Container, Row, Col, Card } from 'react-bootstrap'
 import MerchantUserAdminLayout from 'components/templates/MerchantUserAdminLayout'
-import axios from 'axios'
-import { useDispatch, useSelector } from 'react-redux'
 import { useCookies } from 'react-cookie'
-import { useRouter } from 'next/router'
-import { RootState } from 'redux/store'
-import Unauthorized from 'components/templates/Unauthorized'
 import { CustomerParam } from 'interfaces/CustomerParam'
+import axios from 'axios'
+import { RootState } from 'redux/store'
+import { useRouter } from 'next/router'
+import { showConnectLineUserModalChanged,
+         showEditCustomerModalChanged,
+         firstNameChanged,
+         lastNameChanged,
+         emailChanged,
+         notesChanged,
+         phoneNumberChanged,
+         customerPublicIdChanged } from 'redux/customerSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import CreateCustomerModal from 'components/templates/CreateCustomerModal'
+import EditCustomerModal from 'components/templates/EditCustomerModal'
+import CustomerMailSendModal from 'components/templates/CustomerMailSendModal'
+import Unauthorized from 'components/templates/Unauthorized'
+import LineBrandColorButton from 'components/atoms/LineBrandColorButton'
+import ConnectLineUserModal from 'components/templates/ConnectLineUserModal'
+import CustomerNotesModal from 'components/templates/CustomerNotesModal'
+import LineOfficialAccountModal from 'components/templates/LineOfficialAccountModal'
 
 const Index: NextPage = () => {
   const dispatch = useDispatch()
   const router = useRouter()
   const [cookies] = useCookies(['_square_eight_merchant_session'])
-  const allowReadCustomer = useSelector((state: RootState) => state.merchantUserPermission.allowReadCustomer)
   const [customer, setCustomer] = useState<CustomerParam>()
+  const allowReadCustomer = useSelector((state: RootState) => state.merchantUserPermission.allowReadCustomer)
+  const allowUpdateCustomer = useSelector((state: RootState) => state.merchantUserPermission.allowUpdateCustomer)
+  const allowReadQuestionnaireAnswer = useSelector((state: RootState) => state.merchantUserPermission.allowReadQuestionnaireAnswer)
+  const allowConnectLineUser = useSelector((state: RootState) => state.merchantUserPermission.allowConnectLineUser)
 
   useEffect(() => {
     axios.get(`${process.env.BACKEND_URL}/api/internal/customers/${router.query.public_id}`,
@@ -31,6 +49,15 @@ const Index: NextPage = () => {
     })
   }, [router.query.public_id, dispatch, cookies._square_eight_merchant_session])
 
+  const showEditModal = (customer: CustomerParam) => {
+    dispatch(showEditCustomerModalChanged(true))
+    dispatch(customerPublicIdChanged(customer.public_id))
+    dispatch(lastNameChanged(customer.last_name))
+    dispatch(firstNameChanged(customer.first_name))
+    dispatch(emailChanged(customer.email || ''))
+    dispatch(notesChanged(customer.notes || ''))
+    dispatch(phoneNumberChanged(customer.phone_number || ''))
+  }
 
   return (
     <>
@@ -67,12 +94,95 @@ const Index: NextPage = () => {
                       <>{customer.phone_number}</>}
                     </Col>
                   </Row>
+                  <hr />
+                  <Row>
+                    <Col className='font-weight-bold'>LINEユーザ</Col>
+                    <Col>
+                      {customer && !customer.line_display_name &&
+                        allowConnectLineUser === 'Allow' &&
+                        <LineBrandColorButton
+                          text='LINEユーザと紐付け'
+                          onClick={() => {
+                            dispatch(showConnectLineUserModalChanged(true));
+                            dispatch(customerPublicIdChanged(customer.public_id));
+                          }}
+                          size={'sm'} />}
+                    </Col>
+                  </Row>
+                  <hr />
+                  <Row>
+                    <Col className='font-weight-bold'>アンケート</Col>
+                    <Col>
+                      {customer &&
+                      <>
+                        {allowReadQuestionnaireAnswer === 'Allow' &&
+                        <a href={`/admin/customer/${customer.public_id}/questionnaire_answers`}
+                          className='btn btn-primary btn-sm'>
+                          アンケート回答
+                        </a>}
+                      </>}
+                    </Col>
+                  </Row>
+                  <hr />
+                  <Row>
+                    <Col className='font-weight-bold'>注文履歴</Col>
+                    <Col>
+                      {customer &&
+                      <>
+                        <a href={`/admin/customer/${customer.public_id}/order`}
+                          className='btn btn-primary btn-sm'>
+                          注文履歴
+                        </a>
+                      </>}
+                    </Col>
+                  </Row>
+                  <hr />
+                  <Row>
+                    <Col className='font-weight-bold'>お支払い履歴</Col>
+                    <Col>
+                      {customer &&
+                      <>
+                      <a href={`/admin/customer/${customer.public_id}/charges`}
+                        className='btn btn-primary btn-sm'>
+                        お支払い履歴
+                      </a>
+                      </>}
+                    </Col>
+                  </Row>
+                  <hr />
+                  <Row>
+                    <Col className='font-weight-bold'>編集</Col>
+                    <Col>
+                      {customer &&
+                      <>
+                        {allowUpdateCustomer === 'Allow' &&
+                          <a onClick={() => showEditModal(customer)}
+                          className='btn btn-primary btn-sm mb20'>
+                          編集
+                        </a>}
+                      </>}
+                    </Col>
+                  </Row>
+                  <hr />
+                  <Row>
+                    <Col className='font-weight-bold'>メモ</Col>
+                    <Col>
+                      {customer &&
+                      <>{customer.notes}</>}
+                    </Col>
+                  </Row>
                 </Card.Body>
               </Card>
             </Col>
           </Row>
           <Row></Row>
         </Container>}
+        <CreateCustomerModal></CreateCustomerModal>
+        <EditCustomerModal></EditCustomerModal>
+        <CustomerMailSendModal></CustomerMailSendModal>
+        <ConnectLineUserModal></ConnectLineUserModal>
+        <CustomerNotesModal></CustomerNotesModal>
+        <LineOfficialAccountModal></LineOfficialAccountModal>
         {allowReadCustomer === 'Forbid' && <Unauthorized />}
       </MerchantUserAdminLayout>
     </>
