8320bd97102bfcd7b7cdeb2966225fb4e019f9a7
ikeyu0806 <ikeyu0806@gmail.com>
2023-03-26 09:47:27 +0900

    implement send_mail_schedules_request_spec

---
+++ rails_api/spec/requests/api/internal/send_mail_schedules_request_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+
+RSpec.describe 'Api::Internal::SendMailSchedulesController', type: :request do
+  let(:account) { create(:business_account) }
+  let(:merchant_user) {
+    create(:merchant_user, account: account)
+  }
+  let(:customer) {
+    create(:customer, account_id: account.id)
+  }
+  let!(:send_mail_schedule) {
+    create(:send_mail_schedule,
+      account_id: account.id,
+      merchant_user_id: merchant_user.id,
+      customer_id: customer.id,
+      scheduled_datetime: Time.zone.now) }
+
+  describe 'GET /api/internal/send_mail_schedules' do
+    context 'login as merchant_user' do
+      it 'should return 200' do
+        allow_any_instance_of(ApplicationController).to receive(:current_merchant_user).and_return(merchant_user)
+        get "/api/internal/send_mail_schedules", params: { current_page: 1, display_count: 10 }
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'not login as merchant_user' do
+      it 'should return 401' do
+        get "/api/internal/send_mail_schedules", params: { current_page: 1, display_count: 10 }
+        expect(response.status).to eq 401
+      end
+    end
+  end
+end
