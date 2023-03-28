bba0a9d15161d86671d520826e593579bd39f840
ikeyu0806 <ikeyu0806@gmail.com>
2023-03-28 12:27:06 +0900

    create LandingPageNavbar component

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -8,6 +8,7 @@ import DownArrawIcon from 'components/atoms/DownArrawIcon'
 import { useRouter } from 'next/router'
 import { lightPlanPrice, standardPlanPrice, premiumPlanPrice } from 'constants/systemPlanPrices'
 import { defaultJsonLdData, topPageBreadcrumbListJsonLdData } from 'constants/jsonLdData'
+import LandingPageNavbar from 'components/organisms/LandingPageNavbar'
 import { Container,
          Navbar,
          Nav,
@@ -22,38 +23,7 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <Navbar collapseOnSelect expand='lg' bg='light'>
-        <Container>
-          <Navbar.Brand href='/' className='font-weight-bold'>
-            {/* <ServiceLogoV2 width={22} height={22} /> */}
-            SquareEight
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-          <Navbar.Collapse id='responsive-navbar-nav'>
-            <Nav className='me-auto'>
-              <Nav.Link href='#benefit'>機能・導入効果</Nav.Link>
-              <Nav.Link href='#about'>サービス概要</Nav.Link>
-              <Nav.Link href='#features'>機能一覧</Nav.Link>
-              <Nav.Link href='#pricing'>料金</Nav.Link>
-              <Nav.Link href='/inquiry'>お問い合わせ</Nav.Link>
-              <Nav.Link href='/company_info'>運営会社</Nav.Link>
-            </Nav>
-            <Nav>
-              <Nav.Link href='/customer/login'>
-                <BrandColorButton
-                  buttonText='SquareEightIDログイン'></BrandColorButton>
-              </Nav.Link>
-              <Nav.Link href='/merchant/login'>
-                <BrandColorButton
-                  buttonText='ビジネスアカウントログイン'></BrandColorButton>
-              </Nav.Link>
-              <Nav.Link href='/merchant/signup'>
-
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <LandingPageNavbar />
       {/* <CampaignAlert></CampaignAlert> */}
       <div className={homeStyles.cover_img_parent}>
         <img
