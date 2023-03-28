aa916c8ab54a90475dba29c621b37d19655f245f
ikeyu0806 <ikeyu0806@gmail.com>
2023-03-28 20:18:30 +0900

    create WellnessLandingPageOverview component

--- frontend/pages/lp/wellness.tsx
+++ frontend/pages/lp/wellness.tsx
@@ -10,6 +10,7 @@ import { defaultJsonLdData } from 'constants/jsonLdData'
 import homeStyles from 'styles/Home.module.css'
 import { lightPlanPrice, standardPlanPrice, premiumPlanPrice } from 'constants/systemPlanPrices'
 import WellnessLandingPageBenefit from 'components/organisms/WellnessLandingPageBenefit'
+import WellnessLandingPageOverview from 'components/organisms/WellnessLandingPageOverview'
 
 const Wellness: NextPage = () => {
   return (
@@ -17,35 +18,7 @@ const Wellness: NextPage = () => {
       <LandingPageNavbar />
       <Container>
         <div className='mt20'></div>
-        <Row>
-          <Col>
-            <p>
-              <span className={homeStyles.wellness_lp_linear_gradient_blue}>ウェルネスビジネスのサービス事業者様必見</span>
-            </p>
-            <div>
-              <span className={homeStyles.description_brand_badge}>予約システム</span>
-              <span className={homeStyles.description_brand_badge}>会員管理</span>
-              <span className={homeStyles.description_brand_badge}>月謝</span>
-              <span className={homeStyles.description_brand_badge}>回数券</span>
-              <span className={homeStyles.description_brand_badge}>物販</span>
-              <span className={homeStyles.description_brand_badge}>オンラインアンケート</span>
-              <span className={homeStyles.description_brand_badge}>メール/LINE送信</span>
-            </div>
-            <div className='font-size-30 mt20 mb20'>
-              パーソナルトレーニング・ヨガ・ジム・スポーツスクール・リラク・整体ビジネスを最適化。<br />格安予約システム「SquareEight」
-            </div>
-            <a href='/merchant/signup'><button
-              className={homeStyles.description_trial_button}>
-              無料でお試し。有料プランも{lightPlanPrice}円〜
-            </button></a>
-          </Col>
-          <Col>
-          <img
-            className={homeStyles.fitness_image}
-            src='/images/lp/fitness_image.webp'
-            alt='Top Cover' />
-          </Col>
-        </Row>
+        <WellnessLandingPageOverview />
       <hr />
       <div className='text-center font-size-30 font-weight-bold'>
         <p className={homeStyles.section_headline_parent}>
