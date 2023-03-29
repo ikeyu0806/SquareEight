fa6299bb5dc6b9d64cb4cadcddb3ac174c4162c5
ikeyu0806 <ikeyu0806@gmail.com>
2023-03-29 16:28:51 +0900

    fix businessSample style

--- frontend/components/organisms/LandingPageBusinessTypeSamples.tsx
+++ frontend/components/organisms/LandingPageBusinessTypeSamples.tsx
@@ -10,13 +10,16 @@ const LandingPageBusinessTypeSamples = (): JSX.Element => {
           <span className={homeStyles.section_headline}>こんな場面でお使いになれます!</span>
         </p>
         <div className='text-center mt10'>
-          <p><span className={homeStyles.section_description}>全業種対応。<a href='/lp/wellness'>特にウェルネスビジネスにおすすめ</a></span></p>
+          <p><span className={homeStyles.section_description}>全業種対応</span></p>
         </div>
         <Container>
           <Row>
             <Col lg={4} md={4}>
               <div className={homeStyles.business_type_content}>
-                <div>ウェルネスビジネス運営</div>
+                <div className='font-weight-bold'>ウェルネスビジネス運営</div>
+                <div className='mt20'>
+                  パーソナルトレーニング / ジム / ヨガスタジオ / ピラティス / フィットネスクラブ /テニススクール / スイミングスクール / ダンススクール / ゴルフスクール / 格闘技 / その他スポーツスクール
+                </div>
                 <a
                   href='/lp/wellness'
                   className='btn btn-info text-white mt20'>もっとみる</a>
@@ -24,7 +27,8 @@ const LandingPageBusinessTypeSamples = (): JSX.Element => {
             </Col>
             <Col lg={4} md={4}>
               <div className={homeStyles.business_type_content}>
-                <div>美容系ビジネス運営</div>
+                <div className='font-weight-bold'>美容系ビジネス運営</div>
+                <div className='mt20'>美容室 / ヘアサロン / ネイルサロン / まつげサロン / メイクサロン / 医療クリニック</div>
                 <a
                   href='/lp/beauty'
                   className='btn btn-info text-white mt20'>もっとみる</a>
@@ -32,7 +36,8 @@ const LandingPageBusinessTypeSamples = (): JSX.Element => {
             </Col>
             <Col lg={4} md={4}>
               <div className={homeStyles.business_type_content}>
-                <div>イベント・セミナー運営</div>
+                <div className='font-weight-bold'>イベント・セミナー運営</div>
+                <div className='mt20'>まちおこしイベント / 留学イベント / サークルイベント / 体験型ゲーム / ショーイベント / パーティー / 会社説明会 / 採用説明会 / 講習会</div>
                 <a
                   href='/lp/event'
                   className='btn btn-info text-white mt20'>もっとみる</a>
